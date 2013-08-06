d9a53b350267555a7a5c8a3ab034db7a5bb3ddf0
kaz-yos <kazky@mac.com>
2013-08-06 18:46:45 -0400

    how about this
    (add-hook 'ess-mode-hook		; For ESS mode
              '(lambda()
    	     ;; (local-unset-key [C-tab] 'ess-sas-backward-delete-tab)	; Does not work with the one below??
    	     (local-set-key [(control return)] 'my-ess-eval)
                 (local-set-key [(shift return)] 'my-ess-eval)
    	     (cua-mode nil)					; disable CUA C-RET
    	     (local-set-key (kbd "C-c 4") 'toggle-dollar)
    	     ))

--- init.el
+++ init.el
@@ -798,7 +798,7 @@ In case the execution fails, return an error."
 	     (local-set-key [(control return)] 'my-ess-eval)
              (local-set-key [(shift return)] 'my-ess-eval)
 	     (cua-mode nil)					; disable CUA C-RET
-	     (local-set-key [C-c 4] 'toggle-dollar)
+	     (local-set-key (kbd "C-c 4") 'toggle-dollar)
 	     ))
 (add-hook 'inferior-ess-mode-hook	; For iESS mode
           '(lambda()
