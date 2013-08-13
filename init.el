5fa0bf596f726d81cf58c23eef4a5d7ed2a96f34
user <no.email>
2013-08-13 09:34:23 -0400

    ;; Space bar use (define-key view-mode-map (kbd " ") 'scroll-up) (define-key view-mode-map (kbd "S-SPC") 'scroll-down)

--- init.el
+++ init.el
@@ -1276,6 +1276,33 @@ In case the execution fails, return an error."
 (define-key view-mode-map (kbd "l") 'forward-char)
 (define-key view-mode-map (kbd "J") 'View-scroll-line-forward)
 (define-key view-mode-map (kbd "K") 'View-scroll-line-backward)
+;; Space bar use
+(define-key view-mode-map (kbd " ") 'scroll-up)
+(define-key view-mode-map (kbd "S-SPC") 'scroll-down)
+;; for bm.el
+(define-key view-mode-map (kbd ".") 'bm-toggle)
+(define-key view-mode-map (kbd "[") 'bm-previous)
+(define-key view-mode-map (kbd "]") 'bm-next)
+;;
+;; Open non-writable files in view-mode
+(defadvice find-file
+  (around find-file-switch-to-view-file (file &optional wild) activate)
+  (if (and (not (file-writable-p file))
+           (not (file-directory-p file)))
+      (view-file file)
+    ad-do-it))
+;; Do not exit view-mode if non-writable
+(defvar view-mode-force-exit nil)
+(defmacro do-not-exit-view-mode-unless-writable-advice (f)
+  `(defadvice ,f (around do-not-exit-view-mode-unless-writable activate)
+     (if (and (buffer-file-name)
+              (not view-mode-force-exit)
+              (not (file-writable-p (buffer-file-name))))
+         (message "File is unwritable, so stay in view-mode.")
+       ad-do-it)))
+;; Add advice
+(do-not-exit-view-mode-unless-writable-advice view-mode-exit)
+(do-not-exit-view-mode-unless-writable-advice view-mode-disable)
 ;;
 ;; Activate view-mode for these major modes by activating read-only-mode
 ;; (add-hook 'emacs-lisp-mode-hook 'read-only-mode)	; This breaks start up screen
