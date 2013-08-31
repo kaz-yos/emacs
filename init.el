74024760055f074ecf98dfb7d08a70e248eee8f4
kaz-yos <kazky@mac.com>
2013-08-30 20:13:01 -0400

    paredit-menu disabled rainbow-delimiters

--- init.el
+++ init.el
@@ -1624,31 +1624,31 @@ In case the execution fails, return an error."
 (require 'paredit-menu)
 
 
-;; rainbow-delimiters.el
-;; http://www.emacswiki.org/emacs/RainbowDelimiters
-(require 'rainbow-delimiters)
-(add-hook 'ess-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)	; for programming related modes
-;;
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(completions-common-part ((t (:inherit default :foreground "red"))))
- '(diredp-compressed-file-suffix ((t (:foreground "#7b68ee"))))
- '(diredp-ignored-file-name ((t (:foreground "#aaaaaa"))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "blue"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "blue"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "#8b7500"))))
- '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
- '(show-paren-match ((((class color) (background light)) (:background "azure2")))))
+;; ;; rainbow-delimiters.el
+;; ;; http://www.emacswiki.org/emacs/RainbowDelimiters
+;; (require 'rainbow-delimiters)
+;; (add-hook 'ess-mode-hook 'rainbow-delimiters-mode)
+;; (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)	; for programming related modes
+;; ;;
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(completions-common-part ((t (:inherit default :foreground "red"))))
+;;  '(diredp-compressed-file-suffix ((t (:foreground "#7b68ee"))))
+;;  '(diredp-ignored-file-name ((t (:foreground "#aaaaaa"))))
+;;  '(rainbow-delimiters-depth-1-face ((t (:foreground "blue"))))
+;;  '(rainbow-delimiters-depth-2-face ((t (:foreground "blue"))))
+;;  '(rainbow-delimiters-depth-3-face ((t (:foreground "#8b7500"))))
+;;  '(rainbow-delimiters-depth-4-face ((t (:foreground "#8b7500"))))
+;;  '(rainbow-delimiters-depth-5-face ((t (:foreground "#8b7500"))))
+;;  '(rainbow-delimiters-depth-6-face ((t (:foreground "#8b7500"))))
+;;  '(rainbow-delimiters-depth-7-face ((t (:foreground "#8b7500"))))
+;;  '(rainbow-delimiters-depth-8-face ((t (:foreground "#8b7500"))))
+;;  '(rainbow-delimiters-depth-9-face ((t (:foreground "#8b7500"))))
+;;  '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
+;;  '(show-paren-match ((((class color) (background light)) (:background "azure2")))))
 
 
 
