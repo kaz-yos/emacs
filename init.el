c859180665989b3574059b279951b06c7cf86aac
user <no.email>
2013-11-29 10:28:34 -0500

    reversed ;;; Cursor move undo ;; ;; point-undo.el ;; http://d.hatena.ne.jp/rubikitch/20081230/pointundo (require 'point-undo) ;; (define-key global-map (kbd "<f5>") 'point-undo) ;; (define-key global-map (kbd "S-<f5>") 'point-redo) (define-key global-map (kbd "<C-f5>")   'point-undo) (define-key global-map (kbd "C-S-<f5>") 'point-redo) ;; ;; goto-chg.el ;; http://www.emacswiki.org/emacs/GotoChg (require 'goto-chg) ;; (global-set-key (kbd "<C-f5>") 'goto-last-change) ;; (global-set-key (kbd "C-S-<f5>") 'goto-last-change-reverse) (global-set-key (kbd "<f5>")   'goto-last-change) (global-set-key (kbd "S-<f5>") 'goto-last-change-reverse)

--- init.el
+++ init.el
@@ -1969,14 +1969,18 @@ In case the execution fails, return an error."
 ;; point-undo.el
 ;; http://d.hatena.ne.jp/rubikitch/20081230/pointundo
 (require 'point-undo)
-(define-key global-map (kbd "<f5>") 'point-undo)
-(define-key global-map (kbd "S-<f5>") 'point-redo)
+;; (define-key global-map (kbd "<f5>") 'point-undo)
+;; (define-key global-map (kbd "S-<f5>") 'point-redo)
+(define-key global-map (kbd "<C-f5>")   'point-undo)
+(define-key global-map (kbd "C-S-<f5>") 'point-redo)
 ;;
 ;; goto-chg.el
 ;; http://www.emacswiki.org/emacs/GotoChg
 (require 'goto-chg)
-(global-set-key (kbd "<C-f5>") 'goto-last-change)
-(global-set-key (kbd "C-S-<f5>") 'goto-last-change-reverse)
+;; (global-set-key (kbd "<C-f5>") 'goto-last-change)
+;; (global-set-key (kbd "C-S-<f5>") 'goto-last-change-reverse)
+(global-set-key (kbd "<f5>")   'goto-last-change)
+(global-set-key (kbd "S-<f5>") 'goto-last-change-reverse)
 
 
 ;;; Sequential keys
