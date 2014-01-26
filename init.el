81d76f0ac7d36ee0e3a2ff3d0e64bc53023f8c16
kaz-yos <kazky@mac.com>
2014-01-26 07:30:25 -0500

    turned off flyspell by default. problem with Japanese input

--- init.el
+++ init.el
@@ -532,14 +532,14 @@ If you omit CLOSE, it will reuse OPEN."
 ;; http://ergoemacs.org/emacs/emacs_buffer_management.html
 (defalias 'list-buffers 'ibuffer)
 ;;
-;; Switching to ibuffer puts the cursor on the most recent buffer
+;; Switching to ibuffer puts the cursor on the most recent buffer	; Not useful 2014-01-25
 ;; http://www.emacswiki.org/emacs/IbufferMode#toc13
-(defadvice ibuffer (around ibuffer-point-to-most-recent) ()
-  "Open ibuffer with cursor pointed to most recent buffer name"
-  (let ((recent-buffer-name (buffer-name)))
-    ad-do-it
-    (ibuffer-jump-to-buffer recent-buffer-name)))
-(ad-activate 'ibuffer)
+;; (defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+;;   "Open ibuffer with cursor pointed to most recent buffer name"
+;;   (let ((recent-buffer-name (buffer-name)))
+;;     ad-do-it
+;;     (ibuffer-jump-to-buffer recent-buffer-name)))
+;; (ad-activate 'ibuffer)
 ;;
 ;; Emacs Tip of the Day: Start Using IBuffer ASAP.
 ;; http://mytechrants.wordpress.com/2010/03/25/emacs-tip-of-the-day-start-using-ibuffer-asap/
@@ -558,13 +558,14 @@ If you omit CLOSE, it will reuse OPEN."
 ;; http://unix.stackexchange.com/questions/35830/change-column-width-in-an-emacs-ibuffer-on-the-fly
 (setq ibuffer-formats
       '((mark modified read-only	; Three flags without spaces in between.
-	      " "			; Space
+	      " "			;
 	      (name 18 18 :left :elide)	; Buffer name
+	      " "			;
 	      git-status-mini		; ibuffer-git short status
-	      " "
+	      " "			;
 	      (size-h 9 -1 :right)	; size-h defined at the end
 	      ;;(size 9 -1 :right)	; original size in bytes
-	      " "
+	      " "			;
 	      (mode 10 10 :left :elide)	; Mode
 	      " "
 	      filename-and-process)))
@@ -601,22 +602,8 @@ If you omit CLOSE, it will reuse OPEN."
 	       ("MAGIT"  (or 
 			  (mode . magit-mode)
 			  (mode . magit-status-mode)
-			  (mode . magit-process-mode)))
-	       ("perl" (mode . cperl-mode))
-	       ("erc" (mode . erc-mode))
-	       ("planner" (or
-			   (name . "^\\*Calendar\\*$")
-			   (name . "^diary$")
-			   (mode . muse-mode)))
-	       ("gnus" (or
-			(mode . message-mode)
-			(mode . bbdb-mode)
-			(mode . mail-mode)
-			(mode . gnus-group-mode)
-			(mode . gnus-summary-mode)
-			(mode . gnus-article-mode)
-			(name . "^\\.bbdb$")
-			(name . "^\\.newsrc-dribble")))
+			  (mode . magit-process-mode)
+			  (mode . git-commit-mode)))
 	       ))))
 ;; Group for the other buffers.
 (add-hook 'ibuffer-mode-hook
@@ -827,6 +814,7 @@ In case the execution fails, return an error."
 (define-auto-insert "\\.sas$" "SAS.sas")
 (define-auto-insert "\\.sh$" "shell.sh")
 (define-auto-insert "\\.tex$" "LaTeX.tex")
+(define-auto-insert "\\.gitignore$" ".gitignore")
 
 
 ;;; Auto-completion
@@ -894,6 +882,85 @@ In case the execution fails, return an error."
 ;; http://www.emacswiki.org/emacs/EmacsNewbieWithIcicles
 (require 'icicles)
 (icy-mode 1)
+
+
+;;; flyspell-mode 2014-01-20
+;; Define a fuction to use the popup.el
+;; http://d.hatena.ne.jp/mooz/20100423/p1
+(defun flyspell-correct-word-popup-el ()
+  "Pop up a menu of possible corrections for misspelled word before point."
+  (interactive)
+  ;; use the correct dictionary
+  (flyspell-accept-buffer-local-defs)
+  (let ((cursor-location (point))
+	(word (flyspell-get-word nil)))
+    (if (consp word)
+	(let ((start (car (cdr word)))
+	      (end (car (cdr (cdr word))))
+	      (word (car word))
+	      poss ispell-filter)
+	  ;; now check spelling of word.
+	  (ispell-send-string "%\n")	;put in verbose mode
+	  (ispell-send-string (concat "^" word "\n"))
+	  ;; wait until ispell has processed word
+	  (while (progn
+		   (accept-process-output ispell-process)
+		   (not (string= "" (car ispell-filter)))))
+	  ;; Remove leading empty element
+	  (setq ispell-filter (cdr ispell-filter))
+	  ;; ispell process should return something after word is sent.
+	  ;; Tag word as valid (i.e., skip) otherwise
+	  (or ispell-filter
+	      (setq ispell-filter '(*)))
+	  (if (consp ispell-filter)
+	      (setq poss (ispell-parse-output (car ispell-filter))))
+	  (cond
+	   ((or (eq poss t) (stringp poss))
+	    ;; don't correct word
+	    t)
+	   ((null poss)
+	    ;; ispell error
+	    (error "Ispell: error in Ispell process"))
+	   (t
+	    ;; The word is incorrect, we have to propose a replacement.
+	    (flyspell-do-correct (popup-menu* (car (cddr poss)) :scroll-bar t :margin t)
+				 poss word cursor-location start end cursor-location)))
+	  (ispell-pdict-save t)))))
+
+;; C-M-return at the word to correct to activate
+(add-hook 'flyspell-mode-hook
+          (lambda ()
+            (define-key flyspell-mode-map (kbd "<C-M-return>") 'flyspell-correct-word-popup-el)
+            ))
+
+;; Auto-start flyspell-mode for these files
+;; (add-to-list 'auto-mode-alist '("\\.txt" . flyspell-mode))
+;; (add-to-list 'auto-mode-alist '("\\.tex" . flyspell-mode))
+
+;; Save word without a mouse
+;; http://stackoverflow.com/questions/11070849/flyspell-without-a-mouse
+(defun save-ispell-word (word)
+  (interactive "sA word you want to add to dictionary ")
+  (ispell-send-string (concat "*" word "\n"))
+  (setq ispell-pdict-modified-p '(t)))
+
+
+;; ac-ispell	; 2014-01-20
+;; Auto-completion for English words
+;; (custom-set-variables
+;;   '(ac-ispell-requires 4))
+
+;; (eval-after-load "auto-complete"
+;;   '(progn
+;;       (ac-ispell-setup)))
+
+;; (defun my/enable-ac-ispell ()
+;;   (add-to-list 'ac-sources 'ac-source-ispell))
+
+;; (add-hook 'git-commit-mode-hook 'my/enable-ac-ispell)
+;; (add-hook 'mail-mode-hook 'my/enable-ac-ispell)
+
+
 
 
 ;;; YAsnippet ; Automatic template inserting system.
