4dc3a1e4c7327a0dd361614e90089ece18951a24
kaz-yos <kazky@mac.com>
2014-02-23 21:17:15 -0500

    SLIME config

--- init.el
+++ init.el
@@ -2889,8 +2889,18 @@ If you omit CLOSE, it will reuse OPEN."
 ;; http://dev.ariel-networks.com/wp/archives/462
 (require 'slime)
 (slime-setup '(slime-repl slime-fancy slime-banner))
+;;
+;; elisp as inferior-lisp-program	; did not work
+;; http://stackoverflow.com/questions/6687721/repl-for-emacs-lisp
+;;(setq inferior-lisp-program "/usr/local/bin/emacs --batch --eval '(while t (print (eval (read))))'")
+;;
+;; Common lisp (installed via homebrew)
+(setq inferior-lisp-program "/usr/local/bin/clisp")
 
-;; SLIME-like navigation 
+
+
+
+;; SLIME-like navigation
 ;; This package provides Slime's convenient "M-." and "M-," navigation
 ;; in `emacs-lisp-mode', together with an elisp equivalent of
 ;; `slime-describe-symbol', bound by default to `C-c C-d d`.
