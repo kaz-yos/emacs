740eb9061b9d26a7145a1b0abcaa4aa189bdd587
kaz-yos <kazky@mac.com>
2014-02-24 22:29:02 -0500

    package.el melpa.el el-get.el in init.d/000_package-system-related.el (experimental)

---
+++ init.d/000_package-system-related.el
@@ -0,0 +1,46 @@
+;;; package.el and repositories
+;; THIS HAS TO COME BOFORE init-loader.el (installed via package.el)
+;; http://www.emacswiki.org/emacs/ELPA
+(require 'package)
+;;
+;; Load Emacs Lisp packages, and activate them.
+(package-initialize)
+;;
+;; MELPA	;
+;; http://melpa.milkbox.net/#installing
+;; http://melpa.milkbox.net/#/getting-started
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;;
+;; Marmalade (not active)
+;; http://www.emacswiki.org/emacs/Marmalade
+;; http://qiita.com/items/e81fca7a9797fe203e9f
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;
+;; org mode
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+;;
+;; Refresh contents  if no package-archive-contents available
+;; http://stackoverflow.com/questions/14836958/updating-packages-in-emacs
+(when (not package-archive-contents)
+  (package-refresh-contents))
+;;
+;; melpa.el
+;; Installation:
+;; (progn
+;;   (switch-to-buffer
+;;    (url-retrieve-synchronously
+;;     "https://raw.github.com/milkypostman/melpa/master/melpa.el"))
+;;   (package-install-from-buffer  (package-buffer-info) 'single))
+(require 'melpa)
+
+
+;;; el-get.el package system 2013-02-26
+;; https://github.com/dimitri/el-get
+;; The load-path is configured at the top.
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(el-get 'sync)
