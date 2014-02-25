da72d9f52a88c41a2c8340cd9ceeea1cbda19d33
kaz-yos <kazky@mac.com>
2014-02-24 22:43:14 -0500

    decent-defaults file given 100

--- init.d/100_decent-defaults.el
+++ init.d/100_decent-defaults.el
@@ -9,6 +9,18 @@
 (delete-selection-mode t)			
 
 
+;;; Bars: Menu bar only. No scroll bar or tool bar.
+;; http://www.emacswiki.org/emacs/FullScreen#toc7
+(menu-bar-mode t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+
+;;; Show the full path in the frame bar (title bar)
+;; http://stackoverflow.com/questions/8945056/emacs-how-to-show-the-current-directory-in-the-frame-bar
+(setq frame-title-format '(:eval (if (buffer-file-name) (abbreviate-file-name (buffer-file-name)) "%b")))
+
+
 ;;; No start up message
 (setq inhibit-startup-message t)
 
