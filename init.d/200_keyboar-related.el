34433547156be1956f080d6bd35fa570f1953cb3
kaz-yos <kazky@mac.com>
2014-03-02 07:21:23 -0500

    mac: assigned all alt, super, hyper

--- init.d/200_keyboar-related.el
+++ init.d/200_keyboar-related.el
@@ -1,5 +1,6 @@
 ;;; Keyboard modification
 
+
 ;;; C-h for delete (backspace)
 ;; http://www.emacswiki.org/emacs-en/BackspaceKey
 (define-key key-translation-map [?\C-h] [?\C-?])
@@ -16,15 +17,17 @@
   ;; Mac-only
   ;; Command key as Meta key, Option key untouched
   ;; http://www.emacswiki.org/emacs/MetaKeyProblems#toc15
+  ;; http://ergoemacs.org/emacs/emacs_hyper_super_keys.html
   ;;
-  ;; Remove default both options to meta binding
-  (setq mac-option-modifier nil)
-  ;; right option as meta
-  (setq mac-right-option-modifier 'meta)
-  ;; left option as super
-  (setq mac-left-option-modifier 'super)
-  ;; command as meta
+  ;; left command
   (setq mac-command-modifier 'meta)
+  ;; left option
+  (setq mac-option-modifier 'alt)
+  ;;
+  ;; right command
+  (setq mac-right-command-modifier 'super)
+  ;; right option
+  (setq mac-right-option-modifier 'hyper)
   ;;
   ;; Mac Binding modifier keys
   ;; http://www.emacswiki.org/emacs/EmacsForMacOS#toc23
@@ -35,5 +38,10 @@
   ;; mac-right-command-modifier
   ;; mac-right-control-modifier
   ;; mac-right-option-modifier
-  ;; values can be 'control, 'alt, 'meta, 'super, 'hyper, nil (setting to nil allows the OS to assign values)
+  ;; values can be 'control (C), 'alt (A), 'meta (M), 'super (s), or 'hyper (H).
+  ;; setting to nil allows the OS to assign values
   )
+
+
+;;; Windows only configuration
+
