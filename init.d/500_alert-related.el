8c4757403dd406e0f11095253936d7d56da5c360
kaz-yos <kazky@mac.com>
2014-03-02 16:11:51 -0500

    alert.el added only usable in 24.4??

---
+++ init.d/500_alert-related.el
@@ -0,0 +1,8 @@
+;;; alert.el		; Growl-style notification system for Emacs
+;;
+;; Alert is a Growl-workalike for Emacs which uses a common notification
+;; interface and multiple, selectable "styles", whose use is fully
+;; customizable by the user.
+;;
+(require 'alert)
+
