f24605b148c848034cd2e10eaba1d679f8ea530b
kaz-yos <kazky@mac.com>
2014-03-04 21:56:50 -0500

    mode-line-related added for smart-mode-line

---
+++ init.d/500_mode-line-related.el
@@ -0,0 +1,6 @@
+;;; smart-mode-line.el		; A color coded smart mode-line.
+;; https://github.com/Bruce-Connor/smart-mode-line/
+(require 'smart-mode-line)
+;;
+;; Minimum and maximum size of the file name in the mode-line.
+(setq sml/name-width 15)
