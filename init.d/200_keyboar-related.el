bad29f7a27037fcf115546d801dbffcc91205d7c
kaz-yos <kazky@mac.com>
2014-03-04 22:51:05 -0500

    added windows only super/hyper setting template (not expected to work at this point)

--- init.d/200_keyboar-related.el
+++ init.d/200_keyboar-related.el
@@ -1,5 +1,5 @@
 ;;; Keyboard modification
-
+;; http://www.masteringemacs.org/articles/2011/02/08/mastering-key-bindings-emacs/
 
 ;;; C-h for delete (backspace)
 ;; http://www.emacswiki.org/emacs-en/BackspaceKey
@@ -44,4 +44,16 @@
 
 
 ;;; Windows only configuration
-
+;; http://ergoemacs.org/emacs/emacs_hyper_super_keys.html
+(when (eq system-type 'w32)
+  ;; make PC keyboard's Win key or other to type Super or Hyper, for emacs running on Windows.
+  (setq w32-pass-lwindow-to-system nil
+	w32-pass-rwindow-to-system nil
+	w32-pass-apps-to-system nil
+	;; Left Windows key
+	w32-lwindow-modifier 'super
+	;; Right Windows key
+	w32-rwindow-modifier 'super
+	;; Menu key
+	w32-apps-modifier 'hyper)
+  )
