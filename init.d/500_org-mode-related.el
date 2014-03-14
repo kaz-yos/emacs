d7154fffbd8f0224097567bd8bb137f2e7009d20
kaz-yos <kazky@mac.com>
2014-03-13 22:57:14 -0400

    keep S-TAB buffer move in org mode

--- init.d/500_org-mode-related.el
+++ init.d/500_org-mode-related.el
@@ -1,5 +1,22 @@
 ;;; Org-mode related
+;; Load
+(require 'org)
 ;;
-;; Keep the following
+;; Keep the following key
 ;; (global-set-key (kbd "<C-tab>") 'other-window-or-split)
 (define-key org-mode-map (kbd "<C-tab>") 'other-window-or-split)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
