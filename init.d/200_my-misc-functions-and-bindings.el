ac7cb1e226b6cec2c21c76cb520b4e5313690d9f
kaz-yos <kazky@mac.com>
2014-04-12 14:19:38 -0400

    my-start-repl with local variables rather than global

--- init.d/200_my-misc-functions-and-bindings.el
+++ init.d/200_my-misc-functions-and-bindings.el
@@ -37,33 +37,35 @@
   "Start an REPL using a function specified in fun-repl-start,
 if a buffer named repl-buffer-name is not available."
   (interactive)
-  (if (not (member repl-buffer-name (mapcar #'buffer-name (buffer-list))))
-      (progn
-	;; C-x 1 Keep only the window from which this function was called.
-        (delete-other-windows)
+  ;; Create local variables
+  (let* (window1 window2 name-script-buffer name-repl-buffer)
+    (if (not (member repl-buffer-name (mapcar #'buffer-name (buffer-list))))
+	(progn
+	  ;; C-x 1 Keep only the window from which this function was called.
+	  (delete-other-windows)
 
-	;; Make window1 keep the selected (only) window
-        (setq window1 (selected-window))
-	;; Make name-script-buffer keep the selected (only) buffer
-        (setq name-script-buffer (buffer-name))
-	;; (split-window &optional WINDOW SIZE SIDE)
-	;; Split window1 (only one) without size, and create a new window on the right.
-	;; Use the return value (new window) for window2.
-	;; window1: left, window2: right
-        (setq window2 (split-window window1 nil "right"))
+	  ;; Make window1 keep the selected (only) window
+	  (setq window1 (selected-window))
+	  ;; Make name-script-buffer keep the selected (only) buffer
+	  (setq name-script-buffer (buffer-name))
+	  ;; (split-window &optional WINDOW SIZE SIDE)
+	  ;; Split window1 (only one) without size, and create a new window on the right.
+	  ;; Use the return value (new window) for window2.
+	  ;; window1: left, window2: right
+	  (setq window2 (split-window window1 nil "right"))
 
-	;; Activate the REPL
-        (funcall fun-repl-start)
-	;; Make name-repl-buffer keep the selected buffer (REPL)
-	(setq name-repl-buffer (buffer-name))
+	  ;; Activate the REPL
+	  (funcall fun-repl-start)
+	  ;; Make name-repl-buffer keep the selected buffer (REPL)
+	  (setq name-repl-buffer (buffer-name))
 
-	;; REPL on the left (window1)	; This gives an error.
-        (set-window-buffer window1 name-repl-buffer)
-	;; Script on the right (window2)
-        (set-window-buffer window2 name-script-buffer)
-	;; Select the script window on the right (window2)
-	(select-window window2)
-	)))
+	  ;; REPL on the left (window1)	; This gives an error.
+	  (set-window-buffer window1 name-repl-buffer)
+	  ;; Script on the right (window2)
+	  (set-window-buffer window2 name-script-buffer)
+	  ;; Select the script window on the right (window2)
+	  (select-window window2)
+	  ))))
 ;;
 ;; R interpreter
 ;; (my-repl-start "*R*" #'R)
