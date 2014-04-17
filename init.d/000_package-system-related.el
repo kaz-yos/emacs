b581bcf2a388fca50ae6c431199db04652348b97
kaz-yos <kazky@mac.com>
2014-04-16 23:16:14 -0400

    melpa folder added to path only in 24.3.1

--- init.d/000_package-system-related.el
+++ init.d/000_package-system-related.el
@@ -23,40 +23,12 @@
 ;; http://stackoverflow.com/questions/14836958/updating-packages-in-emacs
 (when (not package-archive-contents)
   (package-refresh-contents))
-
-
-;;; 24.4 work around
-(when (not (equal emacs-version "24.3.1"))
-  ;; https://github.com/LiaoPengyu/emacs.d/commit/fc7a6fedb5c496c613d6d19a4fcdab18b36a8d87
-  ;; https://github.com/LiaoPengyu/emacs.d/blob/fc7a6fedb5c496c613d6d19a4fcdab18b36a8d87/init-elpa.el
-;;; Add support to package.el for pre-filtering available packages
-  (defvar package-filter-function nil
-    "Optional predicate function used to internally filter packages used by package.el.
-
-The function is called with the arguments PACKAGE VERSION ARCHIVE, where
-PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
-ARCHIVE is the string name of the package archive.")
-
-  (defadvice package--add-to-archive-contents
-    (around filter-packages (package archive) activate)
-    "Add filtering of available packages using `package-filter-function', if non-nil."
-    (when (or (null package-filter-function)
-	      (funcall package-filter-function
-		       (car package)
-		       (funcall (if (fboundp 'package-desc-version)
-				    'package--ac-desc-version
-				  'package-desc-vers)
-				(cdr package))
-		       archive))
-      ad-do-it))
-  )
 ;;
-;;
-;; Need to be initialized here after work around.
+;; Need to be initialized.
 (package-initialize)
 
 
-;;; melpa.el (will be obsolete in 24.4
+;;; melpa.el (Obsolete in 24.4. Kept for backward compatibility)
 ;; Installation:
 ;; (progn
 ;;   (switch-to-buffer
@@ -67,9 +39,10 @@ ARCHIVE is the string name of the package archive.")
 ;; Not necessary in 24.4
 ;; https://github.com/milkypostman/melpa/issues/1031
 ;; Load melpa.el if the stable version 2014-03-02
-(if (equal emacs-version "24.3.1")
-    ;; t
-    (require 'melpa))
+(when (equal emacs-version "24.3.1")
+  ;; in "24.3.1" only
+  (add-to-list 'load-path "~/.emacs.d/plugins/melpa-20131205.15")
+  (require 'melpa))
 
 
 ;;; el-get.el package system 2013-02-26
