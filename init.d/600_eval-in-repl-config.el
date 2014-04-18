64fd1c9ccfa208c9eda56a161d24c48b7528bd3e
kaz-yos <kazky@mac.com>
2014-04-18 19:07:59 -0400

    eval-in-repl configuration

---
+++ init.d/600_eval-in-repl-config.el
@@ -0,0 +1,26 @@
+;;; eval-in-repl configuration
+
+;; Set load-path
+(add-to-list 'load-path "~/Documents/programming/eval-in-repl")
+
+;; require
+(require 'eval-in-repl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
