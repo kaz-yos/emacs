1bd6a8633d3715fe376a4e1791889e589424f243
kaz-yos <kazky@mac.com>
2014-04-19 17:28:53 -0400

    slime reconfiguration

--- init.d/500_lang-lisp.el
+++ init.d/500_lang-lisp.el
@@ -140,7 +140,7 @@
 ;;
 ;;; auto-complete for SLIME 2014-02-25
 (require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-mode-hook      'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'slime-repl-mode))
