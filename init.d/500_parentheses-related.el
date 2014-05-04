25d982337721b64475e6cc7daf31fe91d45ddb44
kaz-yos <kazky@mac.com>
2014-05-04 08:14:44 -0400

    smartparens added not configured yet

--- init.d/500_parentheses-related.el
+++ init.d/500_parentheses-related.el
@@ -56,9 +56,14 @@
 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1 (+ 1))))))))))))))
 
 
-
 ;;;
 ;;; lispy.el vi-like Paredit
 ;; https://github.com/abo-abo/lispy
 (require 'lispy)
 
+
+;;;
+;;; smartparens.el
+;; https://github.com/Fuco1/smartparens/wiki
+;; paredit and smartparens: https://github.com/Fuco1/smartparens/wiki/Paredit-and-smartparens
+(require 'smartparens-config)
