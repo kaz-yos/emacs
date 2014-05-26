4666a9234181ee37d5118f148d7cef40881c1005
kaz-yos <kazky@mac.com>
2014-05-26 16:32:07 -0400

    japanese font setting method 2 removed as it was never used

--- init.d/200_language-and-font-related.el
+++ init.d/200_language-and-font-related.el
@@ -76,31 +76,6 @@
   ;; |よしだ|
   ;; |ヨシダ|
 
-  ;;; Method 2	; Not tested yet. Same configuration with a different method.
-  ;; Font set for frame
-  ;; (let* ((size       14)
-  ;;       (asciifont   "Menlo")
-  ;;       (jpfont      "Hiragino Maru Gothic ProN")
-  ;;       (h           (* size 10))
-  ;;       (fontspec    (font-spec :family asciifont))
-  ;;       (jp-fontspec (font-spec :family jpfont)))
-  ;;   ;;
-  ;;   (set-face-attribute 'default nil :family asciifont :height h)
-  ;;   ;;
-  ;;   (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
-  ;;   (set-fontset-font nil 'japanese-jisx0213-2      jp-fontspec)
-  ;;   (set-fontset-font nil 'katakana-jisx0201        jp-fontspec)
-  ;;   (set-fontset-font nil '(#x0080 . #x024F)        fontspec)
-  ;;   (set-fontset-font nil '(#x0370 . #x03FF)        fontspec))
-  ;; ;;
-  ;; (dolist (elt '(("^-apple-hiragino.*"                  . 1.2)
-  ;; 		 (".*osaka-bold.*"                      . 1.2)
-  ;; 		 (".*osaka-medium.*"                    . 1.2)
-  ;; 		 (".*courier-bold-.*-mac-roman"         . 1.0)
-  ;; 		 (".*monaco cy-bold-.*-mac-cyrillic"    . 0.9)
-  ;; 		 (".*monaco-bold-.*-mac-roman"          . 0.9)))
-  ;;   (add-to-list 'face-font-rescale-alist elt))
-
   ;;;
   ;;; inline patch for Japanese IME
   ;; Change to English in minibuffer (require inline patch. No .el dependency)
