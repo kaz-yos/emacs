7e9dd055f24000cd6528e1c24bf3d0bb70d34828
kaz-yos <kazky@mac.com>
2014-07-19 08:19:08 +0900

    Delete ede project file, clean emacsclient config

--- init.d/200_emacsclient-related.el
+++ init.d/200_emacsclient-related.el
@@ -1,19 +1,20 @@
 ;;; Use emacsclient
 ;;
-;; emacsclient is a symlink
-;; ln -s /Applications/Emacs24.3.app/Contents/MacOS/bin/emacsclient /usr/local/bin/emacsclient
+;; emacsclient is a symlink to the one in Application
+;; ln -s /Applications/Emacs.app/Contents/MacOS/bin/emacsclient /usr/local/bin/emacsclient
 ;;
-;; configured as $EDITOR
+;; Configure as $EDITOR
 ;; .profile: export EDITOR="emacsclient"
-;;;
+;;
 ;; http://www.emacswiki.org/emacs/EmacsClient
 ;; http://qiita.com/syohex/items/2bdcd9cd5b701b1112e6
 (require 'server)
-;; Start server unless there is already one running.
+;;; Start server unless there is already one running.
 (unless (server-running-p)
   (server-start))
 ;;
 (defvar server-buffer-clients)
+;;
 (when (and (fboundp 'server-start) (string-equal (getenv "TERM") 'xterm))
   (server-start)
   (defun fp-kill-server-with-buffer-routine ()
