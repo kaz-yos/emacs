9218b298fba5fe7e2e598bdb407e2cde4dec5fed
kaz-yos <kazky@mac.com>
2014-09-11 18:36:32 -0400

    Add some additional R settings

--- autoinsert/auto/knitr.el
+++ autoinsert/auto/knitr.el
@@ -1,19 +1,20 @@
-(TeX-add-style-hook "knitr"
+(TeX-add-style-hook
+ "knitr"
  (lambda ()
     (TeX-run-style-hooks
-     "enumerate"
-     "sectsty"
-     "fancyhdr"
-     "tikz"
-     "graphicx"
-     "courier"
-     "listings"
-     "cancel"
-     "amsmath"
-     "geometry"
-     "margin=2.0cm"
      "latex2e"
-     "art10"
+     ""
      "article"
-     "")))
+     "art10"
+     "margin=2.0cm"
+     "geometry"
+     "amsmath"
+     "cancel"
+     "listings"
+     "courier"
+     "graphicx"
+     "tikz"
+     "fancyhdr"
+     "sectsty"
+     "enumerate")))
 
