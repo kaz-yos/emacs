b97f2edd0f071d4edbd6ef009d25b43c60b08970
kaz-yos <kazky@mac.com>
2014-09-19 09:24:27 -0400

    Change to ac-cider.el from ac-nrepl.el

--- init.d/500_lang-lisp.el
+++ init.d/500_lang-lisp.el
@@ -82,25 +82,41 @@
 (define-key clojure-mode-map (kbd "C-c a") 'auto-complete-mode)
 ;;
 ;;
-;;; ac-nrepl
-;; https://github.com/clojure-emacs/ac-nrepl
-(require 'ac-nrepl)
-(add-hook 'cider-repl-mode-hook
-	  '(lambda ()
-	     (eldoc-mode -1)
-	     (ac-nrepl-setup)
-	     ;; Add ac-source-filename for directory name completion
-	     (add-to-list 'ac-sources 'ac-source-filename)))
-;;
-(add-hook 'cider-mode-hook
-	  '(lambda ()
-	     (eldoc-mode -1)
-	     (ac-nrepl-setup)
-	     ;; Add ac-source-filename for directory name completion
-	     (add-to-list 'ac-sources 'ac-source-filename)))
-;;
+;;; ac-cider.el
+;; https://github.com/clojure-emacs/ac-cider
+(require 'ac-cider)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
 (eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'cider-repl-mode))
+  '(add-to-list 'ac-modes 'cider-mode))
+;; If you want to trigger auto-complete using TAB in CIDER buffers, you may
+;; want to use auto-complete in your `completion-at-point-functions':
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;;
+;;
+;;; ac-nrepl.el (deprecated)
+;; ;; https://github.com/clojure-emacs/ac-nrepl
+;; (require 'ac-nrepl)
+;; (add-hook 'cider-repl-mode-hook
+;; 	  '(lambda ()
+;; 	     (eldoc-mode -1)
+;; 	     (ac-nrepl-setup)
+;; 	     ;; Add ac-source-filename for directory name completion
+;; 	     (add-to-list 'ac-sources 'ac-source-filename)))
+;; ;;
+;; (add-hook 'cider-mode-hook
+;; 	  '(lambda ()
+;; 	     (eldoc-mode -1)
+;; 	     (ac-nrepl-setup)
+;; 	     ;; Add ac-source-filename for directory name completion
+;; 	     (add-to-list 'ac-sources 'ac-source-filename)))
+;; ;;
+;; (eval-after-load "auto-complete"
+;;   '(add-to-list 'ac-modes 'cider-repl-mode))
 ;;
 ;;
 ;;
