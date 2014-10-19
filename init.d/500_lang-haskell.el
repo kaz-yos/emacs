332703c71fbba09a2d266b5572088d7fe7741055
kaz-yos <kazky@mac.com>
2014-10-19 16:37:47 -0400

    Add Haskell settings

--- init.d/500_lang-haskell.el
+++ init.d/500_lang-haskell.el
@@ -10,14 +10,33 @@
 ;;;
 ;;; haskell-mode.el
 ;; https://github.com/haskell/haskell-mode#haskell-mode-for-emacs
+;; http://www.haskell.org/haskellwiki/Emacs/Installing_haskell-mode
 (require 'haskell-mode)
 ;;
 ;;
 (add-hook 'haskell-mode-hook
-	  '(turn-on-eldoc-mode turn-on-haskell-doc turn-on-haskell-indent))
+	  '(turn-on-eldoc-mode turn-on-haskell-doc turn-on-haskell-indentation))
+;;
+;; 3 mutually exclusive indent style
+;; http://www.haskell.org/haskellwiki/Emacs/Indentation
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+;;
+;; Keys
+(defun haskell-mode-keys ()
+  (local-set-key (kbd "C-c C-l") 'inferior-haskell-load-file)
+  (local-set-key (kbd "C-c C-r") 'inferior-haskell-reload-file))
+;;
+(add-hook 'haskell-mode-hook 'haskell-mode-keys)
+;;
+;;; inferior haskell mode
+(require 'inf-haskell)
+;;
+(setq haskell-program-name "/usr/bin/ghci")
 
 
 ;;;
 ;;; ghc.el
-(require 'ghc)
+;; (require 'ghc)
 
