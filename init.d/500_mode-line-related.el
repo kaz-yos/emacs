443edbfd678d77db7e0c2ca66ed61b5f3d461bc6
Kazuki Yoshida <kazuki@MBP17-Early2011.local>
2014-11-06 20:20:30 -0500

    Change to power-line

--- init.d/500_mode-line-related.el
+++ init.d/500_mode-line-related.el
@@ -6,7 +6,58 @@
 ;; https://github.com/milkypostman/powerline
 ;; http://shibayu36.hatenablog.com/entry/2014/02/11/160945
 (require 'powerline)
+;;
+;; https://github.com/shibayu36/emacs/blob/8376ba4e4b4d5cb4668e848c9a494b287232ea6f/emacs.d/inits/01-mode-line.el
+(set-face-attribute 'mode-line nil
+                    :foreground "black"
+                    :background "gray90"
+                    :box nil)
+(set-face-attribute 'powerline-active1 nil
+                    :foreground "black"
+                    :background "gray80"
+                    :inherit 'mode-line)
+(set-face-attribute 'powerline-active2 nil
+                    :foreground "black"
+                    :background "gray70"
+                    :inherit 'mode-line)
+;;
+;; Configure at the end
 (powerline-default-theme)
+;;
+;; https://github.com/shibayu36/emacs/blob/8376ba4e4b4d5cb4668e848c9a494b287232ea6f/emacs.d/inits/01-mode-line.el
+(defvar mode-line-cleaner-alist
+  '(;; For minor-mode, first char is 'space'
+    (flymake-mode . " Fm")
+    (paredit-mode . " pr")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (undo-tree-mode . "")
+    (git-gutter-mode . "")
+    (anzu-mode . "")
+    (yas-minor-mode . "")
+    (guide-key-mode . "")
+    (icicle-mode . "")
+    (magit-auto-revert . "")
+    (elisp-slime-nav-mode . "")
+    ;;
+    ;; Major modes
+    (dired-mode . "Dir")
+    (emacs-lisp-mode . "el")
+    ))
+;;
+(defun clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          ;; major mode
+          (when (eq mode major-mode)
+            (setq mode-name mode-str)))))
+;;
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
 
 
 ;; ;;;
@@ -15,7 +66,7 @@
 ;; ;; https://github.com/Bruce-Connor/smart-mode-line/blob/master/smart-mode-line.el
 ;; ;;
 ;; ;; ALL COLORS ARE CUSTOMIZABLE! M-x sml/customize-faces
-;; ;; 
+;; ;;
 ;; ;; Theme: 'automatic, 'respectful, 'dark, 'light or nil
 ;; ;; To change while in session use: M-x sml/apply-theme
 ;; (setq sml/theme nil)
