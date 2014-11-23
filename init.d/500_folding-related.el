0ee381700e76e6adf97e386c345e9ed694a0b91e
kaz-yos <kazky@mac.com>
2014-11-22 21:35:55 -0500

    Change file name of folding related config file

--- init.d/500_folding-related.el
+++ init.d/500_folding-related.el
@@ -1,13 +1,16 @@
-;;; Overview: (taken from the elisp file)
+;;;
+;;; Code folding related
+
+
+
+;;; 
+;;; fold-dwim.el
 ;;
 ;; DWIM stands for "do what I mean", as in the idea that one keystroke
 ;; can do different things depending on the context. In this package,
 ;; it means that, if the cursor is in a currently hidden folded
 ;; construction, we want to show it; if it's not, we want to hide
 ;; whatever fold the cursor is in.
-;;
-;;; Configuration
-;;
 (require 'fold-dwim)
 ;; This package binds no keys by default, so you need to find three
 ;; free and convenient key-bindings.  This is what I use:
@@ -16,3 +19,31 @@
 ;;  (global-set-key (kbd "<M-f7>")    'fold-dwim-hide-all)
 ;;  (global-set-key (kbd "<S-M-f7>")  'fold-dwim-show-all)
 ;;
+
+
+;;; 
+;;; origami.el
+;; https://github.com/gregsexton/origami.el
+(require 'origami)
+;;
+(add-to-list 'origami-parser-alist '(r-mode . origami-c-style-parser))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
