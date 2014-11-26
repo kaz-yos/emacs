dc3c98ba6623e63122a11763fb3890026a175eac
kaz-yos <kazky@mac.com>
2014-11-25 20:49:51 -0500

    Add tuareg.el for OCaml

--- init.d/500_lang-ml.el
+++ init.d/500_lang-ml.el
@@ -1,7 +1,8 @@
 ;;; 500_lang-ml.el ---                               -*- lexical-binding: t; -*-
 ;; Configuration for ML family of languages
 
-;;
+;;; 
+;;; sml-mode.el
 (require 'sml-mode)
 ;;
 ;; ac setting
@@ -11,3 +12,8 @@
 ;;
 (add-hook 'inferior-sml-mode-hook 'inferior-sml-mode-ac-setting)
 
+
+;;; 
+;;; tuareg.el for OCaml
+;; http://forge.ocamlcore.org/projects/tuareg/
+(require 'tuareg)
