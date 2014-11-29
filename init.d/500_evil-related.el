dc86986afe9293ca0d56bcb65beae7e4b554dfbb
kaz-yos <kazky@mac.com>
2014-11-29 15:16:29 -0500

    Add some evil.el settings (not turned on)

--- init.d/500_evil-related.el
+++ init.d/500_evil-related.el
@@ -3,9 +3,12 @@
 ;;;
 ;;; evil.el
 ;; http://www.emacswiki.org/emacs/Evil
-;; Activate evil (2014-02-03 conflict with helm C-z)
+;; Change toggle key from C-z to s-e to avoid helm.el conflict
+(setq evil-toggle-key "s-e")
+;; Activate
 (require 'evil)
-;; (evil-mode 1)	; not by default
+;; (evil-mode 1)
+;; 
 ;;; Making the most of RET and SPC
 ;; Keep RET and SPC bindings.
 ;; http://www.emacswiki.org/emacs/Evil
@@ -15,6 +18,12 @@
   (define-key keymap-from key nil))
 (my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
 (my-move-key evil-motion-state-map evil-normal-state-map " ")
+;;
+;;; Further config
+;; http://qureana.hatenadiary.com/entry/2013/06/15/204147
+;; http://d.hatena.ne.jp/tarao/20130303/evil_intro
+;; Make insert state completely emacs
+(setcdr evil-insert-state-map nil)
 
 ;;;
 ;;; evil-surround.el
