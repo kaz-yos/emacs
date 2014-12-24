4c5e1ccb8819cc9ae3aaee902690aa831eed1ecd
kaz-yos <kazky@mac.com>
2014-12-24 10:31:37 -0500

    Add column-marker

--- init.d/500_lang-common.el
+++ init.d/500_lang-common.el
@@ -5,3 +5,13 @@
 ;;; electric-spacing.el
 ;; https://github.com/zk-phi/electric-spacing
 (require 'electric-spacing)
+
+
+;;;
+;;; column-marker.el
+;; http://www.emacswiki.org/emacs/column-marker.el
+(require 'column-marker)
+;; Define a function for column 80 highlighting
+(defun column-marker80 ()
+  (interactive)
+  (column-marker-2 80))
