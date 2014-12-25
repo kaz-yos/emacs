4a5af99a50d44d120b724d1ed05d0779517b8f91
kaz-yos <kazky@mac.com>
2014-12-24 20:29:29 -0500

    Enable paredit and rainbow delim in Hy mode

--- init.d/500_parentheses-related.el
+++ init.d/500_parentheses-related.el
@@ -5,12 +5,13 @@
 ;;
 ;; M-x install-elisp http://mumble.net/~campbell/emacs/paredit.el
 (require 'paredit)
-(add-hook 'emacs-lisp-mode-hook		'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook	'enable-paredit-mode)
-(add-hook 'lisp-mode-hook		'enable-paredit-mode)
-(add-hook 'clojure-mode-hook		'enable-paredit-mode)
-(add-hook 'scheme-mode-hook		'enable-paredit-mode)
-;; (add-hook 'ielm-mode-hook		'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook         'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook   'enable-paredit-mode)
+(add-hook 'lisp-mode-hook               'enable-paredit-mode)
+(add-hook 'clojure-mode-hook            'enable-paredit-mode)
+(add-hook 'scheme-mode-hook             'enable-paredit-mode)
+(add-hook 'hy-mode-hook                 'enable-paredit-mode)
+;; (add-hook 'ielm-mode-hook            'enable-paredit-mode)
 ;; (add-hook 'ess-mode-hook 'enable-paredit-mode)		; paredit for ESS. too restrictive
 ;; No space when inserted after a word
 ;; http://stackoverflow.com/questions/913449/changing-paredit-formatting
@@ -24,6 +25,7 @@
 ;; Adds a menu to paredit.el as memory aid
 (require 'paredit-menu)
 
+
 ;;;
 ;;; rainbow-delimiters.el
 ;; http://www.emacswiki.org/emacs/RainbowDelimiters
@@ -31,11 +33,12 @@
 ;;
 ;; Activate in these modes
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-mode-hook	'rainbow-delimiters-mode)
-(add-hook 'clojure-mode-hook	'rainbow-delimiters-mode)
+(add-hook 'lisp-mode-hook       'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook    'rainbow-delimiters-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'scheme-mode-hook     'rainbow-delimiters-mode)
-(add-hook 'ess-mode-hook	'rainbow-delimiters-mode)
+(add-hook 'ess-mode-hook        'rainbow-delimiters-mode)
+(add-hook 'hy-mode-hook         'rainbow-delimiters-mode)
 ;;
 ;; http://ergoemacs.org/misc/emacs_rainbow-delimiters-mode.html
 ;; (custom-set-faces
