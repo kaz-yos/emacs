52d29d21680c02521dd21a7d34dfbc7524fb8350
kaz-yos <kazky@mac.com>
2015-01-01 22:02:35 -0500

    Reconfigure vlf for very large file

--- init.d/500_opening-special-files-related.el
+++ init.d/500_opening-special-files-related.el
@@ -1,7 +1,12 @@
+;;; 500_opening-special-files-related.el ---         -*- lexical-binding: t; -*-
+
+;;;
 ;;; vlf: View Large Files
 ;; https://github.com/m00natic/vlfi
-(require 'vlf-integrate)
+(require 'vlf-setup)
 
+
+;;;
 ;;; open-junk-file.el for creation of permanent test files
 (require 'open-junk-file)
 (global-set-key (kbd "C-x C-z") 'open-junk-file)
