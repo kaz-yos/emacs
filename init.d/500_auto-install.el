8512d417b9399a6de0fff13f3425f71fe31f95ff
kaz-yos <kazky@mac.com>
2015-01-09 11:19:25 -0500

    Reconfigure auto-install

--- init.d/500_auto-install.el
+++ init.d/500_auto-install.el
@@ -1,6 +1,6 @@
 ;;; auto-install.el
 
-;;; Auto-intall	; This is causing problem !!! 2013-03-04
+;;; Auto-intall
 ;; http://www.emacswiki.org/AutoInstall
 ;; http://www.emacswiki.org/emacs/auto-install.el
 (require 'auto-install)
@@ -11,19 +11,16 @@
 ;; http://spiri-tua-lism.com/?p=451
 (when (eq system-type 'darwin)
   ;; Mac-only
-  (setq auto-install-use-wget t)
-  )
+  (setq auto-install-use-wget t))
 ;; This has to be done??
 ;; sudo ln -s `which wget` /usr/bin/wget
 ;; ;;
 ;; THIS FREEZES EMACS IF EMACSWIKI IS DONW!!!!!! Defined at the end of this file.
-;; (auto-install-update-emacswiki-package-name t)
 ;; INSTALL-elisp.el compatilibity
 (auto-install-compatibility-setup)
 ;; ediff associated buffers in one frame
 (setq ediff-windows-setup-function 'ediff-setup-windows-plain)
-
-;; This is placed at the end as it freezes if EmacsWIki is not responding.
-(auto-install-update-emacswiki-package-name t) ; THIS FREEZES EMACS IF EMACSWIKI IS DONW!!!!!!
-;;; end of configuration
-
+;;
+;;
+;; THIS FREEZES EMACS IF EMACSWIKI IS DONW!!!!!! (not necessary)
+;; (auto-install-update-emacswiki-package-name t)
