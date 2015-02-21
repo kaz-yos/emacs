0d0654acc6d9b5eb56eed0a770f1410aa6cd0d71
kaz-yos <kazky@mac.com>
2015-02-20 22:00:08 -0500

    Add change to autosave config

--- init.d/500_autosave-undo-related.el
+++ init.d/500_autosave-undo-related.el
@@ -1,18 +1,14 @@
 ;;; Auto-saving buffers
 
 ;;;
-;;; auto-save-buffers
-;; auto-save-buffers: Never lose your data
-;; http://0xcc.net/misc/auto-save/
-;; http://homepage3.nifty.com/oatu/emacs/archives/auto-save-buffers.el
-;; (require 'auto-save-buffers)
-;; (run-with-idle-timer 1 t 'auto-save-buffers)	; auto-save if idle for 1 sec
-
-
-;;;
 ;;; auto-save-buffers-enhanced.el
+;; 2015-02-20 freezing problem solved by turning off and then back on
+;;
+;; https://github.com/kentaro/auto-save-buffers-enhanced
+;; http://rubikitch.com/2014/11/23/auto-save-buffers-enhanced/
 ;; http://qiita.com/ongaeshi/items/8cbd8d3c792476c59a11
 ;; http://blog.sanojimaru.com/post/20090254216/emacs
+;;
 (require 'auto-save-buffers-enhanced)
 ;; Only in Git, CVS, or Subversion directories
 ;; (auto-save-buffers-enhanced-include-only-checkout-path t)
@@ -24,6 +20,16 @@
 (auto-save-buffers-enhanced t)
 ;; (setq auto-save-buffers-enhanced-include-regexps '(".+"))
 ;; (setq auto-save-buffers-enhanced-exclude-regexps '("^not-save-file" "\\.ignore$"))
+
+
+;; ;;;
+;; ;;; real-auto-save.el
+;; ;; https://github.com/chillaranand/real-auto-save
+;; ;; http://rubikitch.com/2015/02/03/real-auto-save/
+;; (require 'real-auto-save)
+;; (setq real-auto-save-interval 1)
+;; ;; all files
+;; (add-hook 'find-file-hook 'real-auto-save-mode)
 
 
 ;;;
