9d5ff4c8dc47e172f1e8af9d91178ad6730d5efb
kaz-yos <kazky@mac.com>
2015-03-05 08:58:04 -0500

    Add ddskk

---
+++ init.d/500_japanese-input.el
@@ -0,0 +1,14 @@
+;;; 500_japanese-input.el ---                              -*- lexical-binding: t; -*-
+
+;;;
+;;; Daredevil SKK (DDSKK)
+;; http://openlab.ring.gr.jp/skk/ddskk.html
+;;
+;; Manuals
+;; http://openlab.ring.gr.jp/skk/doc.html
+;;
+;; Dictionaries
+;; http://openlab.ring.gr.jp/skk/dic.html
+;;
+
+(require 'skk)
