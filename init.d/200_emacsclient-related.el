dc49b00105f7238bddb64411b565a59610ceda12
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-07-01 13:09:29 -0400

    Modify .Rnw helm incompatibility handling

--- init.d/200_emacsclient-related.el
+++ init.d/200_emacsclient-related.el
@@ -25,18 +25,29 @@
 ;;
 ;;
 ;;;
-;;; Handle *helm* buffer not found issue 2014-09-24
-;; It is caused by helm--maybe-update-keymap remaining in post-command-hook.
-;; helm--maybe-update-keymap requires *helm* to be present.
-;; It should be remove-hook'ed, but it does not happen when doing *.Rnw editing.
+;;; Handle *helm* buffer not found issue 2014-09-24, 2015-07-01
+;;
+;; Following errors were seen in *Messages* after opening *.Rnw file
+;;
+;; Error in post-command-hook (helm--update-header-line): (error "No buffer named *helm*")
+;; Error in post-command-hook (helm--maybe-update-keymap): (error "No buffer named *helm*")
+;; Error in post-command-hook (helm--update-header-line): (error "No buffer named *helm*")
+;; Error in post-command-hook (helm--maybe-update-keymap): (error "No buffer named *helm*")
+;;
+;; These were caused by helm functions unnecessarily remaining in post-command-hook.
+;; These functions requires *helm* buffer to be present.
+;; They should be remove-hook'ed, but it does not happen when doing *.Rnw editing.
 ;; The main problem is emacsclient hits this error and die.
 ;; Magit uses emacsclient for COMMIT messages, so it does not work.
 ;;
-;; Define a function to remove helm--maybe-update-keymap from post-command-hook
-(defun remove-helm--maybe-update-keymap ()
-  (remove-hook 'post-command-hook 'helm--maybe-update-keymap))
+;; Define a function to remove helm functions from post-command-hook
+(defun remove-helm-functions ()
+  (remove-hook 'post-command-hook 'helm--maybe-update-keymap)
+  ;; 2015-07-01 The following function was also remaining in the hook.
+  (remove-hook 'post-command-hook 'helm--update-header-line))
 ;;
-;; This function should be called before helm--maybe-update-keymap is used.
+;; 2015-07-01
+;; This function itself is not remaining in the post-command-hook?
 ;;
 ;; Candidate hooks for making this happen.
 ;; server-done-hook	Hook run when done editing a buffer for the Emacs server.
@@ -44,10 +55,12 @@
 ;; server-switch-hook	Hook run when switching to a buffer for the Emacs server.
 ;; server-visit-hook	Hook run when visiting a file for the Emacs server.
 ;;
-(add-hook 'server-done-hook	'remove-helm--maybe-update-keymap)
-(add-hook 'server-mode-hook	'remove-helm--maybe-update-keymap)
-(add-hook 'server-switch-hook	'remove-helm--maybe-update-keymap)
-(add-hook 'server-visit-hook	'remove-helm--maybe-update-keymap)
+;; (add-hook 'server-done-hook   'remove-helm--maybe-update-keymap)
+;; (add-hook 'server-mode-hook   'remove-helm--maybe-update-keymap)
+;; (add-hook 'server-switch-hook 'remove-helm--maybe-update-keymap)
+;; (add-hook 'server-visit-hook  'remove-helm--maybe-update-keymap)
 ;;
-;; Solution in general
-(add-hook 'post-command-hook	'remove-helm--maybe-update-keymap)
+;; This hacky universal solution works.
+;; (add-hook 'post-command-hook 'remove-helm-functions)
+;; 2015-07-01 Changed to the following.
+(add-hook 'pre-command-hook 'remove-helm-functions)
