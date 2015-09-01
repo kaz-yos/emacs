f3a4cec257b590f4ae35c392ed49fd69b4ac319b
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-09-01 18:52:35 -0400

    Add buffer-do-shell-command (shell command for current buffer)

--- init.d/500_dired-related.el
+++ init.d/500_dired-related.el
@@ -56,6 +56,72 @@
 (define-key dired-mode-map (kbd "C-c !") 'runner-add-extension)
 
 
+;;; Use runner for current buffer file
+;;
+;; Explanation in dired-x.el
+;; GUESS SHELL COMMAND
+;; Brief Description:
+;;
+;; * `dired-do-shell-command' is bound to `!' by dired.el.
+;;
+;; * `dired-guess-shell-command' provides smarter defaults for
+;;    dired-aux.el's `dired-read-shell-command'.
+;;
+;; * `dired-guess-shell-command' calls `dired-guess-default' with list of
+;;    marked files.
+;;
+;; * Parse `dired-guess-shell-alist-user' and
+;;   `dired-guess-shell-alist-default' (in that order) for the first REGEXP
+;;   that matches the first file in the file list.
+;;
+;; * If the REGEXP matches all the entries of the file list then evaluate
+;;   COMMAND, which is either a string or a Lisp expression returning a
+;;   string.  COMMAND may be a list of commands.
+;;
+;; * Return this command to `dired-guess-shell-command' which prompts user
+;;   with it.  The list of commands is put into the list of default values.
+;;   If a command is used successfully then it is stored permanently in
+;;   `dired-shell-command-history'.
+;;
+(defun buffer-do-shell-command (command &optional arg file-list)
+  "Run a shell command COMMAND on the current
+
+Modifed version of dired-do-shell-command in dired-aux.el
+Instead of obtaining file names from dired, gets a file name
+from the current buffer."
+  (interactive
+   (let ((files `(,buffer-file-name)))
+     (list
+      ;; Want to give feedback whether this file or marked files are used:
+      (dired-read-shell-command "! on %s: " current-prefix-arg files)
+      current-prefix-arg
+      files)))
+  (let* ((on-each (not (string-match-p dired-star-subst-regexp command)))
+	 (no-subst (not (string-match-p dired-quark-subst-regexp command)))
+	 (star (string-match-p "\\*" command))
+	 (qmark (string-match-p "\\?" command)))
+    ;; Get confirmation for wildcards that may have been meant
+    ;; to control substitution of a file name or the file name list.
+    (if (cond ((not (or on-each no-subst))
+	       (error "You can not combine `*' and `?' substitution marks"))
+	      ((and star on-each)
+	       (y-or-n-p "Confirm--do you mean to use `*' as a wildcard? "))
+	      ((and qmark no-subst)
+	       (y-or-n-p "Confirm--do you mean to use `?' as a wildcard? "))
+	      (t))
+	(if on-each
+	    (dired-bunch-files
+	     (- 10000 (length command))
+	     (function (lambda (&rest files)
+			 (dired-run-shell-command
+			  (dired-shell-stuff-it command files t arg))))
+	     nil
+	     file-list)
+	  ;; execute the shell command
+	  (dired-run-shell-command
+	   (dired-shell-stuff-it command file-list nil arg))))))
+
+
 ;;; stripe-buffer.el
 ;; Turned off. Need decent color settings
 ;; https://github.com/sabof/stripe-buffer
