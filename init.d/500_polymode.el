ea2455d3f4988865f0a6253a2419e81797f5f480
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-12-07 23:08:23 -0500

    Use polymode for Rnw and md

--- init.d/500_polymode.el
+++ init.d/500_polymode.el
@@ -8,11 +8,11 @@
 (require 'poly-markdown)
 ;;
 ;;; MARKDOWN
-;; (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
 ;;
 ;;; R modes
 ;; (add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
-;; (add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
 (add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
 ;;
 ;; key config
