d3a13afb20855da00ae6246e98d915393cee4b04
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-12-25 15:45:02 -0500

    Add jump-back!

--- init.d/500_autosave-undo-related.el
+++ init.d/500_autosave-undo-related.el
@@ -46,14 +46,48 @@
 ;;;
 ;;; Cursor move undo
 ;;
-;; point-undo.el
+;;; point-undo.el
 ;; http://d.hatena.ne.jp/rubikitch/20081230/pointundo
-(require 'point-undo)
-(define-key global-map (kbd "s-p") 'point-undo)
-(define-key global-map (kbd "s-n") 'point-redo)
+;; (require 'point-undo)
+;; (define-key global-map (kbd "s-p") 'point-undo)
+;; (define-key global-map (kbd "s-n") 'point-redo)
 ;;
-;; goto-chg.el
+;;; goto-chg.el
 ;; http://www.emacswiki.org/emacs/GotoChg
 (require 'goto-chg)
-(global-set-key (kbd "C-s-p") 'goto-last-change)
-(global-set-key (kbd "C-s-n") 'goto-last-change-reverse)
+(global-set-key (kbd "A-M-p") 'goto-last-change)
+(global-set-key (kbd "A-M-n") 'goto-last-change-reverse)
+;;
+;;
+;;; Extended point-undo.el
+;; http://qiita.com/zk_phi/items/c145b7bd8077b8a0f537
+(require 'ring)
+(require 'edmacro)
+;;
+(defvar-local jump-back!--marker-ring nil)
+;;
+(defun jump-back!--ring-update ()
+  (let ((marker (point-marker)))
+    (unless jump-back!--marker-ring
+      (setq jump-back!--marker-ring (make-ring 30)))
+    (ring-insert jump-back!--marker-ring marker)))
+;;
+(run-with-idle-timer 1 t 'jump-back!--ring-update)
+;;
+(defun jump-back! ()
+  (interactive)
+  (if (ring-empty-p jump-back!--marker-ring)
+      (error "No further undo information")
+    (let ((marker (ring-ref jump-back!--marker-ring 0))
+          (repeat-key (vector last-input-event)))
+      (ring-remove jump-back!--marker-ring 0)
+      (if (= (point-marker) marker)
+          (jump-back!)
+        (goto-char marker)
+        (message "(Type %s to repeat)" (edmacro-format-keys repeat-key))
+        (set-temporary-overlay-map
+         (let ((km (make-sparse-keymap)))
+           (define-key km repeat-key 'jump-back!)
+           km))))))
+;;
+(global-set-key (kbd "s-p") 'jump-back!)
