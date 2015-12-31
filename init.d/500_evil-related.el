7fda21424268503aed589f7443fc3d1bed3e4944
kaz-yos <kazukiyoshida@mail.harvard.edu>
2015-12-30 23:19:21 -0500

    use-package evil

--- init.d/500_evil-related.el
+++ init.d/500_evil-related.el
@@ -1,32 +1,27 @@
 ;;; EVIL AND VIM-LIKE CONFIGURATIONS
-
-;;;
-;;; evil.el
-;; http://www.emacswiki.org/emacs/Evil
-;; Change toggle key from C-z to s-e to avoid helm.el conflict
-(setq evil-toggle-key "s-e")
-;; Activate
-(require 'evil)
-;; (evil-mode 1)
-;; 
-;;; Making the most of RET and SPC
-;; Keep RET and SPC bindings.
-;; http://www.emacswiki.org/emacs/Evil
-(defun my-move-key (keymap-from keymap-to key)
-  "Moves key binding from one keymap to another, deleting from the old location. "
-  (define-key keymap-to key (lookup-key keymap-from key))
-  (define-key keymap-from key nil))
-(my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
-(my-move-key evil-motion-state-map evil-normal-state-map " ")
-;;
-;;; Further config
-;; http://qureana.hatenadiary.com/entry/2013/06/15/204147
-;; http://d.hatena.ne.jp/tarao/20130303/evil_intro
-;; Make insert state completely emacs
-(setcdr evil-insert-state-map nil)
-
-;;;
-;;; evil-surround.el
-;; https://github.com/timcharper/evil-surround
-(require 'evil-surround)
-(global-evil-surround-mode 1)
+(use-package evil
+  ;; http://www.emacswiki.org/emacs/Evil
+  :commands (evil-mode)
+  :config
+  ;; Change toggle key from C-z to s-e to avoid helm.el conflict
+  (setq evil-toggle-key "s-e")
+  ;;
+  ;;; Making the most of RET and SPC
+  ;; Keep RET and SPC bindings.
+  ;; http://www.emacswiki.org/emacs/Evil
+  (defun my-move-key (keymap-from keymap-to key)
+    "Moves key binding from one keymap to another, deleting from the old location. "
+    (define-key keymap-to key (lookup-key keymap-from key))
+    (define-key keymap-from key nil))
+  (my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
+  (my-move-key evil-motion-state-map evil-normal-state-map " ")
+  ;;
+  ;; http://qureana.hatenadiary.com/entry/2013/06/15/204147
+  ;; http://d.hatena.ne.jp/tarao/20130303/evil_intro
+  ;; Make insert state completely emacs
+  (setcdr evil-insert-state-map nil)
+  ;;
+  ;;; evil-surround.el
+  ;; https://github.com/timcharper/evil-surround
+  (when (require 'evil-surround nil 'noerror)
+    (global-evil-surround-mode 1)))
