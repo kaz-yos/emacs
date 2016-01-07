c6623e1459298ee71f5cd0401c3c17e59bb9dba5
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-06 21:18:23 -0500

    use-package flycheck

--- init.d/500_lang-syntax-checker-related.el
+++ init.d/500_lang-syntax-checker-related.el
@@ -7,12 +7,10 @@
 ;; http://flycheck.readthedocs.org/en/latest/guide/languages.html#supported-languages
 ;; 3rd party extensions
 ;; http://flycheck.readthedocs.org/en/latest/guide/introduction.html#rd-party-extensions
-(require 'flycheck)
-;;
-;; Global check
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
-;;
-;;
-;;; flycheck-pos-tip.el
-;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b#2-5
-(require 'flycheck-pos-tip)
+(use-package flycheck
+  :commands (flycheck-mode)
+  :config
+  ;;; flycheck-pos-tip.el
+  ;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b#2-5
+  (require 'flycheck-pos-tip)
+  (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages) )
