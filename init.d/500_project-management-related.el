af44fcb164d073edfd2c852334607ab294f45c04
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-06 22:55:07 -0500

    use-package projectile

--- init.d/500_project-management-related.el
+++ init.d/500_project-management-related.el
@@ -1,41 +1,45 @@
 ;;; projectile.el		; Project Interaction Library for Emacs
 ;; https://github.com/bbatsov/projectile
 ;; https://github.com/bbatsov/projectile#interactive-commands
-(require 'projectile)
-;; Enable Projectile globally
-;; 2015-01-01 error *** Eval error ***  End of file during parsing
-;; 2015-01-11 Solved by deleting .emacs.d/projectile-bookmarks.eld
-(projectile-global-mode)
+(use-package projectile
+  :config
+  ;; Simpler lighter
+  (setq projectile-mode-line '(:eval
+                               (format " Pj[%s]"
+                                       (projectile-project-name))))
+  ;; Enable Projectile globally
+  ;; 2015-01-01 error *** Eval error ***  End of file during parsing
+  ;; 2015-01-11 Solved by deleting .emacs.d/projectile-bookmarks.eld
+  (projectile-global-mode)
+  ;; C-c p s should always work even before entering into a project
+  (global-set-key (kbd "C-c p s") 'projectile-switch-project)
+  ;; Indexing
+  ;; https://github.com/bbatsov/projectile#indexing-method
+  ;; (setq projectile-indexing-method 'native)
+  ;; Caching
+  ;; https://github.com/bbatsov/projectile#caching
+  (setq projectile-enable-caching t)
+  ;; No ido use for completion (icicle can be used this way)
+  (setq projectile-completion-system 'default)
+  ;; Switching
+  ;; https://github.com/bbatsov/projectile#switching-projects
+  ;; (setq projectile-switch-project-action 'projectile-find-file)	; default
+  ;; (setq projectile-switch-project-action 'helm-projectile)
+  (setq projectile-switch-project-action 'projectile-dired)
+  (setq projectile-find-dir-includes-top-level t))
+
 ;;
-;; Simpler lighter
-(setq projectile-mode-line '(:eval
-			     (format " Pj[%s]"
-				     (projectile-project-name))))
-;;
-;; C-c p s should always work even before entering into a project
-(global-set-key (kbd "C-c p s") 'projectile-switch-project)
-;; Indexing
-;; https://github.com/bbatsov/projectile#indexing-method
-;; (setq projectile-indexing-method 'native)
-;; Caching
-;; https://github.com/bbatsov/projectile#caching
-(setq projectile-enable-caching t)
-;; No ido use for completion (icicle can be used this way)
-(setq projectile-completion-system 'default)
-;; Switching
-;; https://github.com/bbatsov/projectile#switching-projects
-;; (setq projectile-switch-project-action 'projectile-find-file)	; default
-;; (setq projectile-switch-project-action 'helm-projectile)
-(setq projectile-switch-project-action 'projectile-dired)
-(setq projectile-find-dir-includes-top-level t)
 ;;
 ;;
 ;;; helm-projectile.el		; Helm plugin for projectile
 ;; http://tuhdo.github.io/helm-projectile.html
-(require 'helm-projectile)
-(global-set-key (kbd "C-M-z") 'helm-projectile)
+(use-package helm-projectile
+  :commands (helm-projectile)
+  :bind ("C-M-z" . helm-projectile))
+
 ;;
 ;;
 ;;; helm-projectile-all.el
 ;; https://github.com/spatz/helm-projectile-all
-(require 'helm-projectile-all)
+(use-package helm-projectile-all
+  :commands (helm-projectile-all))
