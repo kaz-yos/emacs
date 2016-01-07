f533a85a1e186b59aa30fa7202662521cc295afc
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-06 21:31:42 -0500

    use-package msic

--- init.d/500_misc-packages.el
+++ init.d/500_misc-packages.el
@@ -1,21 +1,25 @@
 ;;;
 ;;; minor-mode-hack.el
 ;; M-x show-minor-mode-map-priority to see minor mode priority
-(require 'minor-mode-hack)
+(use-package minor-mode-hack
+  :commands (show-minor-mode-map-priority))
 
 ;;;
 ;;; show-keys.el	; Show keys in a buffer as keys are typed.
 ;; http://www.youtube.com/watch?v=0cZ7szFuz18
 ;; https://github.com/AndreaCrotti/minimal-emacs-configuration
-(require 'show-keys)
+(use-package show-keys
+  :commands (show-keys))
 
 ;;;
 ;;; manage-minor-mode.el
 ;; http://fukuyama.co/manage-minor-mode
-(require 'manage-minor-mode)
+(use-package manage-minor-mode
+  :commands (manage-minor-mode))
 
 
 ;;;
 ;;; csv-mode.el
 ;; http://emacswiki.org/emacs/CsvMode
-(require 'csv-mode)
+(use-package csv-mode
+  :mode ("\\.csv" . csv-mod))
