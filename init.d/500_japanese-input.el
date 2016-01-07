e6c6db2d62a2ef4fdb79763a8cba50dcbd160392
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-06 20:45:56 -0500

    use-package skk

--- init.d/500_japanese-input.el
+++ init.d/500_japanese-input.el
@@ -11,22 +11,16 @@
 ;; Dictionaries
 ;; http://openlab.ring.gr.jp/skk/dic.html
 ;;
-(require 'skk)
-;;
-(global-set-key (kbd "s-j") 'skk-mode)
-(global-set-key (kbd "A-j") 'skk-mode)
-(global-set-key (kbd "M-j") 'skk-mode)
-;;
-;; Dictionaries
-;; http://openlab.ring.gr.jp/skk/wiki/wiki.cgi?page=SKK%BC%AD%BD%F1
-;; http://openlab.ring.gr.jp/skk/skk-manual-git/Zui-moJi-Ben-De-naShe-Ding-.html#g_t_6700_3082_57fa_672c_7684_306a_8a2d_5b9a
-(setq skk-large-jisyo     "~/.emacs.d/skk/SKK-JISYO.L")
-(setq skk-cdb-large-jisyo "~/.emacs.d/skk/SKK-JISYO.L.cdb")
-
-
-
+(use-package skk
+  :bind ("A-j" . skk-mode)
+  ;;
+  :config
+  ;; Dictionaries
+  ;; http://openlab.ring.gr.jp/skk/wiki/wiki.cgi?page=SKK%BC%AD%BD%F1
+  ;; http://openlab.ring.gr.jp/skk/skk-manual-git/Zui-moJi-Ben-De-naShe-Ding-.html#g_t_6700_3082_57fa_672c_7684_306a_8a2d_5b9a
+  (setq skk-large-jisyo     "~/.emacs.d/skk/SKK-JISYO.L")
+  (setq skk-cdb-large-jisyo "~/.emacs.d/skk/SKK-JISYO.L.cdb")
 ;;;
 ;;; ac-skk.el
-;; https://github.com/myuhe/ac-skk.el
-;;
-(require 'ac-skk)
+  ;; https://github.com/myuhe/ac-skk.el
+  (require 'ac-skk))
