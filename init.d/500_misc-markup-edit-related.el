08e076426876edefec57fb9bc84bb2f00facf3d1
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-06 21:31:25 -0500

    use-package misc markup

--- init.d/500_misc-markup-edit-related.el
+++ init.d/500_misc-markup-edit-related.el
@@ -1,23 +1,21 @@
 ;;; Markdown mode
 ;; http://jblevins.org/projects/markdown-mode/
 ;;
-(autoload 'markdown-mode "markdown-mode.el"
-  "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
-;;
-;;
+(use-package markdown-mode
+  :mode ("\\.md" . markdown-mode)
+  :config
 ;;; Open current file in Atom for real-time preview
-;; http://qiita.com/takuma510/items/77489cf538580dfcc41d
-(defun open-atom ()
-  (interactive)
-  (call-process
-   "atom" nil nil nil buffer-file-name))
+  ;; http://qiita.com/takuma510/items/77489cf538580dfcc41d
+  (defun open-atom ()
+    (interactive)
+    (call-process
+     "atom" nil nil nil buffer-file-name)))
 
 
 ;;; Graphviz mode
 ;; http://ppareit.github.io/graphviz-dot-mode/
-(require 'graphviz-dot-mode)
+(use-package graphviz-dot-mode
+  :mode ("\\.dot" . graphviz-dot-mode))
 ;; Font locking is automatic, indentation uses the same commands as
 ;; other modes, tab, M-j and C-M-q.  Insertion of comments uses the
 ;; same commands as other modes, M-; .  You can compile a file using
