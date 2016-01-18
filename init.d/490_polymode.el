91178b579416715d1ce813bb88beeff2767a4cb8
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-17 20:46:13 -0500

    Drop Rmd from polymode

--- init.d/490_polymode.el
+++ init.d/490_polymode.el
@@ -9,7 +9,8 @@
   ;; These mode association is overwritten by ess-site
   ;; So include the same thing in ess-site :mode
   :mode (("\\.Rnw" . poly-noweb+r-mode)
-         ("\\.Rmd" . poly-markdown+r-mode))
+         ;; ("\\.Rmd" . poly-markdown+r-mode)
+         )
   :config
   ;; Key config
   (bind-key "C-c n" 'polymode-next-chunk-same-type     polymode-mode-map)
