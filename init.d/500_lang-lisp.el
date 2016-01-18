af7a55f33f4865d46f24a29113c0ed466c38cde0
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-17 20:42:28 -0500

    Use only company-mode for elisp

--- init.d/500_lang-lisp.el
+++ init.d/500_lang-lisp.el
@@ -40,18 +40,18 @@
 ;;
 ;;; Auto-complete in IELM
 ;; http://www.masteringemacs.org/articles/2010/11/29/evaluating-elisp-emacs/
-(defun ielm-auto-complete ()
-  "Enables `auto-complete' support in \\[ielm]."
-  (setq ac-sources '(ac-source-functions
-                     ac-source-variables
-                     ac-source-features
-                     ac-source-symbols
-                     ac-source-words-in-same-mode-buffers))
-  (add-to-list 'ac-modes 'inferior-emacs-lisp-mode)
-  (auto-complete-mode 1))
+;; (defun ielm-auto-complete ()
+;;   "Enables `auto-complete' support in \\[ielm]."
+;;   (setq ac-sources '(ac-source-functions
+;;                      ac-source-variables
+;;                      ac-source-features
+;;                      ac-source-symbols
+;;                      ac-source-words-in-same-mode-buffers))
+;;   (add-to-list 'ac-modes 'inferior-emacs-lisp-mode)
+;;   (auto-complete-mode 1))
 ;;
-(add-hook 'ielm-mode-hook 'ielm-auto-complete)
-(add-hook 'emacs-lisp-mode-hook 'ielm-auto-complete)
+;; (add-hook 'ielm-mode-hook 'ielm-auto-complete)
+;; (add-hook 'emacs-lisp-mode-hook 'ielm-auto-complete)
 ;;
 
 
