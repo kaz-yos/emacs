60dc34d6b45d8003b9dd9e337791e270a1ad0383
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-20 23:07:18 -0500

    Clean mode-line configuration

--- init.d/500_mode-line-related.el
+++ init.d/500_mode-line-related.el
@@ -5,30 +5,32 @@
 ;;; powerline.el
 ;; https://github.com/milkypostman/powerline
 ;; http://shibayu36.hatenablog.com/entry/2014/02/11/160945
-(require 'powerline)
-;;
-;; https://github.com/shibayu36/emacs/blob/8376ba4e4b4d5cb4668e848c9a494b287232ea6f/emacs.d/inits/01-mode-line.el
-(set-face-attribute 'mode-line nil
-                    :foreground "black"
-                    :background "gray90"
-                    :box nil)
-(set-face-attribute 'powerline-active1 nil
-                    :foreground "black"
-                    :background "gray80"
-                    :inherit 'mode-line)
-(set-face-attribute 'powerline-active2 nil
-                    :foreground "black"
-                    :background "gray70"
-                    :inherit 'mode-line)
-;;
-;; Configure at the end
-(powerline-default-theme)
-;;
+(use-package powerline
+  :config
+  ;; https://github.com/shibayu36/emacs/blob/8376ba4e4b4d5cb4668e848c9a494b287232ea6f/emacs.d/inits/01-mode-line.el
+  (set-face-attribute 'mode-line nil
+                      :foreground "black"
+                      :background "gray90"
+                      :box nil)
+  (set-face-attribute 'powerline-active1 nil
+                      :foreground "black"
+                      :background "gray80"
+                      :inherit 'mode-line)
+  (set-face-attribute 'powerline-active2 nil
+                      :foreground "black"
+                      :background "gray70"
+                      :inherit 'mode-line)
+  ;; Configure at the end
+  (powerline-default-theme))
+
+
+
+;; Clean mode-line when switching major modes
 ;; https://github.com/shibayu36/emacs/blob/8376ba4e4b4d5cb4668e848c9a494b287232ea6f/emacs.d/inits/01-mode-line.el
 (defvar mode-line-cleaner-alist
   '(;; For minor-mode, first char is 'space'
-    (flymake-mode           . " Fm")
-    (paredit-mode           . " pr")
+    (flymake-mode           . "")
+    (paredit-mode           . "")
     (eldoc-mode             . "")
     (abbrev-mode            . "")
     (undo-tree-mode         . "")
@@ -42,8 +44,7 @@
     ;;
     ;; Major modes
     (dired-mode             . "Dir")
-    (emacs-lisp-mode        . "el")
-    ))
+    (emacs-lisp-mode        . "elisp")))
 ;;
 (defun clean-mode-line ()
   (interactive)
