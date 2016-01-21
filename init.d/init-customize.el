251f5c39e9f6ed5dd667d1f38a39409dc2b533ae
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-20 22:43:03 -0500

    Drop all configuration from custom-set-variables

--- init.d/init-customize.el
+++ init.d/init-customize.el
@@ -3,60 +3,7 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(Linum-format "%7i ")
- '(ac-ispell-requires 4)
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(background-color "#042028")
- '(background-mode dark)
- '(coffee-args-compile (quote ("-c" "--bare")))
- '(coffee-tab-width 2)
- '(cursor-color "#708183")
- '(custom-enabled-themes (quote (modified-manoj-dark-theme)))
- '(custom-safe-themes
-   (quote
-    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4b2f6ec6f2fd05594a2bafc9c4f7223d0d23289ee9fdcce2abea0e7fc5e5216e" default)))
- '(fci-rule-character-color "#202020")
- '(fci-rule-color "#2D2D2D")
- '(foreground-color "#708183")
- '(frame-brackground-mode (quote dark))
- '(fringe-mode 4 nil (fringe))
- '(global-anzu-mode t)
- '(haskell-mode-hook
-   (quote
-    (turn-on-eldoc-mode turn-on-haskell-doc turn-on-haskell-indent)) t)
- '(linum-format " %7i ")
- '(magit-diff-options nil)
- '(main-line-color1 "#1E1E1E")
- '(main-line-color2 "#111111")
- '(main-line-separator-style (quote chamfer))
- '(powerline-color1 "#1E1E1E")
- '(powerline-color2 "#111111")
- '(vc-annotate-background "#202020")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#C99090")
-     (40 . "#D9A0A0")
-     (60 . "#ECBC9C")
-     (80 . "#DDCC9C")
-     (100 . "#EDDCAC")
-     (120 . "#FDECBC")
-     (140 . "#6C8C6C")
-     (160 . "#8CAC8C")
-     (180 . "#9CBF9C")
-     (200 . "#ACD2AC")
-     (220 . "#BCE5BC")
-     (240 . "#CCF8CC")
-     (260 . "#A0EDF0")
-     (280 . "#79ADB0")
-     (300 . "#89C5C8")
-     (320 . "#99DDE0")
-     (340 . "#9CC7FB")
-     (360 . "#E090C7"))))
- '(vc-annotate-very-old-color "#E090C7"))
+ )
 ;;
 ;;
 (custom-set-faces
