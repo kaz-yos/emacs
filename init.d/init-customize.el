fa774be5fdd6e4315f9cfbac23f2f839fefed7b8
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-01-20 22:44:15 -0500

    Enable modified-manoj-dark-theme

--- init.d/init-customize.el
+++ init.d/init-customize.el
@@ -3,7 +3,11 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- )
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (modified-manoj-dark-theme)))
+ '(custom-safe-themes
+   (quote
+    ("4b2f6ec6f2fd05594a2bafc9c4f7223d0d23289ee9fdcce2abea0e7fc5e5216e" default))))
 ;;
 ;;
 (custom-set-faces
