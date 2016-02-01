87b92bb774465b85bffd83a2d42e930ac7c74820
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-02-01 18:19:18 -0500

    Do not package-initialize in init.el

--- init.el
+++ init.el
@@ -10,6 +10,13 @@
 ;;
 ;; http://emacswiki.org/emacs/LoadPath	; Recursive for elpa directory
 ;; This is NECESSARY if init-loader.el is required before package.el package-initialize.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (let ((default-directory "~/.emacs.d/elpa/"))
        (normal-top-level-add-subdirs-to-load-path))
 ;;
