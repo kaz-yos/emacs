762826f7de0627f06523aea17515dc8d874738cf
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-02-01 19:42:39 -0500

    Minimize init-customize.el by dropping new entries

--- init.d/init-customize.el
+++ init.d/init-customize.el
@@ -4,38 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#26292c" "#ff4a52" "#40b83e" "#f6f080" "#afc4db" "#dc8cc3" "#93e0e3" "#f8f8f8"])
  '(custom-enabled-themes (quote (manoj-dark-modified)))
  '(custom-safe-themes
    (quote
-    ("71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "9122dfb203945f6e84b0de66d11a97de6c9edf28b3b5db772472e4beccc6b3c5" "038e9891d071ca4dfc55775b15534f8be4979866dd5c063c801ec9717a5cd84b" default)))
- '(fci-rule-color "#202325")
+    ("038e9891d071ca4dfc55775b15534f8be4979866dd5c063c801ec9717a5cd84b" default)))
  '(package-selected-packages
    (quote
-    (elscreen zotelo windresize window-number window-layout wgrep-helm wgrep-ag vlf virtualenv viewer use-package tuareg tempbuf stan-mode ssh sql-indent sml-mode smartrep smartparens smart-mode-line shell-command sequential-command runner reveal-in-osx-finder redshank recentf-ext real-auto-save rainbow-mode rainbow-delimiters racket-mode pungi powerline polymode point-undo phi-search paredit-menu origami open-junk-file nose noflet multiple-cursors multi-term moz-controller minor-mode-hack markdown-mode manage-minor-mode magit log4e latex-math-preview latest-clojure-libraries key-chord json-rpc js3-mode js-comint init-loader inflections iedit idomenu ido-vertical-mode ido-ubiquitous icicles ibuffer-git hydra hy-mode highlight-symbol highlight-indentation highlight hexrgb helm-swoop helm-projectile-all helm-projectile helm-open-github helm-mode-manager helm-migemo helm-descbinds helm-dash helm-c-yasnippet helm-c-moccur helm-ag guide-key graphviz-dot-mode gntp github-browse-file git-timemachine git-gutter-fringe+ ghc fuzzy fold-this fold-dwim flyspell-popup flymake-shell flycheck-pos-tip flx-ido find-file-in-project expand-region exec-path-from-shell evil-surround evil ess-R-object-popup es-windows es-lib erlang ensime elisp-slime-nav electric-spacing eldoc-extension ein egison-mode edn dired-subtree dired+ csv-mode crosshairs company-try-hard company-statistics company-quickhelp company-math company-auctex company-anaconda column-marker coffee-mode clojure-test-mode clojure-snippets clojure-quick-repls clojure-cheatsheet cider-tracing cider-spy cider-profile bm beacon bash-completion auto-save-buffers-enhanced auto-install auto-complete-auctex auto-async-byte-compile auctex-latexmk anzu anaphora ag ace-window ace-jump-mode ace-jump-helm-line ac-slime ac-skk ac-math ac-js2 ac-ispell ac-inf-ruby ac-geiser ac-cider)))
- '(vc-annotate-background "#1f2124")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#ff0000")
-     (40 . "#ff4a52")
-     (60 . "#f6aa11")
-     (80 . "#f1e94b")
-     (100 . "#f5f080")
-     (120 . "#f6f080")
-     (140 . "#41a83e")
-     (160 . "#40b83e")
-     (180 . "#b6d877")
-     (200 . "#b7d877")
-     (220 . "#b8d977")
-     (240 . "#b9d977")
-     (260 . "#93e0e3")
-     (280 . "#72aaca")
-     (300 . "#8996a8")
-     (320 . "#afc4db")
-     (340 . "#cfe2f2")
-     (360 . "#dc8cc3"))))
- '(vc-annotate-very-old-color "#dc8cc3"))
+    (elscreen zotelo windresize window-number window-layout wgrep-helm wgrep-ag vlf virtualenv viewer use-package tuareg tempbuf stan-mode ssh sql-indent sml-mode smartrep smartparens smart-mode-line shell-command sequential-command runner reveal-in-osx-finder redshank recentf-ext real-auto-save rainbow-mode rainbow-delimiters racket-mode pungi powerline polymode point-undo phi-search paredit-menu origami open-junk-file nose noflet multiple-cursors multi-term moz-controller minor-mode-hack markdown-mode manage-minor-mode magit log4e latex-math-preview latest-clojure-libraries key-chord json-rpc js3-mode js-comint init-loader inflections iedit idomenu ido-vertical-mode ido-ubiquitous icicles ibuffer-git hydra hy-mode highlight-symbol highlight-indentation highlight hexrgb helm-swoop helm-projectile-all helm-projectile helm-open-github helm-mode-manager helm-migemo helm-descbinds helm-dash helm-c-yasnippet helm-c-moccur helm-ag guide-key graphviz-dot-mode gntp github-browse-file git-timemachine git-gutter-fringe+ ghc fuzzy fold-this fold-dwim flyspell-popup flymake-shell flycheck-pos-tip flx-ido find-file-in-project expand-region exec-path-from-shell evil-surround evil ess-R-object-popup es-windows es-lib erlang ensime elisp-slime-nav electric-spacing eldoc-extension ein egison-mode edn dired-subtree dired+ csv-mode crosshairs company-try-hard company-statistics company-quickhelp company-math company-auctex company-anaconda column-marker coffee-mode clojure-test-mode clojure-snippets clojure-quick-repls clojure-cheatsheet cider-tracing cider-spy cider-profile bm beacon bash-completion auto-save-buffers-enhanced auto-install auto-complete-auctex auto-async-byte-compile auctex-latexmk anzu anaphora ag ace-window ace-jump-mode ace-jump-helm-line ac-slime ac-skk ac-math ac-js2 ac-ispell ac-inf-ruby ac-geiser ac-cider))))
 ;;
 ;;
 (custom-set-faces
