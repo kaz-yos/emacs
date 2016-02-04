7f9e3917a9aff79064f9223eae6fd0a0d85afa87
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-02-04 07:39:37 -0500

    Add mu and offlineimap installation log

--- init.d/500_email.el
+++ init.d/500_email.el
@@ -11,3 +11,22 @@
 ;; https://gist.github.com/areina/3879626
 ;; http://blog.danielgempesaw.com/post/43467552978/installing-mu-and-mu4e-with-homebrew-with-emacs
 ;; installing mu and mu4e with homebrew with emacs from emacsforosx
+;;
+;; Install infrastructure
+;;
+;; mu along with mu4e
+;; make sure (which emacs gives the path to a new emacs (23+)
+;; https://github.com/Homebrew/homebrew/issues/16504#issuecomment-11394215
+;; EMACS=$(which emacs) brew install mu --with-emacs --HEAD
+;; ./configure --prefix=/usr/local/Cellar/mu/HEAD --with-lispdir=/usr/local/Cellar/mu/HEAD/share/emacs/site-lisp/mu
+;;
+;; offlineimap
+;; brew install offlineimap
+;; * minimal configuration:
+;;     cp -n /usr/local/etc/offlineimap.conf.minimal ~/.offlineimaprc
+;; * advanced configuration:
+;;     cp -n /usr/local/etc/offlineimap.conf ~/.offlineimaprc
+;; To have launchd start offlineimap at login:
+;;   ln -sfv /usr/local/opt/offlineimap/*.plist ~/Library/LaunchAgents
+;; Then to load offlineimap now:
+;;   launchctl load ~/Library/LaunchAgents/homebrew.mxcl.offlineimap.plist
