ab77ff4b4ebdac9f04c5d938090f4d315fd6c3c3
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-02-04 06:43:59 -0500

    Add email-related configuration file

---
+++ init.d/500_email.el
@@ -0,0 +1,13 @@
+;;; e-mail related                                 -*- lexical-binding: t; -*-
+
+
+;;; mu4e-related
+;; http://www.djcbsoftware.nl/code/mu/mu4e.html
+;; https://www.emacswiki.org/emacs/mu4e
+;;
+;; E-mail in Emacs with mu4e on OS X
+;; http://cmacr.ae/blog/2015/01/25/email-in-emacs/
+;; Manage your gmail account in emacs with mu4e
+;; https://gist.github.com/areina/3879626
+;; http://blog.danielgempesaw.com/post/43467552978/installing-mu-and-mu4e-with-homebrew-with-emacs
+;; installing mu and mu4e with homebrew with emacs from emacsforosx
