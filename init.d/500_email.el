fa4efd42d51b541a04d104a0f161df932e5063bc
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-02-04 22:39:45 -0500

    Add gnus simple gmail configuration

--- init.d/500_email.el
+++ init.d/500_email.el
@@ -44,3 +44,16 @@
   :disabled t
   :config
   )
+
+
+;;; GNUS-RELATED
+;;; gnus
+;; http://www.emacswiki.org/emacs/GnusGmail
+(setq gnus-select-method
+      '(nnimap "gmail"
+	       (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
+	       (nnimap-server-port "imaps")
+	       (nnimap-stream ssl)))
+
+(setq smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
