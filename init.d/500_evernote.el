42e053ccfbbaeca5b91dd8b60c48eab769358d63
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-02-20 23:16:32 -0500

    Add geeknote configuration

---
+++ init.d/500_evernote.el
@@ -0,0 +1,12 @@
+;;; Evernote-related -*- lexical-binding: t; -*-
+
+;;; geeknote.el
+;; https://github.com/avendael/emacs-geeknote
+;;
+;; Install geeknote command-line tool first
+;; https://github.com/VitaliyRodnenko/geeknote
+;; Problem with Python 3.4; Installed with Python2.7
+;; sudo python2.7 setup.py install
+;;
+(use-package geeknote
+  :disabled t)
