21549475f599d36cb2925a08205b0cfe6b524feb
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-02-27 23:10:11 -0500

    Use smartchr not key-combo

--- init.d/500_sequential-keybinding-related.el
+++ init.d/500_sequential-keybinding-related.el
@@ -25,107 +25,110 @@
 ;; 2014-03-30 (mc/prompt-for-inclusion-in-whitelist 'smartchr) did not help.
 ;;
 (use-package smartchr
+  :init
+  ;; ESS
+  (defun my-ess-smartchr-setting ()
+    (local-set-key (kbd "=") (smartchr '("=" " = " " == ")))
+    (local-set-key (kbd "+") (smartchr '("+" " + ")))
+    (local-set-key (kbd "-") (smartchr '("-" " - " "--------------------------------------------------------------------------------")))
+    (local-set-key (kbd "#") (smartchr '("# " "## " "### " "################################################################################")))
+    (local-set-key (kbd "~") (smartchr '("~" " ~ ")))
+    (local-set-key (kbd "$") (smartchr '("$" "$`!!'$")))
+    (local-set-key (kbd "%") (smartchr '("%" " %`!!'% ")))
+    )
+  (add-hook 'ess-mode-hook          'my-ess-smartchr-setting)
+  (add-hook 'inferior-ess-mode-hook 'my-ess-smartchr-setting)
+  ;;
+  ;; Python
+  (defun my-python-smartchr-setting ()
+    (local-set-key (kbd "=") (smartchr '("=" " = " " == ")))
+    (local-set-key (kbd "+") (smartchr '("+" " + ")))
+    (local-set-key (kbd "-") (smartchr '("-" " - ")))
+    (local-set-key (kbd "#") (smartchr '("# " "## " "### " "################################################################################")))
+    )
+  (add-hook 'ein:notebook-multilang-mode-hook 'my-python-smartchr-setting)
+  (add-hook 'python-mode-hook                 'my-python-smartchr-setting)
+  (add-hook 'inferior-python-mode-hook        'my-python-smartchr-setting)
+  ;;
+  ;; SML
+  (defun my-sml-smartchr-setting ()
+    (local-set-key (kbd "=") (smartchr '(" = " " => " "=")))
+    (local-set-key (kbd ":") (smartchr '(" : " "::"))))
+  (add-hook 'sml-mode-hook 'my-sml-smartchr-setting)
+  ;;
+  ;; LaTeX
+  (defun my-LaTeX-smartchr-setting ()
+    (local-set-key (kbd "$") (smartchr '("$`!!'$" "$")))
+    (local-set-key (kbd "%") (smartchr '("% " "%% " "%%% " "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))))
+  (add-hook 'LaTeX-mode-hook 'my-LaTeX-smartchr-setting)
+  ;;
+  ;; Emacs Lisp
+  (defun my-elisp-smartchr-setting ()
+    (local-set-key (kbd ";") (smartchr '("; " ";; " ";;; " ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"))))
+  (add-hook 'emacs-lisp-mode-hook 'my-elisp-smartchr-setting)
+  ;;
+  ;; Haskell
+  (defun my-haskell-smartchr-setting ()
+    (local-set-key (kbd "=") (smartchr '(" = " " == " "=")))
+    (local-set-key (kbd "+") (smartchr '(" + " " ++ " "+")))
+    (local-set-key (kbd "-") (smartchr '(" - " " -> " "-")))
+    (local-set-key (kbd ":") (smartchr '(" : " " :: " ":"))))
+  (add-hook 'haskell-mode-hook 'my-haskell-smartchr-setting)
+  ;;
+  ;; Ruby
+  (defun my-ruby-smartchr-setting ()
+    (local-set-key (kbd "=") (smartchr '(" = " " == " "="))))
+  (add-hook 'ruby-mode-hook 'my-ruby-smartchr-setting)
+  ;;
+  ;; Configuration
   :commands (smartchr))
-;;
-;; ESS
-(defun my-ess-smartchr-setting ()
-  (local-set-key (kbd "=") (smartchr '("=" " = " " == ")))
-  (local-set-key (kbd "+") (smartchr '("+" " + ")))
-  (local-set-key (kbd "-") (smartchr '("-" " - " "--------------------------------------------------------------------------------")))
-  (local-set-key (kbd "#") (smartchr '("# " "## " "### " "################################################################################")))
-  (local-set-key (kbd "~") (smartchr '("~" " ~ ")))
-  (local-set-key (kbd "$") (smartchr '("$" "$`!!'$")))
-  (local-set-key (kbd "%") (smartchr '("%" " %`!!'% ")))
-  )
-(add-hook 'ess-mode-hook          'my-ess-smartchr-setting)
-(add-hook 'inferior-ess-mode-hook 'my-ess-smartchr-setting)
-;;
-;; Python
-(defun my-python-smartchr-setting ()
-  (local-set-key (kbd "=") (smartchr '("=" " = " " == ")))
-  (local-set-key (kbd "+") (smartchr '("+" " + ")))
-  (local-set-key (kbd "-") (smartchr '("-" " - ")))
-  (local-set-key (kbd "#") (smartchr '("# " "## " "### " "################################################################################")))
-  )
-(add-hook 'ein:notebook-multilang-mode-hook 'my-python-smartchr-setting)
-(add-hook 'python-mode-hook                 'my-python-smartchr-setting)
-(add-hook 'inferior-python-mode-hook        'my-python-smartchr-setting)
-;;
-;; SML
-(defun my-sml-smartchr-setting ()
-  (local-set-key (kbd "=") (smartchr '(" = " " => " "=")))
-  (local-set-key (kbd ":") (smartchr '(" : " "::"))))
-(add-hook 'sml-mode-hook 'my-sml-smartchr-setting)
-;;
-;; LaTeX
-(defun my-LaTeX-smartchr-setting ()
-  (local-set-key (kbd "$") (smartchr '("$`!!'$" "$")))
-  (local-set-key (kbd "%") (smartchr '("% " "%% " "%%% " "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))))
-(add-hook 'LaTeX-mode-hook 'my-LaTeX-smartchr-setting)
-;;
-;; Emacs Lisp
-(defun my-elisp-smartchr-setting ()
-  (local-set-key (kbd ";") (smartchr '("; " ";; " ";;; " ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"))))
-(add-hook 'emacs-lisp-mode-hook 'my-elisp-smartchr-setting)
-;;
-;; Haskell
-(defun my-haskell-smartchr-setting ()
-  (local-set-key (kbd "=") (smartchr '(" = " " == " "=")))
-  (local-set-key (kbd "+") (smartchr '(" + " " ++ " "+")))
-  (local-set-key (kbd "-") (smartchr '(" - " " -> " "-")))
-  (local-set-key (kbd ":") (smartchr '(" : " " :: " ":"))))
-(add-hook 'haskell-mode-hook 'my-haskell-smartchr-setting)
-;;
-;; Ruby
-(defun my-ruby-smartchr-setting ()
-  (local-set-key (kbd "=") (smartchr '(" = " " == " "="))))
-(add-hook 'ruby-mode-hook 'my-ruby-smartchr-setting)
 
 
 ;;;
 ;;; key-combo.el
 ;; https://github.com/uk-ar/key-combo
-;; (use-package key-combo
-;;   :config
-;;   ;; ESS
-;;   (setq my-ess-mode-hooks
-;;         '(ess-mode-hook
-;;           inferior-ess-mode-hook))
-;;   (setq my-key-combos-for-ess
-;;         '(("=" . ("=" " = " " == "))
-;;           ("+" . ("+" " + "))
-;;           ("-" . ("-" " - " "--------------------------------------------------------------------------------"))
-;;           ("#" . ("# " "## " "### " "################################################################################"))
-;;           ("~" . ("~" " ~ "))
-;;           ("$" . ("$" "$`!!'$"))
-;;           ("%" . ("%" " %`!!'% "))))
-;;   (key-combo-define-hook my-ess-mode-hooks
-;;                          'my-ess-mode-hooks
-;;                          my-key-combos-for-ess)
-;;   ;;
-;;   ;; Python
-;;   (setq my-python-mode-hooks
-;;         '(ein:notebook-multilang-mode-hook
-;;           python-mode-hook
-;;           inferior-python-mode-hook))
-;;   (setq my-key-combos-for-python
-;;         '(("=" . ("=" " = " " == "))
-;;           ("+" . ("+" " + "))
-;;           ("-" . ("-" " - "))
-;;           ("#" . ("# " "## " "### " "################################################################################"))))
-;;   (key-combo-define-hook my-python-mode-hooks
-;;                          'my-python-mode-hooks
-;;                          my-key-combos-for-python)
-;;   ;;
-;;   ;; LaTeX
-;;   (setq my-latex-mode-hooks
-;;         '(LaTeX-mode-hook))
-;;   (setq my-key-combos-for-latex
-;;         '(("$" . ("$`!!'$" "$"))
-;;           ("%" . ("% " "%% " "%%% " "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))))
-;;   (key-combo-define-hook my-latex-mode-hooks
-;;                          'my-latex-mode-hooks
-;;                          my-key-combos-for-latex))
+(use-package key-combo
+  :disabled t
+  :config
+  ;; ESS
+  (setq my-ess-mode-hooks
+        '(ess-mode-hook
+          inferior-ess-mode-hook))
+  (setq my-key-combos-for-ess
+        '(("=" . ("=" " = " " == "))
+          ("+" . ("+" " + "))
+          ("-" . ("-" " - " "--------------------------------------------------------------------------------"))
+          ("#" . ("# " "## " "### " "################################################################################"))
+          ("~" . ("~" " ~ "))
+          ("$" . ("$" "$`!!'$"))
+          ("%" . ("%" " %`!!'% "))))
+  (key-combo-define-hook my-ess-mode-hooks
+                         'my-ess-mode-hooks
+                         my-key-combos-for-ess)
+  ;;
+  ;; Python
+  (setq my-python-mode-hooks
+        '(ein:notebook-multilang-mode-hook
+          python-mode-hook
+          inferior-python-mode-hook))
+  (setq my-key-combos-for-python
+        '(("=" . ("=" " = " " == "))
+          ("+" . ("+" " + "))
+          ("-" . ("-" " - "))
+          ("#" . ("# " "## " "### " "################################################################################"))))
+  (key-combo-define-hook my-python-mode-hooks
+                         'my-python-mode-hooks
+                         my-key-combos-for-python)
+  ;;
+  ;; LaTeX
+  (setq my-latex-mode-hooks
+        '(LaTeX-mode-hook))
+  (setq my-key-combos-for-latex
+        '(("$" . ("$`!!'$" "$"))
+          ("%" . ("% " "%% " "%%% " "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))))
+  (key-combo-define-hook my-latex-mode-hooks
+                         'my-latex-mode-hooks
+                         my-key-combos-for-latex))
 
 
 ;;;
@@ -137,19 +140,19 @@
   (eval-after-load "org"
     '(progn
        (smartrep-define-key
-        org-mode-map "C-c" '(("C-n" . (lambda ()
-                                        (outline-next-visible-heading 1)))
-                             ("C-p" . (lambda ()
-                                        (outline-previous-visible-heading 1)))))))
+           org-mode-map "C-c" '(("C-n" . (lambda ()
+                                           (outline-next-visible-heading 1)))
+                                ("C-p" . (lambda ()
+                                           (outline-previous-visible-heading 1)))))))
   ;;
   ;; Make C-' a prefix
   (defvar ctl-quote-map (make-keymap))
   (define-key global-map (kbd "C-'") ctl-quote-map)
   ;; Scroll the other window
   (smartrep-define-key
-   global-map "C-'" '(("n" . (lambda () (scroll-other-window 1)))
-                      ("p" . (lambda () (scroll-other-window -1)))
-                      ("N" . 'scroll-other-window)
-                      ("P" . (lambda () (scroll-other-window '-)))
-                      ("a" . (lambda () (beginning-of-buffer-other-window 0)))
-                      ("e" . (lambda () (end-of-buffer-other-window 0))))))
+      global-map "C-'" '(("n" . (lambda () (scroll-other-window 1)))
+                         ("p" . (lambda () (scroll-other-window -1)))
+                         ("N" . 'scroll-other-window)
+                         ("P" . (lambda () (scroll-other-window '-)))
+                         ("a" . (lambda () (beginning-of-buffer-other-window 0)))
+                         ("e" . (lambda () (end-of-buffer-other-window 0))))))
