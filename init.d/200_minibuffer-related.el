8b50a7f4959bc9f320edc8cd088a096779043927
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-03-04 13:23:27 -0500

    Reconfigure mini-buffer

--- init.d/200_minibuffer-related.el
+++ init.d/200_minibuffer-related.el
@@ -1,6 +1,15 @@
-;;; mini-buffer configuration 2014-02-03
-(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup)
-;; Larger font size
+;;; mini-buffer configuration
+
+
+;;; Larger font size
 (defun my-minibuffer-setup ()
-       (set (make-local-variable 'face-remapping-alist)
-          '((default :height 1.2))))	; Larger font size
+  (set (make-local-variable 'face-remapping-alist)
+       '((default :height 1.5))))
+;;
+(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup)
+
+
+;;; Improve coding experience within mini-buffer
+;; http://emacsredux.com/blog/2016/03/02/pimp-my-minibuffer/
+(add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
