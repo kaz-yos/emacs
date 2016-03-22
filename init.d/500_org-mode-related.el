740e3e53431ef93600db6608cd3f382aa7032ba8
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-03-22 08:57:10 -0400

    Add ox-beamer setting

--- init.d/500_org-mode-related.el
+++ init.d/500_org-mode-related.el
@@ -5,4 +5,8 @@
   :config
   ;; Reassign Keep the following key
   (define-key org-mode-map (kbd "<C-tab>") 'other-window-or-split)
-  (define-key org-mode-map (kbd "<A-tab>") 'org-global-cycle))
+  (define-key org-mode-map (kbd "<A-tab>") 'org-global-cycle)
+  ;;
+  ;; Beamer presentations using the new export engine
+  ;; http://orgmode.org/worg/exporters/beamer/ox-beamer.html
+  (require 'ox-beamer))
