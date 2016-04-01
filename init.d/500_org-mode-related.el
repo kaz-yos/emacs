c8a80cab12ed413d6e0151a01c5ecaa462f56e64
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-04-01 10:10:29 -0400

    Set line wrapping in org mode

--- init.d/500_org-mode-related.el
+++ init.d/500_org-mode-related.el
@@ -7,6 +7,11 @@
   (define-key org-mode-map (kbd "<C-tab>") 'other-window-or-split)
   (define-key org-mode-map (kbd "<A-tab>") 'org-global-cycle)
   ;;
+  ;; Linewrap in Org-mode of Emacs
+  ;; http://superuser.com/questions/299886/linewrap-in-org-mode-of-emacs
+  (setq org-startup-truncated nil)
+  (define-key org-mode-map "\M-q" 'toggle-truncate-lines)
+  ;;
   ;; Beamer presentations using the new export engine
   ;; http://orgmode.org/worg/exporters/beamer/ox-beamer.html
   (require 'ox-beamer)
