823964bbb29363e0042fbda1d8ac055a01f807ea
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-04-20 15:59:53 -0400

    Add org-mode settings

--- init.d/500_org-mode-related.el
+++ init.d/500_org-mode-related.el
@@ -28,6 +28,15 @@
   ;; This breaks other behaviors?
   ;; (define-key org-mode-map (kbd "<return>") 'org-meta-return)
   ;;
+  ;; Completion (Taken from Shunguang's)
+  (setq org-completion-use-ido t)
+  ;; Targets include this file and any file contributing to the agenda - up to 3 levels deep
+  (setq org-refile-targets (quote ((org-agenda-files :maxlevel . 3) (nil :maxlevel . 3))))
+  ;; Targets start with the file name - allows creating level 1 tasks
+  (setq org-refile-use-outline-path (quote file))
+  ;; Targets complete in steps so we start with filename, TAB shows the next level of targets etc
+  (setq org-outline-path-complete-in-steps t)
+  ;;
   ;; Linewrap in Org-mode of Emacs
   ;; http://superuser.com/questions/299886/linewrap-in-org-mode-of-emacs
   (setq org-startup-truncated nil)
@@ -45,14 +54,22 @@
   ;; http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-R.html
   (org-babel-do-load-languages
    'org-babel-load-languages
-   '((python . t)
+   '((sql . t)
+     (sqlite . t)
+     (python . t)
      (R . t)
      (emacs-lisp . t)))
   ;;
+
   ;; http://orgmode.org/worg/org-contrib/babel/examples/fontify-src-code-blocks.html
   (setq org-src-fontify-natively t)
+  ;;
+  ;; LaTeX
+  (setq org-highlight-latex-fragments-and-specials t)
   ;; http://stackoverflow.com/questions/17239273/org-mode-buffer-latex-syntax-highlighting
   (setq org-highlight-latex-and-related '(latex script entities))
+  ;; Default packages (Taken from Shunguang's)
+  (setq org-latex-packages-alist '(("AUTO" "inputenc" t) ("T1" "fontenc" t) ("" "fixltx2e" t) ("" "graphicx" t) ("" "longtable" t) ("" "float" t) ("" "wrapfig" t) ("" "soul" t) ("" "textcomp" t) ("" "marvosym" t) ("" "amssymb" t) ("" "mathtools" t) ("" "amsmath" t) ("" "wasysym" t) ("" "latexsym" t)  ("" "hyperref" t) ("" "pdfpages" t) ("" "tikz" t ) ("" "listings" t) ("" "color" t) "\\tolerance=1000"))
   ;;
   ;; Citation management
   ;; https://github.com/jkitchin/org-ref
@@ -72,6 +89,7 @@
   ;;
   ;; Emacs Org-mode Bibtex Screencast
   ;; https://vimeo.com/99167082
+  ;; (require 'ox-bibtex) ; not available?
   ;;
   ;; Beamer presentations using the new export engine
   ;; http://orgmode.org/worg/exporters/beamer/ox-beamer.html
