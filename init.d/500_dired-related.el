2b3e87c5564cbc466ede09cc6c12390a2787cc37
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-10-24 16:50:54 -0400

    Add ora-dired-rsync

--- init.d/500_dired-related.el
+++ init.d/500_dired-related.el
@@ -22,6 +22,35 @@
 ;;
 
 
+;;; Use rsync in dired.el
+;; https://github.com/abo-abo/oremacs/blob/2011da0874d27058d3ce8bded5cd8817314c27be/auto.el#L225-L247
+;; https://www.reddit.com/r/emacs/comments/58zieq/still_cant_get_over_how_powerful_tramp_is/
+(defun ora-dired-rsync (dest)
+  (interactive
+   (list (expand-file-name
+          (read-file-name "Rsync to:" (dired-dwim-target-directory)))))
+  ;; store all selected files into "files" list
+  (let ((files (dired-get-marked-files nil current-prefix-arg))
+        ;; the rsync command
+        (tmtxt/rsync-command "rsync -arvz --progress "))
+    ;; add all selected file names as arguments to the rsync command
+    (dolist (file files)
+      (setq tmtxt/rsync-command
+            (concat tmtxt/rsync-command
+                    (shell-quote-argument file)
+                    " ")))
+    ;; append the destination
+    (setq tmtxt/rsync-command
+          (concat tmtxt/rsync-command
+                  (shell-quote-argument dest)))
+    ;; run the async shell command
+    (async-shell-command tmtxt/rsync-command "*rsync*")
+    ;; finally, switch to that window
+    (other-window 1)))
+;; Bind to Y
+(define-key dired-mode-map "Y" 'ora-dired-rsync)
+
+
 ;;; dired-subtree.el
 ;; https://github.com/Fuco1/dired-hacks/blob/master/dired-subtree.el
 (require 'dired-subtree)
