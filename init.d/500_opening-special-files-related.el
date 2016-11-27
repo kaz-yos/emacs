51e577a2d87144a880c05444c0f0f91997d19030
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-11-27 13:09:00 -0500

    Add pdf-tools configuration

--- init.d/500_opening-special-files-related.el
+++ init.d/500_opening-special-files-related.el
@@ -15,3 +15,32 @@
   :bind (("C-x C-z" . open-junk-file))
   :config
   (setq open-junk-file-directory "~/junk/%Y/%m/%d-%H%M%S."))
+
+
+;;;
+;;; pdf-tools.el
+;; https://github.com/politza/pdf-tools
+;; brew install poppler automake
+;; Although OS X is not officially supported, it has been reported to have been
+;; successfully compiled. You will need to install poppler which you can get with
+;; homebrew via brew install poppler automake (recipe below takes care of this)
+;;
+;; http://emacs.stackexchange.com/questions/13314/install-pdf-tools-on-emacs-macosx
+;; Install epdfinfo via 'brew install homebrew/emacs/pdf-tools' and then install the
+;; pdf-tools elisp via the use-package below. To upgrade the epdfinfo
+;; server, just do 'brew upgrade pdf-tools' prior to upgrading to newest
+;; pdf-tools package using Emacs package system. If things get messed
+;; up, just do 'brew uninstall pdf-tools', wipe out the elpa
+;; pdf-tools package and reinstall both as at the start.
+(use-package pdf-tools
+  :ensure t
+  :config
+  ;; Whether PDF Tools should handle upgrading itself.
+  ;; Up grading should be via Homebrew
+  (setq pdf-tools-handle-upgrades nil)
+  ;;
+  ;; Filename of the epdfinfo executable installed via Homebrew
+  (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo")
+  ;;
+  ;; Install PDF-Tools in all current and future PDF buffers.
+  (pdf-tools-install))
