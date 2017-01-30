a91c45b5bda929f26c91c8cca22da2a6619896e1
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-01-30 18:39:37 -0500

    Make init for async org-mode minimum
    ob-* files are loaded by org-babel-do-load-languages, thus, there is
    no need to require them explicitly.

--- init_org_async.el
+++ init_org_async.el
@@ -3,69 +3,41 @@
 ;; Specify this file in `org-export-async-init-file'
 ;; Errors in async process can be examined using M-x org-export-stack
 
-;;;
 ;;; Configure directories
 (let ((default-directory "~/.emacs.d/elpa/"))
   (normal-top-level-add-subdirs-to-load-path))
 
-;;; Loading
-;; Some packages use defstruct, which is obsolete. Alias cl-defstruct to avoid errors.
-;; 2016-01-09 latex-math-preview still depends on defstruct, and gives an error w/o it.
-(defalias 'defstruct 'cl-defstruct)
-;;
-;; Need to be initialized.
-(package-initialize)
-
-
-;;;
-;;; use-package.el
-(require 'use-package)
 
 ;;;
 ;;; Configure org-mode
 (require 'org)
-;;; Load plugins
 (require 'org-ref)
 ;;; Load exporters
-(require 'ox)
 (require 'ox-beamer)
-(require 'ob-R)
-(require 'ob-python)
-(require 'ob-ditaa)
-(require 'ob-dot)
-;;; Language packages
-;;; ESS
-(require 'ess-site)
-(setq inferior-R-args "--no-restore-history --no-save ")
-(setq ess-ask-for-ess-directory nil)
+;;
 ;;
 ;;; Enable languages
 ;; Org-Mode Evaluation of code disabled
 ;; https://github.com/syl20bnr/spacemacs/issues/7641
 (org-babel-do-load-languages
  'org-babel-load-languages
- '(;; http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-LaTeX.html
-   (latex . t)
-   ;; http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-R.html
+ '((latex . t)
    (R . t)
-   ;; http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-python.html
    (python . t)
-   ;; http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-ditaa.html
    (ditaa . t)
-   ;; http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-dot.html
    (dot . t)
-   ;;
+   ;; This does not work unless there is a slime buffer.
+   ;; (lisp . t)
    (emacs-lisp . t)
    (shell . t)))
 ;;
 ;; Do not ask code execution confirmation
 (setq org-confirm-babel-evaluate nil)
 ;;
-;; Language specific settings
-;; ditaa diagram language (git repo org-mode has necessary scripts)
-(setq org-ditaa-jar-path "~/.emacs.d/packages/org-mode/contrib/scripts/ditaa.jar")
 ;;
+;;; Language specific settings
 ;;
+;;;  latex
 ;; PDF processing with correct bibtex handling
 ;; http://lists.gnu.org/archive/html/emacs-orgmode/2013-05/msg00791.html
 ;; %f: full file name
@@ -77,6 +49,15 @@
         "pdflatex -interaction nonstopmode -output-directory %o %f"
         "pdflatex -interaction nonstopmode -output-directory %o %f"))
 ;;
+;;;  R
+(setq inferior-R-args "--no-restore-history --no-save ")
+(setq ess-ask-for-ess-directory nil)
 ;;
-;; In case there are missed configurations. (loading this way may not be functions)
-;; (load "~/.emacs.d/init.d/500_org-mode-related.el")
+;;;  slime
+;; This does not work unless there is a slime process already running (not possible in async).
+;; https://lists.gnu.org/archive/html/emacs-orgmode/2010-07/msg01237.html
+(setq inferior-lisp-program (executable-find "sbcl"))
+;;
+;;;  ditaa diagram language
+;; (git repo org-mode has necessary scripts)
+(setq org-ditaa-jar-path "~/.emacs.d/packages/org-mode/contrib/scripts/ditaa.jar")
