9720a3c9a32fe882a4d41b0042da788da54ba38b
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-01-30 11:23:44 -0500

    Add toggle window split

--- init.d/200_window-management-related.el
+++ init.d/200_window-management-related.el
@@ -41,6 +41,37 @@
 
 
 ;;;
+;;; Toggle window split layout
+;; http://stackoverflow.com/questions/14881020/emacs-shortcut-to-switch-from-a-horizontal-split-to-a-vertical-split-in-one-move
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+;;;
+(global-set-key (kbd "C-x |") 'toggle-window-split)
+
+
+;;;
 ;;; Scroll window with C-t/C-v
 ;; transpose-char changed to cua-scroll-down
 (global-set-key (kbd "C-t") 'cua-scroll-down)	; C-t to scroll down, C-v to scroll up
