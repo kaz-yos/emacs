39b4d478d9b6ad63c5f3172b663bf03a6bf0fb2a
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-05-09 13:13:31 -0400

    Add function to show last several messages

--- init.d/200_minibuffer-related.el
+++ init.d/200_minibuffer-related.el
@@ -13,3 +13,31 @@
 ;; http://emacsredux.com/blog/2016/03/02/pimp-my-minibuffer/
 (add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
+
+
+;;; Show last several messages in the mini-buffer.
+;; http://mbork.pl/2017-05-01_show-some-last-messages
+(defcustom default-messages-to-show 4
+  "Default number of messages for `show-some-last-messages'.")
+;;
+(defun show-some-last-messages (count)
+  "Show COUNT last lines of the `*Messages*' buffer."
+  (interactive "P")
+  (setq count (if count (prefix-numeric-value count)
+		default-messages-to-show))
+  (save-excursion
+    (set-buffer "*Messages*")
+    (let ((prev-point-max (point-max-marker))
+	  (inhibit-read-only t))
+      (message "%s"
+               (progn
+		 (set-buffer "*Messages*")
+		 (buffer-substring-no-properties
+		  (progn
+                    (goto-char (point-max))
+                    (unless (bolp)
+                      (insert "\n"))
+                    (forward-line (- count))
+                    (point))
+		  (point-max))))
+      (delete-region (point-max) prev-point-max))))
