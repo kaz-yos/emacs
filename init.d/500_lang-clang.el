d830c7130b71d4cf3f4362456e21abe911f1c1fc
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-05-19 05:19:44 -0400

    Configure C++ compiler

--- init.d/500_lang-clang.el
+++ init.d/500_lang-clang.el
@@ -2,17 +2,19 @@
 
 ;; using the current buffer's file name in M-x compile
 ;; http://stackoverflow.com/questions/12756531/using-the-current-buffers-file-name-in-m-x-compile
-(add-hook 'c++-mode-hook
-          (lambda ()
-            (unless (or (file-exists-p "makefile")
-                        (file-exists-p "Makefile"))
-              ;; Proceed unless there is a Makefile.
-              (set
-               ;; (make-local-variable VARIABLE)
-               ;; Make VARIABLE have a separate value in the current buffer.
-               (make-local-variable 'compile-command)
-               ;; This will be the value for c++.
-               (concat "g++ -pipe -O2 -std=c++14 "
-                       buffer-file-name
-                       "-lm"
-                       (file-name-sans-extension buffer-file-name))))))
+(defun c++compile-command-setter ()
+  "Set compile-command for C++"
+  (unless (or (file-exists-p "makefile")
+              (file-exists-p "Makefile"))
+    ;; Proceed unless there is a Makefile.
+    (set
+     ;; (make-local-variable VARIABLE)
+     ;; Make VARIABLE have a separate value in the current buffer.
+     (make-local-variable 'compile-command)
+     ;; This will be the value for c++.
+     (concat "g++ -pipe -O2 -std=c++14 "
+             buffer-file-name
+             " -lm -o "
+             (file-name-sans-extension buffer-file-name)))))
+
+(add-hook 'c++-mode-hook #'c++compile-command-setter)
