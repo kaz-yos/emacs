ff722e336afce4d9075cc94dc8608b36aca8dd30
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-07-31 09:25:09 -0400

    Make inf-ruby use use-package

--- init.d/500_lang-ruby.el
+++ init.d/500_lang-ruby.el
@@ -1,14 +1,6 @@
 ;;; 500_lang-ruby.el ---                             -*- lexical-binding: t; -*-
 
-;; ac-inf-ruby        20131115.350 available  melpa      Enable auto-complete in inf-ruby sessions
-;; company-inf-ruby   20140805.... available  melpa      company-mode completion back-end for inf-ruby
-;; flymake-ruby       20121104.... available  melpa      A flymake handler for ruby-mode files
-;; inf-ruby           20141005.550 available  melpa      Run a Ruby process in a buffer
-
 ;;; 
 ;;; inf-ruby.el
-(require 'inf-ruby)
-
-;;;
-;;; ac-inf-ruby.el
-(require 'ac-inf-ruby)
+(use-package inf-ruby
+  :commands (inf-ruby))
