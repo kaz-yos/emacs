c57af9f9270aa0d98dab1b64fbc97a34255fe3ba
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-08-11 15:15:51 -0400

    Clean javascript configuration

--- init.d/500_lang-javascript.el
+++ init.d/500_lang-javascript.el
@@ -3,17 +3,18 @@
 ;;; javascript and related languages
 
 
+;; Setting up Emacs for JavaScript (part #1)
+;; https://emacs.cafe/emacs/javascript/setup/2017/04/23/emacs-setup-javascript.html
+
+
 ;;;
 ;;; js2-mode.el
 ;; Improved JavaScript editing mode for GNU Emacs
 ;; https://github.com/mooz/js2-mode
-;;
-;; Set up Javascript development environment in Emacs
-;; http://truongtx.me/2014/02/23/set-up-javascript-development-environment-in-emacs/
-;;
 (use-package js2-mode
   :mode (("\\.json$" . js-mode)
          ("\\.js$" . js-mode))
+  ;;
   :config
   (add-hook 'js-mode-hook 'js2-minor-mode)
   (add-hook 'js2-mode-hook 'ac-js2-mode)
@@ -21,7 +22,7 @@
   (setq js2-highlight-level 3)
   ;; ac-js2.el
   (add-hook 'js2-mode-hook 'ac-js2-mode)
-
+  ;;
 ;;; js3-mode.el
   ;; A chimeric fork of js2-mode and js-mode
   ;; https://github.com/thomblake/js3-mode
