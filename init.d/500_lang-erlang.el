0a186813865f7663f3ad7efeab9faaea5227a64e
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-08-11 15:17:10 -0400

    Drop erlang support

--- init.d/500_lang-erlang.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-;;;
-;;; Erlang-related
-;;; erlang.el
-(use-package erlang-start
-  :mode (("\\.erl\\'" . erlang-mode)
-         ("\\.hrl\\'" . erlang-mode))
-  :interpreter ("escript" . erlang-mode))
