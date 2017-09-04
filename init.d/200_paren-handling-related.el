431263ba482bb065b197069625edf20cb3c8a873
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-09-04 10:43:11 -0400

    Rewrite parenthesis handling configuration

--- init.d/200_paren-handling-related.el
+++ init.d/200_paren-handling-related.el
@@ -4,14 +4,18 @@
 
 ;;;
 ;;; paren.el
-(show-paren-mode t)
+(use-package paren
+  :config
+  (show-paren-mode t))
 
 
 ;;;
 ;;; elec-pair.el
 ;; Electric Pair mode is a global minor mode.  When enabled, typing
 ;; an open parenthesis automatically inserts the corresponding
-(electric-pair-mode t)
-;; If non-nil, skip char instead of inserting a second closing paren.
-;; Better not to prevent overlapping ] before ]
-(setq electric-pair-skip-self nil)
+(use-package elec-pair
+  :config
+  (electric-pair-mode t)
+  ;; If non-nil, skip char instead of inserting a second closing paren.
+  ;; Better not to prevent overlapping ] before ]
+  (setq electric-pair-skip-self nil))
