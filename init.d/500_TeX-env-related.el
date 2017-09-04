aa5759e9b794a42689b63e98f697c6db7872b964
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-09-04 09:52:39 -0400

    Clean zotelo configuration

--- init.d/500_TeX-env-related.el
+++ init.d/500_TeX-env-related.el
@@ -280,13 +280,11 @@
   (add-hook 'TeX-mode-hook 'zotelo-minor-mode)
   ;;
   :config
-  (setq zotelo-use-ido nil)
-  ;; C-c z c         zotelo-set-collection (also C-c z s)
-  ;; C-c z u         zotelo-update-database
-  ;; C-c z e         zotelo-export-secondary
-  ;; C-c z r         zotelo-reset
-  ;; C-c z t         zotelo-set-translator
-  )
+  (setq zotelo-use-journal-abbreviation nil)
+  ;;
+  ;; zotelo-set-collection uses the ido interface by default.
+  ;; If you don't like it, set `zotelo-use-ido' to nil.
+  (setq zotelo-use-ido nil))
 
 
 ;;;  zotxt.el
