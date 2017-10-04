7e328bb94d4ba7ab20b251e186dd1964461b01a3
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-10-03 21:37:32 -0400

    Enable global flycheck mode

--- init.d/500_lang-syntax-checker-related.el
+++ init.d/500_lang-syntax-checker-related.el
@@ -2,15 +2,22 @@
 
 ;;;
 ;;; fly-check.el
+;; http://www.flycheck.org/en/latest/
 ;; https://github.com/flycheck/flycheck
 ;; Supported languages
 ;; http://flycheck.readthedocs.org/en/latest/guide/languages.html#supported-languages
 ;; 3rd party extensions
 ;; http://flycheck.readthedocs.org/en/latest/guide/introduction.html#rd-party-extensions
 (use-package flycheck
-  :commands (flycheck-mode)
+  :commands (flycheck-mode
+             global-flycheck-mode)
+  :init
+  (add-hook 'after-init-hook 'global-flycheck-mode)
+  ;;
   :config
   ;;; flycheck-pos-tip.el
   ;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b#2-5
-  (require 'flycheck-pos-tip)
-  (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages) )
+  (use-package flycheck-pos-tip
+    :config
+    (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
+    (flycheck-pos-tip-mode +1)))
