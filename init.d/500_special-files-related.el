bf4d5d590b5d36d7870bc76c7ac112f583e82dc8
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-10-06 13:57:07 -0400

    Do not install pdf-tools via homebrew (use package.el)
    The MELPA package pdf-tools can install the required executable
    /usr/local/bin/epdfinfo
    automatically.
    
    So installing the pdf-tools homebrew package is redundant.
    The backend poppler still has to be installed via homebrew.
    
    The 2017-10-06 version of poppler breaks the epdfinfo dependency.
    The following is the record.
    
    Issue with poppler 0.60.1
    
    /usr/local/Cellar/poppler/0.60.1 (443 files, 18.4MB) *
    Poured from bottle on 2017-10-06 at 12:35:58
    
    $ epdfinfo
    dyld: Library not loaded: /usr/local/opt/poppler/lib/libpoppler.70.dylib
    Referenced from: /usr/local/bin/epdfinfo
    Reason: image not found
    
    Solution
    
    $ cd /usr/local/opt/poppler/lib/
    $ ln -s libpoppler.71.dylib libpoppler.70.dylib

--- init.d/500_special-files-related.el
+++ init.d/500_special-files-related.el
@@ -22,88 +22,43 @@
 ;;;
 ;;; pdf-tools.el
 ;; https://github.com/politza/pdf-tools
-;; $ brew install poppler automake
 ;; Although OS X is not officially supported, it has been reported to have been
 ;; successfully compiled. You will need to install poppler which you can get with
-;; homebrew via $ brew install poppler automake (recipe below takes care of this)
+;; homebrew via
+;; $ brew install poppler automake
 ;;
-;; http://emacs.stackexchange.com/questions/13314/install-pdf-tools-on-emacs-macosx
-;; Install epdfinfo via 'brew install homebrew/emacs/pdf-tools' and then install the
-;; pdf-tools elisp via the use-package below. To upgrade the epdfinfo
-;; server, just do 'brew upgrade pdf-tools' prior to upgrading to newest
-;; pdf-tools package using Emacs package system. If things get messed
-;; up, just do 'brew uninstall pdf-tools', wipe out the elpa
-;; pdf-tools package and reinstall both as at the start.
+;; When it's not working try running epdfinfo directly to look for errors.
+;; $ epdfinfo
 ;;
-;; When dysfunctional, try uninstalling and installing again.
-;; $ brew reinstall pdf-tools
+;; Issue with poppler 0.60.1
+;; /usr/local/Cellar/poppler/0.60.1 (443 files, 18.4MB) *
+;; Poured from bottle on 2017-10-06 at 12:35:58
+;; $ epdfinfo
+;; dyld: Library not loaded: /usr/local/opt/poppler/lib/libpoppler.70.dylib
+;; Referenced from: /usr/local/bin/epdfinfo
+;; Reason: image not found
 ;;
-;; Homebrew recipe moved to:
-;; https://github.com/dunn/homebrew-emacs
-;;
-;;;  poppler compatibility issue
-;; $ brew install pdf-tools
-;; Updating Homebrew...
-;; ==> Installing pdf-tools from dunn/emacs
-;; ==> Downloading https://github.com/politza/pdf-tools/archive/v0.70.tar.gz
-;; Already downloaded: /Users/kazuki/Library/Caches/Homebrew/pdf-tools-0.70.tar.gz
-;; ==> make server/epdfinfo
-;; Last 15 lines from /Users/kazuki/Library/Logs/Homebrew/pdf-tools/01.make:
-;; configure.ac:11: installing './compile'
-;; configure.ac:6: installing './install-sh'
-;; configure.ac:6: installing './missing'
-;; Makefile.am: installing './depcomp'
-;; cd server && ./configure -q
-;; configure: WARNING: Annot.h: present but cannot be compiled
-;; configure: WARNING: Annot.h:     check for missing prerequisite headers?
-;; configure: WARNING: Annot.h: see the Autoconf documentation
-;; configure: WARNING: Annot.h:     section "Present But Cannot Be Compiled"
-;; configure: WARNING: Annot.h: proceeding with the compiler's result
-;; configure: WARNING:     ## ---------------------------------- ##
-;; configure: WARNING:     ## Report this to politza@fh-trier.de ##
-;; configure: WARNING:     ## ---------------------------------- ##
-;; configure: error: cannot find necessary  poppler-private header (see README.org)
-;; make: *** [server/Makefile] Error 1
-;; If reporting this issue please do so at (not Homebrew/brew or Homebrew/core):
-;; https://github.com/dunn/homebrew-emacs/issues
-;;
-;; $ brew switch poppler 0.57.0_1 # This fixed the above issue.
-;;
-;; poppler development information.
-;; https://poppler.freedesktop.org
+;; Solved by creating a symlink.
+;; $ cd /usr/local/opt/poppler/lib/
+;; $ ln -s libpoppler.71.dylib libpoppler.70.dylib
 (use-package pdf-tools
   ;; The deferring configuration was take from the following repository.
   ;; https://github.com/kaushalmodi/.emacs.d/blob/master/setup-files/setup-pdf.el
   :mode (("\\.pdf\\'" . pdf-view-mode))
   ;;
-  :init
-  ;; Add the path to the MELPA version to avoid loading the Homebrew version.
-  (add-to-list 'load-path
-               ;; We need a wild card as a MELPA package keeps changing the folder name.
-               ;; http://emacs.stackexchange.com/questions/9768/elisp-files-in-load-path-are-not-loaded-on-emacs-start
-               ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/List-Elements.html
-               (car
-                (last
-                 (file-expand-wildcards
-                  (concat user-emacs-directory "elpa/pdf-tools*")))))
-  ;;
   :config
+  ;; These are necessary to create autoloads if not using (package-initialize)
   (use-package pdf-history
     :commands (pdf-history-minor-mode))
-  ;;
-  ;; Whether PDF Tools should handle upgrading itself.
-  ;; Up grading should be via Homebrew
-  (setq pdf-tools-handle-upgrades nil)
-  ;;
-  ;; Filename of the epdfinfo executable installed via Homebrew
-  (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo")
-  ;;
-  ;; Install PDF-Tools in all current and future PDF buffers.
-  ;; https://github.com/politza/pdf-tools/issues/72
   (use-package pdf-occur
     ;; These are required by pdf-tools-install.
     :commands (pdf-occur-global-minor-mode))
   ;;
+  ;; Filename of the epdfinfo executable.
+  (setq pdf-info-epdfinfo-program (executable-find "epdfinfo"))
+  ;;
+  ;; Install PDF-Tools in all current and future PDF buffers.
+  ;; https://github.com/politza/pdf-tools/issues/72
   (pdf-tools-install)
   ;;
   ;; Auto-revert
