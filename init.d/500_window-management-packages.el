0afac262e9281eb77777bb72bab21672fd828ca9
kaz-yos <kazukiyoshida@mail.harvard.edu>
2017-11-22 14:20:06 -0500

    Add window-purpose.el

--- init.d/500_window-management-packages.el
+++ init.d/500_window-management-packages.el
@@ -172,3 +172,30 @@ Set name truncation length in ELSCREEN-TRUNCATE-LENGTH"
   ;;
   ;; It has to kick in.
   (elscreen-start))
+
+
+;;;
+;;; window-purpose.el
+;; https://github.com/bmag/emacs-purpose
+(use-package window-purpose
+  :config
+  ;; Purpose Configuration:
+  ;; Customize `purpose-user-mode-purposes', `purpose-user-name-purposes',
+  ;; `purpose-user-regexp-purposes' and
+  ;; `purpose-use-default-configuration'.
+  ;;
+  ;; Basic Usage:
+  ;; 1. Load/Save window/frame layout (see `purpose-load-window-layout',
+  ;;    `purpose-save-window-layout', etc.)
+  ;; 2. Use regular switch-buffer functions - they will not mess your
+  ;;    window layout (Purpose overrides them).
+  ;; 3. If you don't want a window's purpose/buffer to change, dedicate
+  ;;    the window:
+  ;;    C-c , d: `purpose-toggle-window-purpose-dedicated'
+  ;;    C-c , D: `purpose-toggle-window-buffer-dedicated'
+  ;; 4. To use a switch-buffer function that ignores Purpose, prefix it
+  ;;    with C-u. For example, [C-u C-x b] calls
+  ;;    `switch-buffer-without-purpose'.
+  ;;
+  ;; Activate
+  (purpose-mode))
