45a0a1037298d11b9e7826c6d1dae690c12b79c9
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-02-13 12:49:08 -0500

    Change to latest mu via homebrew

--- init.d/500_email.el
+++ init.d/500_email.el
@@ -70,7 +70,7 @@
               ("m" . mu4e))
   :init
   ;; Check for mu4e directory before invoking all the following
-  (let ((mu4e-dir "/usr/local/Cellar/mu/0.9.18_1/share/emacs/site-lisp/mu/mu4e/"))
+  (let ((mu4e-dir "/usr/local/Cellar/mu/1.0/share/emacs/site-lisp/mu/mu4e/"))
     (when (file-exists-p mu4e-dir)
       (add-to-list 'load-path mu4e-dir)))
   ;; Start mu4e after init.
