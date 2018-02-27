698cfa888e3310da693c0a15010aa9cb71e85da6
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-02-27 03:06:40 -0500

    Drop moz repl related packages

--- init.d/500_lang-javascript.el
+++ init.d/500_lang-javascript.el
@@ -32,29 +32,3 @@
   ;; Set inferior-js-program-command to the execution command for running your javascript REPL
   ;; Use Node.js https://nodejs.org/en/
   (setq inferior-js-program-command "node --interactive"))
-
-
-;;;
-;;; MOZ-RELATED
-
-;;;  moz.el
-;; Lets current buffer interact with inferior mozilla
-;; https://github.com/bard/mozrepl/wiki/Emacs-integration
-;; C-c C-s: open a MozRepl interaction buffer and switch to it
-;; C-c C-l: save the current buffer and load it in MozRepl
-;; C-M-x: send the current function (as recognized by c-mark-function) to MozRepl
-;; C-c C-c: send the current function to MozRepl and switch to the interaction buffer
-;; C-c C-r: send the current region to MozRepl
-;; In the interaction buffer:
-;; C-c c: insert the current name of the REPL plus the dot operator (usually repl.)
-(use-package moz
-  :commands (inferior-moz-mode
-             moz-minor-mode))
-
-
-;;;  moz-controller.el
-;; Control Firefox from Emacs
-;; https://github.com/RenWenshan/emacs-moz-controller
-(use-package moz-controller
-  :commands (moz-controller-mode
-             moz-controller-global-mode))
