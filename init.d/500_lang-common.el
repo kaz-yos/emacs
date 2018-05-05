026059292ee862d68790508c502a751be0df7b28
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-05-05 05:29:29 -0400

    Drop unused packages

--- init.d/500_lang-common.el
+++ init.d/500_lang-common.el
@@ -1,70 +1,13 @@
 ;;; 500_lang-common.el ---                           -*- lexical-binding: t; -*-
 ;; Settings useful for programming in general
 
-;;;
-;;; column-marker.el
-;; http://www.emacswiki.org/emacs/column-marker.el
-(use-package column-marker
-  :commands column-marker80
-  :config
-  (defun column-marker80 ()
-    (interactive)
-    (column-marker-2 80)))
-
 
 ;;;
 ;;; aggressive-indent.el
 ;; https://github.com/Malabarba/aggressive-indent-mode
 (use-package aggressive-indent
+  :hook (emacs-lisp-mode . aggressive-indent-mode)
   :commands (aggressive-indent-mode))
-(add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
-
-
-;;;
-;;; pos-tip.el
-;; https://github.com/tjarvstrand/pos-tip
-;; It breaks down on macOS?
-(use-package pos-tip
-  :disabled t
-  :config
-  (defun pos-tip-eldoc-display-message (format-string &rest args)
-    "Display eldoc message near point."
-    (when format-string
-      (pos-tip-show (apply 'format format-string args))))
-  ;; Use
-  (setq eldoc-message-function #'pos-tip-eldoc-display-message))
-
-
-;;;
-;;; fold-dwim.el
-;;
-;; DWIM stands for "do what I mean", as in the idea that one keystroke
-;; can do different things depending on the context. In this package,
-;; it means that, if the cursor is in a currently hidden folded
-;; construction, we want to show it; if it's not, we want to hide
-;; whatever fold the cursor is in.
-(use-package fold-dwim
-  :commands (fold-dwim-toggle
-             fold-dwim-hide-all
-             fold-dwim-show-all)
-  ;; :bind (("<f7>" . fold-dwim-toggle)
-  ;;        ("<M-f7>" . fold-dwim-hide-all)
-  ;;        ("<S-M-f7>" . fold-dwim-show-all))
-  )
-;; This package binds no keys by default, so you need to find three
-;; free and convenient key-bindings.  This is what I use:
-;;
-;;  (global-set-key (kbd "<f7>")      'fold-dwim-toggle)
-;;  (global-set-key (kbd "<M-f7>")    'fold-dwim-hide-all)
-;;  (global-set-key (kbd "<S-M-f7>")  'fold-dwim-show-all)
-;;
-
-
-;;;
-;;; origami.el
-;; https://github.com/gregsexton/origami.el
-(use-package origami
-  :commands (origami))
 
 
 ;;;
@@ -124,24 +67,6 @@
   (setq smart-jump-bind-keys t)
   ;; smart-jump-default-mode-list defines modes for the following.
   (smart-jump-setup-default-registers))
-
-
-;;;
-;;; outshine.el
-;; https://github.com/alphapapa/outshine
-(use-package outshine
-  :commands (outshine-hook-function)
-  :init
-  ;; Hook outline-minor-mode.
-  (add-hook 'outline-minor-mode-hook 'outshine-hook-function)
-  ;; Use outline-minor-mode
-  (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
-  (add-hook 'ess-mode-hook 'outline-minor-mode)
-  (add-hook 'ledger-mode-hook 'outline-minor-mode)
-  (add-hook 'message-mode-hook 'outline-minor-mode)
-  ;;
-  :config
-  )
 
 
 ;;;
