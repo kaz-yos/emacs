847d39a2d20979af5a873b35d9a41ed7d8e0767a
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-05-21 16:53:47 -0400

    Abandon sink() in favor of Rscriptee
    Rscriptee redirects stdout and stderr to .txt
    Changing shebang to Rscriptee did not work.

--- autoinsert/rscript.R
+++ autoinsert/rscript.R
@@ -12,11 +12,10 @@
 ### Prepare environment
 ################################################################################
 
-## sink() if being run non-interactively
-if (sink.number() != 0) {sink()}
+## When running non-interactively
 .script_name. <- gsub("^--file=", "", Filter(function(x) {grepl("^--file=", x)}, commandArgs()))
 if (length(.script_name.) == 1) {
-    sink(file = paste0(.script_name., ".txt"), split = TRUE)
+    cat("### Running:", paste(commandArgs()), "\n")
     options(width = 100)
 }
 
@@ -53,7 +52,6 @@ cat("
 
 
 
-
 ################################################################################
 cat("
 ###
@@ -65,5 +63,3 @@ end_time <- Sys.time()
 cat("\n### Started  ", as.character(start_time), "\n")
 cat("### Finished ", as.character(end_time), "\n")
 print(end_time - start_time)
-## Stop sinking to a file if active
-if (sink.number() != 0) {sink()}
