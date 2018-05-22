b3d3a07d2e8fe85274c126e06910ab0bc2436ad3
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-05-21 22:48:26 -0400

    Add Rscriptee as candidate for R

--- runner-conf.el
+++ runner-conf.el
@@ -1,4 +1,4 @@
 
 ("ext-R" "ext-pdf" "ext-clj")
 
-(("ext-R" 0 "R" (("Rscript" "Rscript" 5))) ("ext-pdf" 0 "pdf" (("" "open" 0) ("" "open -a /Applications/Skim.app" 5))) ("ext-clj" 0 "clj" (("" "lein oneoff" 0) ("" "lein exec" 5))))
+(("ext-R" 0 "R" (("" "Rscript" 5) ("" "Rscriptee" 0))) ("ext-pdf" 0 "pdf" (("" "open" 0) ("" "open -a /Applications/Skim.app" 5))) ("ext-clj" 0 "clj" (("" "lein oneoff" 0) ("" "lein exec" 5))))
