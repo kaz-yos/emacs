6ac96985d2f1ff81735c1c0766b465eb938c9212
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-05-21 23:19:37 -0400

    Use {run:out} in runner for R

--- runner-conf.el
+++ runner-conf.el
@@ -1,4 +1,5 @@
 
-("ext-R" "ext-pdf" "ext-clj")
+("ext-R" "ext-pdf")
 
-(("ext-R" 0 "R" (("" "Rscript" 5) ("" "Rscriptee" 0))) ("ext-pdf" 0 "pdf" (("" "open" 0) ("" "open -a /Applications/Skim.app" 5))) ("ext-clj" 0 "clj" (("" "lein oneoff" 0) ("" "lein exec" 5))))
+(("ext-R" 0 "R" (("" "{run:out} Rscript" 5) ("" "{run:out} Rscriptee" 0)))
+ ("ext-pdf" 0 "pdf" (("" "open" 0) ("" "open -a /Applications/Skim.app" 5))))
