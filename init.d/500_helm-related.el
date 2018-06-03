56e24e3f82efb160e7da74bc44bf120eed39095c
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-06-02 22:15:48 -0400

    Clean helm configuration by classifying

--- init.d/500_helm-related.el
+++ init.d/500_helm-related.el
@@ -16,54 +16,73 @@
          ("x" . execute-extended-command)
          )
   :config
-  (use-package helm-config)
+  ;; Global limit for number of candidates displayed.
+  (setq helm-candidate-number-limit 200)
+  ;; Always use the other window for helm (below if no other window exists)
+  (setq helm-split-window-default-side 'other)
+  ;; Emulate `kill-line' in helm minibuffer
+  ;; http://d.hatena.ne.jp/a_bicky/20140104/1388822688
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
   ;;
+;;;  helm-config.el
+  (use-package helm-config
+    :config
+    ;; helm-command-prefix
+    (setq helm-command-prefix-key "C-x c"))
+  ;;
+;;;  helm-ring.el
+  ;; kill-ring, mark-ring, and register browsers for helm.
   ;; Make helm-mark-ring follow
   ;; https://groups.google.com/forum/#!topic/emacs-helm/US8FWnfRu5o
   (use-package helm-ring
     :hook (helm-before-initialize . (lambda ()
                                       ;; Set follow attribute to 1 (yes) for helm-source-mark-ring
                                       (helm-attrset 'follow 1 helm-source-mark-ring))))
+;;;  helm-for-files.el
+  (use-package helm-for-files
+    :config
+    ;; Restrict what to show in helm-for-files
+    (setq helm-for-files-preferred-list '(helm-source-buffers-list
+                                          helm-source-recentf)))
   ;;
-  ;; Restrict what to show in helm-for-files
-  (setq helm-for-files-preferred-list '(helm-source-buffers-list
-                                        helm-source-recentf))
+;;;  helm-locate.el
+  (use-package helm-locate
+    :config
+    ;; For case macro
+    (use-package cl-macs)
+    ;; locate command
+    ;; https://github.com/emacs-helm/helm/wiki/Locate
+    ;; https://github.com/syl20bnr/spacemacs/issues/3280
+    (setq helm-locate-command
+          (case system-type
+            ('gnu/linux "locate -i -r %s")
+            ('berkeley-unix "locate -i %s")
+            ('windows-nt "es %s")
+            ('darwin "mdfind -name %s %s")
+            (t "locate %s")))
+    (setq helm-locate-fuzzy-match
+          (case system-type
+            ('darwin nil)
+            (t 't))))
   ;;
-  ;; locate command
-  ;; https://github.com/emacs-helm/helm/wiki/Locate
-  ;; https://github.com/syl20bnr/spacemacs/issues/3280
-  (setq helm-locate-command
-        (case system-type
-          ('gnu/linux "locate -i -r %s")
-          ('berkeley-unix "locate -i %s")
-          ('windows-nt "es %s")
-          ('darwin "mdfind -name %s %s")
-          (t "locate %s")))
-  (setq helm-locate-fuzzy-match
-        (case system-type
-          ('darwin nil)
-          (t 't)))
+;;;  helm-command.el.
+  (use-package helm-command
+    :config
+    ;; helm-M-x Save command in extended-command-history even when it fail.
+    (setq helm-M-x-always-save-history t))
+
+  ;;;  helm-buffers.el
+  (use-package helm-buffers
+    :config
+    ;; Max length of buffer names before truncate.
+    (setq helm-buffer-max-length nil))
   ;;
-  (setq helm-idle-delay 0.3)
-  (setq helm-input-idle-delay 0.3)
-  (setq helm-candidate-number-limit 200)
-  (setq helm-M-x-always-save-history t)
-  ;; Max length of buffer names before truncate.
-  (setq helm-buffer-max-length nil)
-  ;; Show full path in helm-find-files
-  ;; http://emacs.stackexchange.com/questions/22407/view-a-whole-file-name-or-path-in-helm-buffer
-  (setq helm-ff-transformer-show-only-basename nil)
-  ;; This causes cycle with Buffers above Recentf and is useless.
-  ;; (setq helm-move-to-line-cycle-in-source t)
-  ;; Always use the other window for helm (below if no other window exists)
-  (setq helm-split-window-default-side 'other)
-  ;; helm for isearch 2014-02-01
-  ;; http://shibayu36.hatenablog.com/entry/2013/12/30/190354
-  (define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
-  ;;
-  ;; Emulate `kill-line' in helm minibuffer
-  ;; http://d.hatena.ne.jp/a_bicky/20140104/1388822688
-  (setq helm-delete-minibuffer-contents-from-point t)
-  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
-    "Emulate `kill-line' in helm minibuffer"
-    (kill-new (buffer-substring (point) (field-end)))))
+;;;  helm-files.el
+  (use-package helm-files
+    :config
+    ;; Show full path in helm-find-files
+    ;; http://emacs.stackexchange.com/questions/22407/view-a-whole-file-name-or-path-in-helm-buffer
+    (setq helm-ff-transformer-show-only-basename nil)))
