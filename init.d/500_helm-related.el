bda360e73ecf903a92b7befaa32a7bd137e55480
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-06-02 22:04:24 -0400

    Clean hook

--- init.d/500_helm-related.el
+++ init.d/500_helm-related.el
@@ -21,13 +21,9 @@
   ;; Make helm-mark-ring follow
   ;; https://groups.google.com/forum/#!topic/emacs-helm/US8FWnfRu5o
   (use-package helm-ring
-    :config
-    (add-hook 'helm-before-initialize-hook
-              #'(lambda ()
-                  ;; Set follow attribute to 1 (yes) for helm-source-mark-ring
-                  (helm-attrset 'follow 1 helm-source-mark-ring)
-                  ;; (helm-attrset 'follow 1 helm-source-mark-ring)
-                  )))
+    :hook (helm-before-initialize . (lambda ()
+                                      ;; Set follow attribute to 1 (yes) for helm-source-mark-ring
+                                      (helm-attrset 'follow 1 helm-source-mark-ring))))
   ;;
   ;; Restrict what to show in helm-for-files
   (setq helm-for-files-preferred-list '(helm-source-buffers-list
