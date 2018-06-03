363c918ba2684f606ecbd6427f59ac701a4031e4
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-06-02 22:01:17 -0400

    Minimize helm key bindings

--- init.d/500_helm-related.el
+++ init.d/500_helm-related.el
@@ -1,43 +1,24 @@
+;;; 500_helm-related.el ---                          -*- lexical-binding: t; -*-
+
+
+;;;
 ;;; Helm
-;; http://d.hatena.ne.jp/a_bicky/20140104/1388822688		; arabiki
-;; http://d.hatena.ne.jp/a_bicky/20140125/1390647299		; arabiki 2
-;; http://sleepboy-zzz.blogspot.com/2012/09/anythinghelm.html	; in general
-;; http://d.hatena.ne.jp/syohex/20121207/1354885367		; some useful tips
-;; http://pages.sachachua.com/.emacs.d/Sacha.html#orgheadline12
+;; https://emacs-helm.github.io/helm/
 (use-package helm
-  :ensure t
+  :demand t
   :diminish helm-mode
-  :init
-  (use-package helm-config)
   :bind (("M-x" . helm-M-x)
-         ("s-x" . helm-M-x)
-         ;; Escape hatches
-         ("A-x" . execute-extended-command)
-         ("A-M-x" . execute-extended-command)
-         ("C-M-x" . execute-extended-command)
-         ("s-e" . execute-extended-command)
-         ("H-x" . execute-extended-command)
-         ;;
          ("C-z" . helm-for-files)
-         ("C-c z" . helm-for-files)
-         ;; This does not support jump to package on a package name
-         ;; ("C-x C-f" . helm-find-files)
          ("M-y" . helm-show-kill-ring)
-         ;; ("C-x b" . helm-buffers-list)
-         ("C-x C-r" . helm-recentf)
-         ;; ("C-^" . helm-c-apropos)
-         ;; This brings back the last helm used
-         ;; ("C-;" . helm-resume)
-         ;; ("s-c" . helm-occur)
-         ("A-m" . helm-mark-ring)
-         ("A-l" . helm-locate)
-         ;; ("M-z" . helm-do-grep)
-         ;; ("C-S-h" . helm-descbinds)
          ;;
          :map my-key-map
          ("z" . helm-for-files)
+         ;; Original M-x
+         ("x" . execute-extended-command)
          )
   :config
+  (use-package helm-config)
+  ;;
   ;; Make helm-mark-ring follow
   ;; https://groups.google.com/forum/#!topic/emacs-helm/US8FWnfRu5o
   (use-package helm-ring
@@ -97,8 +78,3 @@
 ;; (defvar *helm for files*) in helm.el
 ;; 2014-09-21 Add the following to test how auctex works with this
 ;; (setq helm-buffer "*helm for files*")
-
-
-
-;;;
-;;; Optional helm packages
