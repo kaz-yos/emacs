9b6893edeef11d49d0b4eb83522739b3dbde7888
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-06-05 14:15:03 -0400

    Improve eir configuration

--- init.d/600_eval-in-repl-config.el
+++ init.d/600_eval-in-repl-config.el
@@ -1,12 +1,15 @@
-;;; eval-in-repl configuration
-
-;;; Uncomment to use the local dev repo
-(let ((eir-repo "~/Documents/programming/emacs-lisp-repos/eval-in-repl"))
-  (when (file-exists-p eir-repo)
-    (add-to-list 'load-path eir-repo)))
+;;; 600_eval-in-repl-config.el ---                   -*- lexical-binding: t; -*-
 
 
-;;; require the main file containing common functions
+;;; EVAL-IN-REPL-RELATED
+;;;  Uncomment to use the local dev repo
+;; (let ((eir-repo "~/Documents/programming/emacs-lisp-repos/eval-in-repl"))
+;;   (when (file-exists-p eir-repo)
+;;     (add-to-list 'load-path eir-repo)))
+
+
+;;;  eval-in-repl.el
+;; require the main file containing common functions
 (use-package eval-in-repl
   :config
   ;;; Default behaviors
@@ -18,89 +21,82 @@
   (setq eir-repl-placement 'left))
 
 
-;;; ielm support (for emacs lisp)
+;;;  ielm support (for emacs lisp)
 (use-package eval-in-repl-ielm
   :commands (eir-eval-in-ielm)
-  :init
-  ;; for .el files
-  (define-key emacs-lisp-mode-map (kbd "<C-return>") 'eir-eval-in-ielm)
-  ;; for *scratch*
-  (define-key lisp-interaction-mode-map (kbd "<C-return>") 'eir-eval-in-ielm)
-  ;; for M-x info
-  (define-key Info-mode-map (kbd "<C-return>") 'eir-eval-in-ielm)
+  :bind (;; for .el files
+         :map emacs-lisp-mode-map
+         ("C-<return>" . eir-eval-in-ielm)
+         ;; for *scratch*
+         :map lisp-interaction-mode-map
+         ("C-<return>" . eir-eval-in-ielm)
+         ;; for M-x info
+         :map Info-mode-map
+         ("C-<return>" . eir-eval-in-ielm))
   :config
   (setq eir-ielm-eval-in-current-buffer t))
 
 
-;;; cider support (for Clojure)
-;; (require 'cider) ; if not done elsewhere
+;;;  cider support (for Clojure)
 (use-package eval-in-repl-cider
-  :commands eir-eval-in-cider
-  :init
-  (with-eval-after-load 'clojure-mode
-    (define-key clojure-mode-map (kbd "<C-return>") 'eir-eval-in-cider)))
+  :after cider
+  :commands (eir-eval-in-cider)
+  :config
+  (define-key clojure-mode-map (kbd "<C-return>") 'eir-eval-in-cider))
 
 
-
-;;; SLIME support (for common lisp)
-;; (require 'slime) ; if not done elsewhere
+;;;  SLIME support (for common lisp)
 (use-package eval-in-repl-slime
-  :commands eir-eval-in-slime
-  :init
-  ;; (with-eval-after-load 'slime
-  ;;   (define-key lisp-mode-map (kbd "<C-return>") 'eir-eval-in-slime))
+  :after slime
+  :commands (eir-eval-in-slime)
+  :config
   (add-hook 'lisp-mode-hook
             '(lambda ()
                (local-set-key (kbd "<C-return>") 'eir-eval-in-slime))))
 
 
-
-;;; geiser support (for Racket and Guile Scheme)
+;;;  geiser support (for Racket and Guile Scheme)
 ;; When using this, turn off racket-mode and scheme supports
 ;; (require 'geiser) ; if not done elsewhere
 (use-package eval-in-repl-geiser
-  :commands eir-eval-in-geiser
-  :init
+  :after geiser-mode
+  :commands (eir-eval-in-geiser)
+  :config
   (add-hook 'geiser-mode-hook
             '(lambda ()
                (local-set-key (kbd "<C-return>") 'eir-eval-in-geiser))))
 
 
-;; ;;; racket-mode support (for Racket)
-;; (require 'racket-mode) ; if not done elsewhere
-;; (require 'eval-in-repl-racket)
-;; (define-key racket-mode-map (kbd "<C-return>") 'eir-eval-in-racket)
-
-;; ;;; scheme support
-;; (require 'scheme)    ; if not done elsewhere
-;; (require 'cmuscheme) ; if not done elsewhere
-;; (require 'eval-in-repl-scheme)
-;; (add-hook 'scheme-mode-hook
-;; 	  '(lambda ()
-;; 	     (local-set-key (kbd "<C-return>") 'eir-eval-in-scheme)))
+;;;  racket-mode support (for Racket)
+(use-package eval-in-repl-racket
+  :after racket-mode
+  :commands (eir-eval-in-racket)
+  :bind (:map racket-mode-map
+              ("C-<return>" . eir-eval-in-racket)))
 
 
-;;; python support
+;;;  scheme support
+(use-package eval-in-repl-scheme
+  :after (scheme cmuscheme)
+  :commands (eir-eval-in-racket)
+  :hook (scheme-mode . (lambda ()
+                         (local-set-key (kbd "<C-return>") 'eir-eval-in-scheme))))
+
+
+;;;  python support
 ;; (require 'python) ; if not done elsewhere
 (use-package eval-in-repl-python
-  :commands eir-eval-in-python
-  :init
-  (add-hook 'python-mode-hook
-            '(lambda ()
-               (local-set-key (kbd "<C-return>") 'eir-eval-in-python)))
-  (add-hook 'ein:notebook-multilang-mode
-            '(lambda ()
-               (local-set-key (kbd "<C-return>") 'eir-eval-in-python))))
+  :commands (eir-eval-in-python)
+  :hook (python-mode . (lambda ()
+                         (local-set-key (kbd "<C-return>") 'eir-eval-in-python))))
 
 
-;;; shell support
-;; (require 'essh) ; if not done elsewhere
+;;;  shell support
 (use-package eval-in-repl-shell
+  :after sh-mode
   :commands eir-eval-in-shell
-  :init
-  (add-hook 'sh-mode-hook
-            '(lambda()
-               (local-set-key (kbd "C-<return>") 'eir-eval-in-shell)))
+  :bind (:map sh-mode-map
+              ("C-<return>" . eir-eval-in-shell))
   :config
   ;; Version with opposite behavior to eir-jump-after-eval configuration
   (defun eir-eval-in-shell2 ()
@@ -110,63 +106,61 @@ This version has the opposite behavior to the eir-jump-after-eval
 configuration when invoked to evaluate a line."
     (interactive)
     (let ((eir-jump-after-eval (not eir-jump-after-eval)))
-      (eir-eval-in-shell)))
-  (add-hook 'sh-mode-hook
-            '(lambda()
-               (local-set-key (kbd "C-M-<return>") 'eir-eval-in-shell2))))
+      (eir-eval-in-shell))))
 
 
-;;; sml support
+;;;  sml support
 ;; (require 'sml-mode) ; if not done elsewhere
 (use-package eval-in-repl-sml
+  :after sml-mode
   :commands eir-eval-in-sml
-  :init
-  (with-eval-after-load 'sml-mode
-    (define-key sml-mode-map (kbd "<C-return>") 'eir-eval-in-sml)
-    ;; function to send a semicolon to SML REPL
-    (define-key sml-mode-map (kbd "C-;") 'eir-send-to-sml-semicolon)))
+  :bind (:map sml-mode-map
+              ("<C-return>" . eir-eval-in-sml)
+              ("C-;" . eir-send-to-sml-semicolon)))
 
 
-;;; ruby support
+;;;  ruby support
 ;; (require 'ruby-mode) ; if not done elsewhere
 ;; (require 'inf-ruby)  ; if not done elsewhere
-;; (require 'ess)       ; if not done elsewhere
 (use-package eval-in-repl-ruby
-  :commands eir-eval-in-ruby
-  :init
-  (with-eval-after-load 'ruby-mode
-    (define-key ruby-mode-map (kbd "<C-return>") 'eir-eval-in-ruby)))
+  :after ruby-mode
+  :commands (eir-eval-in-ruby)
+  :bind (:map ruby-mode-map
+              ("<C-return>" . eir-eval-in-ruby)))
 
 
-;;; ocaml support
-;; (require 'tuareg) ; if not done elsewhere
+;;;  ocaml support
 (use-package eval-in-repl-ocaml
-  :commands eir-eval-in-ocaml
-  :init
-  (with-eval-after-load 'tuareg
-    (define-key tuareg-mode-map (kbd "<C-return>") 'eir-eval-in-ocaml)
-    ;; function to send a semicolon to OCaml REPL
-    (define-key tuareg-mode-map (kbd "C-;") 'eir-send-to-ocaml-semicolon)))
+  :after tuareg
+  :commands (eir-eval-in-ocaml)
+  :bind (:map sml-mode-map
+              ("<C-return>" . eir-eval-in-ocaml)
+              ("C-;" . eir-send-to-ocaml-semicolon)))
 
 
-;;; hy support
+;;;  hy support
 ;; (require 'hy-mode) ; if not done elsewhere
 (use-package eval-in-repl-hy
-  :commands eir-eval-in-hy
-  :init
-  (with-eval-after-load 'hy-mode
-    (define-key hy-mode-map (kbd "<C-return>") 'eir-eval-in-hy)))
+  :after hy-mode
+  :commands (eir-eval-in-hy)
+  :bind (:map hy-mode-map
+              ("<C-return>" . eir-eval-in-hy)))
 
 
-;;; javascript support
-;; (require 'js-comint) ; if not done elsewhere
+;;;  javascript support
 (use-package eval-in-repl-javascript
+  :after js3-mode
   :commands eir-eval-in-javascript
-  :init
-  (with-eval-after-load 'js3-mode
-    (require 'eval-in-repl-javascript)
-    (define-key js3-mode-map (kbd "<C-return>") 'eir-eval-in-javascript))
+  :bind (:map js3-mode-map
+              ("<C-return>" . eir-eval-in-javascript)
+              :map js2-mode-map
+              ("<C-return>" . eir-eval-in-javascript)))
 
-  (with-eval-after-load 'js2-mode
-    (require 'eval-in-repl-javascript)
-    (define-key js2-mode-map (kbd "<C-return>") 'eir-eval-in-javascript)))
+
+;;;
+;;; org-babel-eval-in-repl.el
+;; https://github.com/diadochos/org-babel-eval-in-repl
+(use-package org-babel-eval-in-repl
+  :after ob
+  :bind (:map org-mode-map
+              ("C-<return>" . ober-eval-in-repl)))
