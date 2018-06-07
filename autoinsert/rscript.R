4e90ccbe53f7207c10b518b8604b0a07ad106f13
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-06-07 09:13:26 -0400

    Improve R script template

--- autoinsert/rscript.R
+++ autoinsert/rscript.R
@@ -8,17 +8,17 @@
 ## Author: Kazuki Yoshida
 ################################################################################
 
-
-###
-### Prepare environment
-################################################################################
-
 ## When running non-interactively
 .script_name. <- gsub("^--file=", "", Filter(function(x) {grepl("^--file=", x)}, commandArgs()))
 if (length(.script_name.) == 1) {
     cat("### Running:", paste(commandArgs()), "\n")
     options(width = 100)
 }
+
+cat("
+###
+### Prepare environment
+################################################################################\n")
 
 ## Record start time
 start_time <- Sys.time()
@@ -42,10 +42,10 @@ doParallel::registerDoParallel(cores = n_cores)
 cat("### Using", foreach::getDoParWorkers(), "cores\n")
 cat("### Using", foreach::getDoParName(), "as backend\n")
 
-## Load packages
-library(tidyverse)
 ## https://github.com/tidyverse/tibble/issues/395
 options(crayon.enabled = FALSE)
+## Load packages
+library(tidyverse)
 
 
 cat("
