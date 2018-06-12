e932508ce8b698e69f40563af305117d8f374886
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-06-12 05:33:07 -0400

    Add octave configuration

---
+++ init.d/500_octave-related.el
@@ -0,0 +1,7 @@
+;;; 500_octave-related.el ---                        -*- lexical-binding: t; -*-
+
+;;;
+;;; octave.el
+;; https://octave.org/doc/v4.0.0/Using-Octave-Mode.html
+(use-package octave
+  :mode ("\\.m\\'" . octave-mode))
