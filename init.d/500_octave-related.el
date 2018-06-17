612bd6c08e81ce42d5e3a41537ad07d0d5e219fd
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-06-17 16:34:47 -0400

    Fix comment

--- init.d/500_octave-related.el
+++ init.d/500_octave-related.el
@@ -4,4 +4,6 @@
 ;;; octave.el
 ;; https://octave.org/doc/v4.0.0/Using-Octave-Mode.html
 (use-package octave
-  :mode ("\\.m\\'" . octave-mode))
+  :mode ("\\.m\\'" . octave-mode)
+  ;; https://emacs.stackexchange.com/questions/10713/how-to-change-comment-character-from-hash-to-percent-sign-for-octave-maj
+  :hook ((octave-mode . (lambda () (setq comment-start "% ")))))
