5bb1c7dacff1faf6071b23f4a34484c15ae50300
kaz-yos <kazukiyoshida@mail.harvard.edu>
2018-07-29 05:23:29 -0400

    Configure ein

--- init.d/500_lang-python.el
+++ init.d/500_lang-python.el
@@ -44,28 +44,60 @@
 ;;;  ein.el
 ;; https://github.com/millejoh/emacs-ipython-notebook
 ;; http://millejoh.github.io/emacs-ipython-notebook/
+;; https://github.com/millejoh/emacs-ipython-notebook#requirements
 ;; http://millejoh.github.io/emacs-ipython-notebook/#usage
 ;; https://github.com/millejoh/emacs-ipython-notebook/wiki/Tips
+;;
 ;; ein:notebooklist-open fails #154 (token support)
 ;; https://github.com/millejoh/emacs-ipython-notebook/issues/154
+;; https://github.com/millejoh/emacs-ipython-notebook#requirements
+;;
+;; Anaconda managing environments
+;; https://conda.io/docs/user-guide/tasks/manage-environments.html
+;;
+;; Usage
+;; M-x shell
+;; $ conda env list
+;; $ source activate someenvironmentname
+;; M-x ein:notebooklist-login (enter the token as the password)
+;; M-x ein:notebooklist-open
 (use-package ein
   :commands (;; Start the jupyter notebook server at the given path.
+             ;; This only works if jupyter is in the default conda env.
              ein:jupyter-server-start
              ;; Log in and open a notebooklist buffer for a running jupyter notebook server.
              ein:jupyter-server-login-and-open
              ;; Log on to a jupyterhub server using PAM authentication.
              ;; Requires jupyterhub version 0.8 or greater.
              ein:jupyterhub-connect
-             ;; This is for old iPython Notebook
-             ein:connect-to-notebook)
+             ;;
+             ;; Login to IPython notebook server.
+             ;; Use the server token as a password.
+             ein:notebooklist-login
+             ;; Open notebook list buffer.
+             ein:notebooklist-open)
+  ;;
   :config
   ;; http://millejoh.github.io/emacs-ipython-notebook/#quick-try
   (use-package ein-loaddefs)
-  (use-package ein-notebook)
+  ;;
+  (use-package ein-notebook
+    :config
+    ;; Sets the frequency (in seconds) at which the notebook is automatically saved.
+    (setq ein:notebook-autosave-frequency 10))
+  ;;
   (use-package ein-subpackages)
   ;;
-  ;; Sets the frequency (in seconds) at which the notebook is automatically saved.
-  (setq ein:notebook-autosave-frequency 10))
+  (use-package ein-jupyter
+    :config
+    ;; The name of the buffer to run a jupyter notebook server session in.
+    (setq ein:jupyter-server-buffer-name "*ein:jupyter-server*")
+    ;; The default command to start a jupyter notebook server.
+    (setq ein:jupyter-default-server-command "jupyter")
+    ;; options you wish to include with the call to the jupyter notebook.
+    (setq ein:jupyter-server-args nil)
+    ;;
+    (setq ein:jupyter-default-notebook-directory nil)))
 
 
 ;;;
