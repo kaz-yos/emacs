722bc06e72ad23c47f031ffb558d37048d99b36a
kaz-yos <kazukiyoshida@mail.harvard.edu>
2019-07-10 05:35:44 -0400

    Add company-stan-backend

--- init.d/500_ess-related.el
+++ init.d/500_ess-related.el
@@ -317,20 +317,81 @@
 ;;; stan-mode.el
 (use-package stan-mode
   :mode ("\\.stan\\'" . stan-mode)
-  :commands (my-stan-setup)
+  :commands (my-stan-setup
+             company-stan-backend)
   :hook (stan-mode . my-stan-setup)
   ;;
   :config
   ;; These variables are not directly used.
   (setq stan-comment-start "//")
   (setq stan-comment-end "")
-  ;; They have to be set to buffer-local comment-start/end variables.
+  ;; Rather, they have to be set to buffer-local comment-start/end variables.
   (defun my-stan-setup ()
     (setq comment-start stan-comment-start)
     (setq comment-end stan-comment-end)
     ;; Two-character indent.
     ;; https://mc-stan.org/docs/2_18/stan-users-guide/white-space.html
-    (setq c-basic-offset 2)))
+    (setq c-basic-offset 2)
+    ;; Add company-stan-backend to the company-backends buffer-locally.
+    (add-to-list (make-local-variable 'company-backends)
+                 'company-stan-backend))
+  ;;
+  ;; Writing backends for the company-mode.
+  ;; https://github.com/company-mode/company-mode/wiki/Writing-backends
+  ;; http://sixty-north.com/blog/series/how-to-write-company-mode-backends.html
+  ;;
+  ;; stan-keywords-lists.el defines the following:
+  ;;  stan-types-list
+  ;;  stan-function-return-types-list
+  ;;  stan-blocks-list
+  ;;  stan-range-constraints-list
+  ;;  stan-keywords-list
+  ;;  stan-functions-list
+  ;;  stan-distribution-list
+  ;;  stan-reserved-list
+  ;;  Do NOT USE stan-deprecated-function-list
+  ;;
+  ;; The signature (command &optional arg &rest ignored) is mandated.
+  (defun company-stan-backend (command &optional arg &rest ignored)
+    ;; Making it interactive allows interactive testing.
+    (interactive (list 'interactive))
+    ;; (cl-case EXPR (KEYLIST BODY...)...)
+    ;; Eval EXPR and choose among clauses on that value.
+    ;; Here we decide what to do based on COMMAND and respond to
+    ;; interactive, prefix, and candidates.
+    (cl-case command
+      ;; 1. interactive call
+      ;; (company-begin-backend BACKEND &optional CALLBACK)
+      ;; Start a completion at point using BACKEND.
+      (interactive (company-begin-backend 'company-stan-backend))
+      ;;
+      ;; 2. prefix command
+      ;;  It should return the text that is to be completed.
+      ;;  If it returns nil, this backend is not used.
+      ;;  Here we need to verify the major mode.
+      (prefix (and (eq major-mode 'stan-mode)
+                   ;; If point is at the end of a symbol, return it for completion.
+                   ;; Otherwise, if point is not inside a symbol, return an empty string.
+                   ;; This will give the prefix to be completed.
+                   (company-grab-symbol)))
+      ;;
+      ;; 3. candidates commands
+      ;;  This is where we actually generate a list of possible completions.
+      ;;  When this is called arg holds the prefix string to be completed
+      (candidates
+       (cl-remove-if-not
+        ;; Retain if matching in prefix
+        (lambda (c) (string-prefix-p arg c))
+        ;; from a long list of all stan object names.
+        (append stan-types-list
+                stan-function-return-types-list
+                stan-blocks-list
+                stan-range-constraints-list
+                stan-keywords-list
+                stan-functions-list
+                stan-distribution-list
+                stan-reserved-list)))))
+  )
 
 ;;;
 ;;; julia-mode.el
