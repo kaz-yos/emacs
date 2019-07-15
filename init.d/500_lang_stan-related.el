a2f84ca0e50c8e32d9d351baae9dcf1b31ae5974
kaz-yos <kazukiyoshida@mail.harvard.edu>
2019-07-14 21:35:46 -0400

    Move code to company-stan package

--- init.d/500_lang_stan-related.el
+++ init.d/500_lang_stan-related.el
@@ -5,8 +5,7 @@
 (use-package stan-mode
   :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/stan-mode"
   :mode ("\\.stan\\'" . stan-mode)
-  :commands (my-stan-setup
-             company-stan-backend)
+  :commands (my-stan-setup)
   :hook (stan-mode . my-stan-setup)
   ;;
   :config
@@ -19,87 +18,37 @@
     (setq comment-end stan-comment-end)
     ;; Two-character indent.
     ;; https://mc-stan.org/docs/2_18/stan-users-guide/white-space.html
-    (setq c-basic-offset 2)
+    (setq c-basic-offset 2)))
+
+
+;;; company-stan.el
+(use-package company-stan
+  :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/company-stan/"
+  :commands (company-stan-backend
+             company-stan-setup)
+  :hook (stan-mode . company-stan-setup)
+  ;;
+  :config
+  (defun company-stan-setup ()
+    "Set up `company-stan-backend'
+
+Add `company-stan-backend' to `company-backends'
+buffer locally."
     ;; Add company-stan-backend to the company-backends buffer-locally.
     (add-to-list (make-local-variable 'company-backends)
-                 'company-stan-backend))
-  ;;
-  ;; Writing backends for the company-mode.
-  ;; https://github.com/company-mode/company-mode/wiki/Writing-backends
-  ;; http://sixty-north.com/blog/series/how-to-write-company-mode-backends.html
-  ;; Definitions
-  ;; https://github.com/company-mode/company-mode/blob/master/company.el
-  ;; Example in company-math.el
-  ;; https://github.com/vspinu/company-math/blob/master/company-math.el#L210
-  ;;
-  ;; stan-keywords-lists.el defines the following:
-  ;;  stan-types-list
-  ;;  stan-function-return-types-list
-  ;;  stan-blocks-list
-  ;;  stan-range-constraints-list
-  ;;  stan-keywords-list
-  ;;  stan-functions-list
-  ;;  stan-distribution-list
-  ;;  stan-reserved-list
-  ;;  Do NOT USE stan-deprecated-function-list
-  ;;
-  (defun propertize-list (lst category)
-    "Propertize each element of a string list"
-    (mapcar (lambda (s)
-              (propertize s :category category))
-            lst))
-  ;;
-  (defun company-stan-backend-annotation (s)
-    "Construct an annotation string from the :category property"
-    (format " [%s]" (get-text-property 0 :category s)))
-  ;;
-  ;; The signature (command &optional arg &rest ignored) is mandated.
-  (defun company-stan-backend (command &optional arg &rest ignored)
-    "company-mode backend function for stan-mode"
-    ;; Making it interactive allows interactive testing.
-    (interactive (list 'interactive))
-    ;; (cl-case EXPR (KEYLIST BODY...)...)
-    ;; Eval EXPR and choose among clauses on that value.
-    ;; Here we decide what to do based on COMMAND.
-    ;; One of {interactive, prefix, candidates, annotation}
-    (cl-case command
-      ;; 1. interactive call
-      ;; (company-begin-backend BACKEND &optional CALLBACK)
-      ;; Start a completion at point using BACKEND.
-      (interactive (company-begin-backend 'company-stan-backend))
-      ;; 2. prefix command
-      ;;  It should return the text that is to be completed.
-      ;;  If it returns nil, this backend is not used.
-      ;;  Here we need to verify the major mode.
-      (prefix (and (eq major-mode 'stan-mode)
-                   ;; If point is at the end of a symbol, return it for completion.
-                   ;; Otherwise, if point is not inside a symbol, return an empty string.
-                   ;; This will give the prefix to be completed.
-                   (company-grab-symbol)))
-      ;; 3. candidates command
-      ;;  This is where we actually generate a list of possible completions.
-      ;;  When this is called arg holds the prefix string to be completed
-      (candidates
-       (cl-remove-if-not
-        ;; Retain if matching
-        (lambda (c) (my-company-fuzzy-match arg c))
-        ;; from a long list of all stan object names.
-        (append (propertize-list stan-types-list "type")
-                (propertize-list stan-function-return-types-list "function return type")
-                (propertize-list stan-blocks-list "block")
-                (propertize-list stan-range-constraints-list "range constraint")
-                (propertize-list stan-keywords-list "keyword")
-                (propertize-list stan-functions-list "function")
-                (propertize-list stan-distribution-list "distribution")
-                (propertize-list stan-reserved-list "reserved")
-                )))
-      ;; 4. annotation command
-      (annotation (company-stan-backend-annotation arg))))
-  )
+                 'company-stan-backend)))
 
 
 ;;; eldoc-stan.el
 (use-package eldoc-stan
   :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/eldoc-stan/"
   :hook (stan-mode . eldoc-stan-setup)
-  )
+  ;;
+  :config
+  (defun eldoc-stan-setup ()
+    "Set up `eldoc-stan-eldoc-documentation-function'
+
+Specify `eldoc-stan-eldoc-documentation-function' as
+`eldoc-documentation-function'"
+    (setq-local eldoc-documentation-function
+                #'eldoc-stan-eldoc-documentation-function)))
