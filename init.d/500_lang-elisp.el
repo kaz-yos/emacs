a12a9cdf708b529043f05e91fc4cd65ad58c173c
kaz-yos <kazukiyoshida@mail.harvard.edu>
2019-07-27 08:19:22 -0400

    Reorganize elisp part

--- init.d/500_lang-elisp.el
+++ init.d/500_lang-elisp.el
@@ -1,12 +1,23 @@
 ;;; -*- lexical-binding: t; -*-
-;;;
-;;; EMACS LISP
-;;;  elisp programming configurations
+
 ;; Non-nil means enter debugger if an error is signaled.
 ;; (setq debug-on-error t)
 
+;;;
+;;; Contributing a new recipe to MELPA
+;; https://github.com/melpa/melpa/blob/master/CONTRIBUTING.org
+
+
+;;;
+;;; Cask
+;; Project management tool for Emacs
+;; https://github.com/cask/cask
+;; https://cask.readthedocs.io/en/latest/
 (add-to-list 'auto-mode-alist '("Cask" . emacs-lisp-mode))
 
+
+;;;
+;;; Code navigation and evaluation
 ;;;  elisp-slime-nav.el
 ;; SLIME-like navigation for elisp
 ;; https://github.com/purcell/elisp-slime-nav
@@ -19,13 +30,11 @@
   (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
     (add-hook hook 'turn-on-elisp-slime-nav-mode)))
 
-
 ;;;  eros.el
 ;; https://github.com/xiongtx/eros/tree/dd8910279226259e100dab798b073a52f9b4233a
 (use-package eros
   :config
   (eros-mode 1))
-
 
 ;;;  lispxmp.el
 ;; https://www.emacswiki.org/emacs/lispxmp.el
@@ -34,39 +43,17 @@
 (use-package lispxmp
   :commands (lispxmp))
 
-
 ;;;  macrostep.el
 ;; https://github.com/joddie/macrostep
 (use-package macrostep
   :commands (macrostep-mode
              macrostep-expand))
 
-
 ;;;  nameless.el
 ;; https://github.com/Malabarba/Nameless
 ;; http://endlessparentheses.com/nameless-less-is-more.html
 (use-package nameless
   :commands (nameless-mode))
-
-
-;;;  buttercup.el
-;; Behavior-Driven Emacs Lisp Testing
-;; https://github.com/jorgenschaefer/emacs-buttercup
-(use-package buttercup
-  :hook (emacs-lisp-mode . buttercup-minor-mode)
-  :commands (buttercup-minor-mode))
-
-
-;;;  package-lint.el
-;; https://github.com/purcell/package-lint
-(use-package package-lint
-  :commands (package-lint-current-buffer))
-
-
-;;;  checkdoc.el
-(use-package checkdoc
-  :hook (emacs-lisp-mode . checkdoc-minor-mode))
-
 
 ;;;  let*-setq macro
 ;; Crude interactive testing
@@ -88,3 +75,22 @@ This will globally bind these variables for crude interactive testing."
         (lambda (let-pair)
           (cons 'setq let-pair))
         body)))
+
+
+;;;
+;;; Good practice related
+;;;  buttercup.el
+;; Behavior-Driven Emacs Lisp Testing
+;; https://github.com/jorgenschaefer/emacs-buttercup
+(use-package buttercup
+  :hook (emacs-lisp-mode . buttercup-minor-mode)
+  :commands (buttercup-minor-mode))
+
+;;;  package-lint.el
+;; https://github.com/purcell/package-lint
+(use-package package-lint
+  :commands (package-lint-current-buffer))
+
+;;;  checkdoc.el
+(use-package checkdoc
+  :hook (emacs-lisp-mode . checkdoc-minor-mode))
