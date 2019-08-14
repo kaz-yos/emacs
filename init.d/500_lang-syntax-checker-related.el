db5c468312851f14830c00e6a5bf65c444008b97
kaz-yos <kazukiyoshida@mail.harvard.edu>
2019-08-14 08:41:25 -0400

    Configure flycheck

--- init.d/500_lang-syntax-checker-related.el
+++ init.d/500_lang-syntax-checker-related.el
@@ -14,27 +14,40 @@
 ;; http://www.flycheck.org/en/latest/
 ;; https://github.com/flycheck/flycheck
 ;; Supported languages
-;;  http://flycheck.readthedocs.org/en/latest/guide/languages.html#supported-languages
-;; 3rd party extensions
-;;  http://flycheck.readthedocs.org/en/latest/guide/introduction.html#rd-party-extensions
+;;  http://www.flycheck.org/en/latest/languages.html
+;; Troubleshooting
+;;  http://www.flycheck.org/en/latest/user/troubleshooting.html
+;; Developer’s Guide
+;;  http://www.flycheck.org/en/latest/developer/developing.html
+;;
+;; M-x `flycheck-verify-setup' in an appropriate buffer to diagnose issues.
 (use-package flycheck
-  :commands (flycheck-mode
-             global-flycheck-mode)
-  ;;
   :config
-  (global-flycheck-mode +1)
-  ;;; flycheck-pos-tip.el
-  ;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b#2-5
-  (use-package flycheck-pos-tip
-    :config
-    (setq flycheck-display-errors-function
-          #'flycheck-pos-tip-error-messages)
-    (flycheck-pos-tip-mode +1)))
+  (global-flycheck-mode +1))
 
+;;;  flycheck-pos-tip.el
+;; https://github.com/flycheck/flycheck-pos-tip
+(use-package flycheck-pos-tip
+  :disabled t
+  :after flycheck
+  :config
+  (setq flycheck-display-errors-function
+        #'flycheck-pos-tip-error-messages)
+  (flycheck-pos-tip-mode +1))
 
 ;;;  flycheck-package.el
 ;; Flycheck checker for elisp package metadata
 ;; https://github.com/purcell/flycheck-package
 (use-package flycheck-package
+  :after flycheck
   :commands (flycheck-package-setup)
-  :hook (emacs-lisp-mode . flycheck-package-setup))
+  :config
+  (flycheck-package-setup))
+
+;;;  flycheck-cask.el
+;; https://github.com/flycheck/flycheck-cask
+(use-package flycheck-cask
+  :after flycheck
+  :commands (flycheck-cask-setup)
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-cask-setup))
