c49730ef3ae76eba5061aa6901445c59297432f4
kaz-yos <kazukiyoshida@mail.harvard.edu>
2019-08-15 08:36:28 -0400

    Add reb-mode enhancement

--- init.d/500_lang-syntax-checker-related.el
+++ init.d/500_lang-syntax-checker-related.el
@@ -2,6 +2,7 @@
 
 ;;;
 ;;; FLYMAKE-RELATED
+;;;
 ;;;  flymake.el
 (use-package flymake
   :commands (flymake-mode)
@@ -10,6 +11,8 @@
 
 ;;;
 ;;; FLYCHECK-RELATED
+;;;
+
 ;;;  flycheck.el
 ;; http://www.flycheck.org/en/latest/
 ;; https://github.com/flycheck/flycheck
@@ -21,16 +24,42 @@
 ;;  http://www.flycheck.org/en/latest/developer/developing.html
 ;;
 ;; M-x `flycheck-verify-setup' in an appropriate buffer to diagnose issues.
+;; M-x `flycheck-compile' to explicitly check the buffer file.
+;;
 (use-package flycheck
+  :commands (flycheck-enhance-rx-buffer-locally)
+  :hook (reb-mode . flycheck-enhance-rx-buffer-locally)
   :config
+  ;;
+  (defun flycheck-enhance-rx-buffer-locally ()
+    "Enhances `rx' buffer locally with `flycheck' elements.
+
+`flycheck' adds keywords `line', `column', `file-name',
+`message', and `id' to `rx-constituents' defined in `rx.el'
+to handle error message parsing.
+
+This function is intended for `re-builder'."
+    (setq-local rx-constituents
+                (append
+                 `((line . ,(rx (group-n 2 (one-or-more digit))))
+                   (column . ,(rx (group-n 3 (one-or-more digit))))
+                   (file-name flycheck-rx-file-name 0 nil)
+                   (message flycheck-rx-message 0 nil)
+                   (id flycheck-rx-id 0 nil))
+                 rx-constituents nil)))
+  ;; Delay in seconds before displaying errors at point.
+  (setq flycheck-display-errors-delay 0.9)
+  ;;
+  ;; Enable everywhere
   (global-flycheck-mode +1))
 
 ;;;  flycheck-pos-tip.el
 ;; https://github.com/flycheck/flycheck-pos-tip
 (use-package flycheck-pos-tip
-  :disabled t
   :after flycheck
+  :commands (flycheck-pos-tip-error-messages)
   :config
+  ;; Function to display error messages.
   (setq flycheck-display-errors-function
         #'flycheck-pos-tip-error-messages)
   (flycheck-pos-tip-mode +1))
