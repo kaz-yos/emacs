47d4023c2d7a521a64c98c3537839b72fe3bf1c2
kaz-yos <kazukiyoshida@mail.harvard.edu>
2019-09-06 05:35:01 -0400

    Clean stan-mode configuration

--- init.d/500_lang_stan-related.el
+++ init.d/500_lang_stan-related.el
@@ -1,46 +1,62 @@
 ;;; 500_stan-related.el ---                          -*- lexical-binding: t; -*-
 
-;;;
+
+;; Uncomment the line below if not required elsewhere.
+;; (require 'use-package)
+
 ;;; stan-mode.el
 (use-package stan-mode
-  :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/stan-mode"
+  ;; :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/stan-mode"
   :mode ("\\.stan\\'" . stan-mode)
   :hook (stan-mode . stan-mode-setup)
   ;;
   :config
-  (setq stan-indentation-offset 2)
-  (setq stan-handle-deprecated-comments nil))
-
+  ;; The officially recommended offset is 2.
+  (setq stan-indentation-offset 2))
 
 ;;; company-stan.el
 (use-package company-stan
-  :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/company-stan/"
+  ;; :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/company-stan/"
   :hook (stan-mode . company-stan-setup)
   ;;
   :config
   ;; Whether to use fuzzy matching in `company-stan'
-  (setq company-stan-fuzzy nil)
-  )
-
+  (setq company-stan-fuzzy nil))
 
 ;;; eldoc-stan.el
 (use-package eldoc-stan
-  :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/eldoc-stan/"
+  ;; :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/eldoc-stan/"
   :hook (stan-mode . eldoc-stan-setup)
   ;;
   :config
+  ;; No configuration options as of now.
   )
 
+;;; flycheck-stan.el
+(use-package flycheck-stan
+  ;; :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/flycheck-stan/"
+  :hook (stan-mode . flycheck-stan-setup)
+  ;;
+  :config
+  ;; No configuration options as of now.
+  )
 
+;;; stan-snippets.el
 (use-package stan-snippets
-  :after stan-mode
-  :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/stan-snippets/"
+  ;; :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/stan-snippets/"
+  :hook (stan-mode . stan-snippets-initialize)
+  ;;
+  :config
+  ;; No configuration options as of now.
   )
 
 ;;; ac-stan.el
 (use-package ac-stan
-  :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/ac-stan/"
-  ;; :hook (stan-mode . stan-ac-mode-setup)
+  ;; :load-path "~/Documents/programming/emacs-lisp-repos/stan-mode/ac-stan/"
+  ;; Delete the line below if using.
+  :disabled t
+  :hook (stan-mode . stan-ac-mode-setup)
   ;;
   :config
+  ;; No configuration options as of now.
   )
