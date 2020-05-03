5b064ceab501a3ffc47c90e34a2aceecf7f53aa4
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-05-03 07:09:41 -0400

    Add .emacs.d rsync scripts

---
+++ misc/rsync_o2.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# contents of ~/.emacs.d/ to o2:~/.emacs.d/
+rsync -av ~/.emacs.d/ o2:~/.emacs.d/
---
+++ misc/rsync_odyssey.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# contents of ~/.emacs.d/ to odyssey:~/.emacs.d/
+rsync -av ~/.emacs.d/ odyssey:~/.emacs.d/
