2cc30f09b224c539035e365b1e439a3ad351c3ef
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-05-30 14:04:45 -0400

    Configure both gnu and melpa with https and http

--- init.el
+++ init.el
@@ -70,26 +70,28 @@
 ;;;
 ;;; package.el and repositories
 (require 'package)
-;;
-;;;  ELPA
-;; https://elpa.gnu.org/packages/
-;; https://www.reddit.com/r/emacs/comments/ccrxk9/elpa_down/
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")))
-;;
-;;;  MELPA repository
-;; http://melpa.milkbox.net/#installing
 ;; http://melpa.milkbox.net/#/getting-started
+;; Change repository settings depending on gnutls availability.
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
+       (protocol (if no-ssl "http" "https")))
+  ;;
   (when no-ssl (warn "
 Your version of Emacs does not support SSL connections,
 which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+  ;;
+;;;  ELPA
+  ;; https://elpa.gnu.org/packages/
+  ;; https://www.reddit.com/r/emacs/comments/ccrxk9/elpa_down/
+  (setq package-archives (cons "gnu" (concat protocol "://elpa.gnu.org/packages/")))
+  ;;
+;;;  MELPA repository
+  ;; http://melpa.milkbox.net/#installing
+  ;; http://melpa.milkbox.net/#/getting-started
+  (add-to-list 'package-archives (cons "melpa" (concat protocol "://melpa.org/packages/")) t))
 ;;
 ;; Refresh contents  if no package-archive-contents available
 ;; http://stackoverflow.com/questions/14836958/updating-packages-in-emacs
