d1293e96a7efa8bae1428a7af98ef840552a26b7
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-09-03 09:09:25 -0400

    Add svg screenshot function for linux

--- init.d/200_window-management-related.el
+++ init.d/200_window-management-related.el
@@ -145,3 +145,17 @@
   ;; Do not open a new frame opening a file from Finder
   ;; http://stackoverflow.com/questions/6068819/alias-to-make-emacs-open-a-file-in-a-new-buffer-not-frame-and-be-activated-com
   (setq ns-pop-up-frames nil))
+
+
+;;; svg screenshot within emacs (> 27)
+;; https://www.reddit.com/r/emacs/comments/idz35e/emacs_27_can_take_svg_screenshots_of_itself/
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
