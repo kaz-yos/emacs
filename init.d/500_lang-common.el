62ce7d3a59b1f9ed71babe454c63bbd5e7da6354
Kazuki Yoshida <ky088@eris1n2.research.partners.org>
2020-09-08 16:21:50 -0400

    Configure lsp for r-mode

--- init.d/500_lang-common.el
+++ init.d/500_lang-common.el
@@ -8,7 +8,26 @@
 ;; https://emacs-lsp.github.io/lsp-mode/page/installation/#use-package
 (use-package lsp-mode
   :ensure t
-  :commands (lsp))
+  :commands (lsp)
+  :hook ((r-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :config
+  (setq lsp-keymap-prefix "s-l")
+  ;; optionally
+  (use-package lsp-ui
+    :commands (lsp-ui-mode))
+  ;; if you are helm user
+  (use-package helm-lsp
+    :commands (helm-lsp-workspace-symbol))
+  ;; if you are ivy user
+  (use-package lsp-ivy
+    :commands (lsp-ivy-workspace-symbol))
+  ;; (use-package lsp-treemacs
+  ;;   :commands (lsp-treemacs-errors-list))
+  ;; optionally if you want to use debugger
+  ;; (use-package dap-mode)
+  ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+  )
 
 
 ;;;
