5516db5a57d30ba8670730a1001f25c733906b91
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-12-14 21:08:27 -0500

    Comment out display-buffer-in-new-tab

--- init.d/200_tab-related.el
+++ init.d/200_tab-related.el
@@ -26,7 +26,7 @@
          ("s" . tab-bar-select-tab-by-name)
          ("r" . tab-bar-rename-tab)
          ("u" . tab-bar-undo-close-tab)
-         ("D" . display-buffer-in-new-tab)
+         ;; ("D" . display-buffer-in-new-tab) ; not a user command
          ;;
          ("1" . my-tab-bar-select-tab-1)
          ("2" . my-tab-bar-select-tab-2)
