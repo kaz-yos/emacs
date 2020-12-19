108025d0225702c19652558d0ec571dc1e0908cf
kaz-yos <kazukiyoshida@mail.harvard.edu>
2020-12-19 07:15:30 -0500

    Configure gnus

--- init.d/500_email.el
+++ init.d/500_email.el
@@ -488,11 +488,13 @@ It restores mu4e window layout after killing the compose-buffer."
 ;;; GNUS-RELATED
 ;;; gnus
 ;; http://www.emacswiki.org/emacs/GnusGmail
-(setq gnus-select-method
-      '(nnimap "gmail"
-	       (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
-	       (nnimap-server-port "imaps")
-	       (nnimap-stream ssl)))
-
-(setq smtpmail-smtp-service 587
-      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+(use-package gnus
+  :config
+  (setq gnus-select-method
+        '(nnimap "gmail"
+	         (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
+	         (nnimap-server-port "imaps")
+	         (nnimap-stream ssl)))
+  ;;
+  (setq smtpmail-smtp-service 587)
+  (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"))
