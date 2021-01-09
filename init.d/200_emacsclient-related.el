3a57f6f2579dd98f02dbc7a5a325858b7d225939
kaz-yos <kazukiyoshida@mail.harvard.edu>
2021-01-09 07:44:05 -0500

    Add an emacsclient-c starter

--- init.d/200_emacsclient-related.el
+++ init.d/200_emacsclient-related.el
@@ -8,6 +8,22 @@
 ;;
 ;; Configure as $EDITOR
 ;; .profile: export EDITOR="emacsclient"
+;;
+;; 39.3 emacsclient Options
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/emacsclient-Options.html
+;;
+;; Create a new graphical client frame, instead of using an existing Emacs frame.
+;; emacsclient --create-frame
+;;
+;; Create a new client frame on the current text terminal. (Also -t --tty)
+;; emacsclient -nw
+(defun my-start-emacsclient-c ()
+  "Start a process of a new graphical emacsclient."
+  (interactive)
+  ;; (start-process NAME BUFFER PROGRAM &rest PROGRAM-ARGS)
+  (start-process "emacsclient-c" nil
+                 "emacsclient" "--create-frame"))
+(bind-key "e" 'my-start-emacsclient-c my-key-map)
 
 
 ;;;
