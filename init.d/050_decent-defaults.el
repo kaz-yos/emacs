a06f421b49b1f459a40f1f76e60bfb0db45c02e5
kaz-yos <kazukiyoshida@mail.harvard.edu>
2021-01-19 05:11:03 -0500

    Add *Messages* to (frame-parameter nil 'buffer-list) for the first frame

--- init.d/050_decent-defaults.el
+++ init.d/050_decent-defaults.el
@@ -404,3 +404,9 @@
 ;; https://www.reddit.com/r/emacs/comments/hsszos/how_to_get_rid_of_black_border_around_emacsmacosx/
 ;; If non-nil, frame sizes can increase/decrease by one pixel.
 (setq frame-resize-pixelwise t)
+
+
+;;;
+;;; Add *Messages* to (frame-parameter nil 'buffer-list) for the first frame.
+;; To have *Messages* in `my-tab-bar-buffer-name-filter'.
+(add-to-list 'default-frame-alist `(buffer-list . ,(list (get-buffer "*Messages*"))))
