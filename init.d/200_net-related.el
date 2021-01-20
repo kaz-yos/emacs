0b619ab3082ae1a9462a11196eee9dea055ebdaf
kaz-yos <kazukiyoshida@mail.harvard.edu>
2021-01-20 10:49:02 -0500

    Invoke both Firefox and Safari on macOS

--- init.d/200_net-related.el
+++ init.d/200_net-related.el
@@ -7,4 +7,15 @@
   (when (and (display-graphic-p)
              (fboundp 'xwidget-webkit-browse-url)
              (featurep 'xwidget-internal))
-    (setq browse-url-browser-function 'xwidget-webkit-browse-url)))
+    (setq browse-url-browser-function 'xwidget-webkit-browse-url))
+  ;;
+  (when (eq system-type 'darwin)
+    (defun browse-url-macosx-firefox-safari-browser (url &optional _new-window)
+      "Invoke the macOS system's Firefox and Safari.
+The optional NEW-WINDOW argument is not used."
+      (interactive (browse-url-interactive-arg "URL: "))
+      (start-process (concat "open-firefox" url) nil "open" "-a" "/Applications/Firefox.app" url)
+      (start-process (concat "open-safari" url) nil "open" "-a" "/Applications/Safari.app" url))
+    (advice-add #'browse-url-default-macosx-browser
+                :override
+                #'browse-url-macosx-firefox-safari-browser)))
