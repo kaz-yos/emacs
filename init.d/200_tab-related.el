d41de267cf34c7d4f64d380768a79f435edf20bc
kaz-yos <kazukiyoshida@mail.harvard.edu>
2021-02-19 05:57:55 -0500

    Do not use projectile project name for tab-bar name

--- init.d/200_tab-related.el
+++ init.d/200_tab-related.el
@@ -218,18 +218,6 @@ This is similar to `elscreen-clone'."
     (advice-add 'tab-bar-make-keymap-1
                 :filter-return #'my-add-buffer-info))
   ;;
-  ;; Use the projectile project name for the tab name.
-  ;; https://github.com/toyboot4e/dotfiles/blob/master/editor/emacs/elisp/ide.el#L172-L178
-  (with-eval-after-load "projectile"
-    (defun my-tab-bar-tab-name-current-project-or-buffer ()
-      (let ((project-name (projectile-project-name)))
-        (if (string-equal project-name "-")
-            (tab-bar-tab-name-current)
-          project-name)))
-    ;; Function to get a tab name.
-    (setq tab-bar-tab-name-function
-          #'my-tab-bar-tab-name-current-project-or-buffer))
-  ;;
   ;; Defines when to show the tab bar.
   (setq tab-bar-show t)
   ;; Do not show buttons.
