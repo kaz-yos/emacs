37ffde0796bb779e656150bda2105a7bb1299420
kaz-yos <kazukiyoshida@mail.harvard.edu>
2021-02-22 13:07:26 -0500

    Clean yasnippet configuration

--- init.d/500_yasnippet-related.el
+++ init.d/500_yasnippet-related.el
@@ -1,27 +1,23 @@
 ;;; 500_yasnippet-related.el ---                     -*- lexical-binding: t; -*-
 
 ;;; yasnippet.el
-;; https://github.com/capitaomorte/yasnippet
-;; https://joaotavora.github.io/yasnippet
-;; https://blog.alex-miller.co/emacs/spacemacs/2017/05/28/yasnippets.html/
-;; http://fukuyama.co/yasnippet
-;; http://d.hatena.ne.jp/kiwanami/20110224/1298526678
-;; https://www.youtube.com/watch?v=xmBovJvQ3KU
+;; https://github.com/joaotavora/yasnippet
+;; http://joaotavora.github.io/yasnippet/
 (use-package yasnippet
   :ensure t
   :defer 2
   :diminish  yas-minor-mode
   :bind (:map yas-minor-mode-map
-              ;; Insert with a menu
-              ("C-x i i" . yas-insert-snippet)
-              ;; Expand at point
-              ("C-x i e" . yas-expand)
-              ;; Open a buffer to create a new snippet
-              ("C-x i n" . yas-new-snippet)
-              ;; View/edit snippets
-              ("C-x i v" . yas-visit-snippet-file)
-              ;; Display snippets for each table.
-              ("C-x i d" . yas-describe-tables))
+         ;; Insert with a menu
+         ("C-x i i" . yas-insert-snippet)
+         ;; Expand at point
+         ("C-x i e" . yas-expand)
+         ;; Open a buffer to create a new snippet
+         ("C-x i n" . yas-new-snippet)
+         ;; View/edit snippets
+         ("C-x i v" . yas-visit-snippet-file)
+         ;; Display snippets for each table.
+         ("C-x i d" . yas-describe-tables))
   :config
   ;; Use Popup isearch for Yasnippet Prompt.
   ;; http://iany.me/2012/03/use-popup-isearch-for-yasnippet-prompt/
