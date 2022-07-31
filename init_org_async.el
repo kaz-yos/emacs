255d2465eeef282d073c68823ea4515cb8edac2b
kaz-yos <kazukiyoshida@mail.harvard.edu>
2022-07-31 11:39:49 -0400

    Drop ox-extra

--- init_org_async.el
+++ init_org_async.el
@@ -30,9 +30,6 @@
 (require 'org)
 (require 'org-ref)
 ;;; Load exporters
-;; https://emacs.stackexchange.com/questions/9492/is-it-possible-to-export-content-of-subtrees-without-their-headings
-(require 'ox-extra)
-(ox-extras-activate '(ignore-headlines))
 (require 'ox-beamer)
 ;; bold is bold not \alert{} (can appear as red text)
 ;; http://orgmode.org/worg/exporters/beamer/ox-beamer.html#export-filters
