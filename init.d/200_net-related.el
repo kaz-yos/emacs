2d07ca8f15c053720deeaff7e5a79f650fbe8be1
kaz-yos <kazukiyoshida@mail.harvard.edu>
2025-09-28 20:42:13 -0400

    Add my-xwidget-open-in-system-browser (not functional yet)

--- init.d/200_net-related.el
+++ init.d/200_net-related.el
@@ -7,9 +7,22 @@
   (when (and (display-graphic-p)
              (fboundp 'xwidget-webkit-browse-url)
              (featurep 'xwidget-internal))
+    ;; Avoid configuring xwidget-webkit-browse-url as the default browser.
+    ;; Using the system browswer is better in most settings.
     ;; (setq browse-url-browser-function 'xwidget-webkit-browse-url)
-    )
+    ;;
+    (defun my-xwidget-open-in-system-browser ()
+      "Get the current URL from an xwidget-webkit buffer and open it
+  in the system's default browser."
+      (interactive)
+      (if (equal major-mode 'xwidget-webkit-mode)
+          (let ((url (xwidget-webkit-current-url)))
+            (if url
+                (browse-url url)
+              (message "Could not get URL from xwidget buffer.")))
+        (message "Not in an xwidget-webkit buffer."))))
   ;;
+  ;; Legacy configuration to open both Safari and Firefox (for Zotero).
   (when (eq system-type 'darwin)
     (defun browse-url-macosx-firefox-safari-browser (url &optional _new-window)
       "Invoke the macOS system's Firefox and Safari.
