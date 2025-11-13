fc61829202b7697fe5aaa1b6656bbca3d929c3d2
Kazuki Yoshida (he/him/his) <yoshida@usmafpcfx9t3gpm.bos1.vrtx.com>
2025-11-13 10:17:17 -0500

    Drop parallel from rscript.R template

--- autoinsert/rscript.R
+++ autoinsert/rscript.R
@@ -24,29 +24,6 @@ if (length(.script_name.) == 1) {
 start_time <- Sys.time()
 cat("### Started ", as.character(start_time), "\n")
 
-## Configure parallelization
-## Parallel backend for foreach (also loads foreach and parallel; includes doMC)
-library(doParallel)
-## Reproducible parallelization
-library(doRNG)
-## Detect core count. This supports LSF and SLURM.
-n_cores <- future::availableCores()
-## Used by parallel::mclapply() as default
-options(mc.cores = n_cores)
-## Used by doParallel as default
-options(cores = n_cores)
-## Register doParallel as the parallel backend for foreach
-## http://stackoverflow.com/questions/28989855/the-difference-between-domc-and-doparallel-in-r
-doParallel::registerDoParallel(cores = n_cores)
-## future: Unified Parallel and Distributed Processing in R for Everyone
-## https://cran.r-project.org/web/packages/future/vignettes/future-1-overview.html
-library(future)
-future::plan(multicore)
-## Report multicore use
-foreach::getDoParName()
-foreach::getDoParWorkers()
-future::availableCores()
-
 ## Load packages
 library(tidyverse)
 
@@ -68,6 +45,4 @@ diff_time <- difftime(end_time, start_time, units = "auto")
 cat("Started  ", as.character(start_time), "\n",
     "Finished ", as.character(end_time), "\n",
     "Time difference of ", diff_time, " ", attr(diff_time, "units"), "\n",
-    "Used ", foreach::getDoParWorkers(), " cores\n",
-    "Used ", foreach::getDoParName(), " as backend\n",
     sep = "")
